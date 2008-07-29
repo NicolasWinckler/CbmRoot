f56f65ebc35347dc4d365d59a112cf05890b9ce1
steinle <steinle@0381ead4-6506-0410-b988-94b70fbc4730>
2008-07-29 10:06:52 +0000

    Jul25
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@3315 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ htrack/AnalysisLIB/include/prelutRangeAnalysisDef.h
@@ -0,0 +1,64 @@
+//////////////////////////////////////////////////////////////////////
+/// (C)opyright 2004
+/// 
+/// Institute of Computer Science V
+/// Prof. M‰nner
+/// University of Mannheim, Germany
+/// 
+/// *******************************************************************
+/// 
+/// Designer(s):   Steinle / Gl‰ﬂ
+/// 
+/// *******************************************************************
+/// 
+/// Project:     Trackfinder for CBM-Project at GSI-Darmstadt, Germany
+/// 
+/// *******************************************************************
+/// 
+/// Description:
+///
+///   file:
+///     - global definition of structs, defines and enums for project
+///
+/// *******************************************************************
+///
+/// $Author: csteinle $
+/// $Date: 2007-12-13 13:47:19 $
+/// $Revision: 1.1 $
+///
+//////////////////////////////////////////////////////////////////////
+
+
+#ifndef _PRELUTRANGEANALYSISDEF_H
+#define _PRELUTRANGEANALYSISDEF_H
+
+
+#define PRELUTRANGEEVENTMODIFIER            "Event"											/**< Defines the text to display in the header of a histogram for an event */
+
+#define PRELUTRANGEANALYSISDIR              "StsAnalysis"									/**< This defines the name of the directory where the analysis is placed. */
+#define PRELUTRANGESAVINGDIRECTORY          "PrelutRange"									/**< Defines the name of the saving directory */
+
+#define PRELUTRANGEMAINLAYERNAME            "PrelutRangeMainLayer"							/**< Defines the name for the display */
+#define RELUTRANGEMAINLAYERTITLE            "Prelut Range Main Layer"						/**< Defines the title for the display */
+
+#define PRELUTRANGEMAINLAYERWINDOWNAME      "PRMLAWindow"									/**< Defines the name for the window which shows the displays */
+#define PRELUTRANGEMAINLAYERWINDOWTITLE     "Prelut Range Main Layer Analysis Window"		/**< Defines the title for the window which shows the displays */
+
+#define PRELUTRANGECORRETLAYERNAME          "PrelutRangeCorrectLayer"						/**< Defines the name for the display */
+#define PRELUTRANGECORRECTLYERTITLE         "Prelut Range Correct Layer"					/**< Defines the title for the display */
+
+#define PRELUTRANGECORRECTLAYERWINDOWNAME   "PRCLAWindow"									/**< Defines the name for the window which shows the displays */
+#define PRELUTRANGECORRECTLAYERWINDOWTITLE  "Prelut Range Correct Layer Analysis Window"	/**< Defines the title for the window which shows the displays */
+
+#define PRELUTRANGEXAXITITLE                "Minimum [#]"									/**< Defines the text to display for the title of the x-axi for the display */
+#define PRELUTRANGEYAXITITLE                "Maximum [#]"									/**< Defines the text to display for the title of the y-axi for the display */
+
+#define PRELUTRANGECONSTRAINTZAXITITLE      "Number of Entries [#]"							/**< Defines the text to display for the title of the z-axi for the display */
+#define PRELUTRANGENORMALVALUEZAXITITLE     "Number of Hits [#]"							/**< Defines the text to display for the title of the z-axi for the display */
+#define PRELUTRANGERELATIVEVALUEZAXITITLE   "Percentage of Hits [%]"						/**< Defines the text to display for the title of the z-axi for the display */
+
+#define PRELUTRANGEHITSXAXITITLE            "Station [index]"								/**< Defines the text to display for the title of the x-axi for the display */
+#define PRELUTRANGEHITSYAXITITLE            "Number of hits [#]"							/**< Defines the text to display for the title of the y-axi for the display */
+
+
+#endif

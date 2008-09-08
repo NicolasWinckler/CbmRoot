f0864faf33e7cc829fd021aa7dafc565f072f220
friese <friese@0381ead4-6506-0410-b988-94b70fbc4730>
2008-09-08 21:09:31 +0000

    Class CbmPropagator added
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@3637 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ cbmbase/CbmPropagator.h
@@ -0,0 +1,60 @@
+/** CbmPropagator.h
+ **
+ ** Interface for track propagation algorithms.
+ **/ 
+
+#ifndef CBMPROPAGATOR_H_
+#define CBMPROPAGATOR_H_
+
+#include "CbmStatusCode.h"
+
+#include "TObject.h"
+#include "TMatrixD.h"
+
+#include <vector>
+
+class CbmTrackParam;
+
+class CbmPropagator : public TObject {
+public:
+   CbmPropagator();
+   virtual ~CbmPropagator();
+   
+   /** Pure virtual function for track parameters propagation
+    ** @param parIn            input track parameter
+    ** @param parOut           output track parameter
+    ** @param zOut             z position to propagate to
+    ** @param pdg              PDG code of the particle 
+    **/
+   virtual StatusCode Propagate( 
+		   const CbmTrackParam *parIn,
+           CbmTrackParam *parOut,
+           Double_t zOut,
+           Int_t pdg) = 0;
+    
+   /** Pure virtual function for track parameters propagation
+    ** @param par              input/output track parameter
+    ** @param zOut             z position to propagate to
+    ** @param pdg              PDG code of the particle 
+    **/
+   virtual StatusCode Propagate( 
+		   CbmTrackParam *par, 
+           Double_t zOut,
+           Int_t pdg) = 0;
+   
+   /** Pure virtual function to access the transport matrix
+    ** @param F      output 5x5 transport matrix as a vector with 25 elements
+    **/
+   virtual void TransportMatrix(
+		   std::vector<Double_t>& F) = 0;
+   
+   /** Pure virtual function to access the transport matrix
+    ** @param F      output 5x5 transport matrix as a TMatrixD class 
+    **/
+   virtual void TransportMatrix(
+		   TMatrixD& F) = 0;
+      
+   ClassDef(CbmPropagator,1);
+}; 
+
+#endif /*CBMPROPAGATOR_H_*/

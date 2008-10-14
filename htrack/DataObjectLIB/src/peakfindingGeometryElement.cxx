6c29d7c890e7414f1e13c4b4f9db05e9a5383a85
steinle <steinle@0381ead4-6506-0410-b988-94b70fbc4730>
2008-10-14 14:15:06 +0000

    Add missing object
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@3820 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ htrack/DataObjectLIB/src/peakfindingGeometryElement.cxx
@@ -0,0 +1,328 @@
+//////////////////////////////////////////////////////////////////////
+/// (C)opyright 2004
+/// 
+/// Institute of Computer Science V
+/// Prof. Männer
+/// University of Mannheim, Germany
+/// 
+/// *******************************************************************
+/// 
+/// Designer(s):   Steinle
+/// 
+/// *******************************************************************
+/// 
+/// Project:     Trackfinder for CBM-Project at GSI-Darmstadt, Germany
+/// 
+/// *******************************************************************
+/// 
+/// Description:
+///
+///   class:
+///     - class for holding the information of an element in the
+///       automatic generated filter geometry
+///
+/// *******************************************************************
+///
+/// $Author: csteinle $
+/// $Date: 2008-10-10 13:47:05 $
+/// $Revision: 1.3 $
+///
+//////////////////////////////////////////////////////////////////////
+
+
+#include "../../MiscLIB/include/conversionRoutines.h"
+#include "../include/dataObjectError.h"
+#include "../include/peakfindingGeometryElement.h"
+
+
+#define sqr(a)  ((a) * (a)) 
+#define RADIX   10
+
+
+/****************************************************************
+ * Default constructor											*
+ ****************************************************************/
+
+peakfindingGeometryElement::peakfindingGeometryElement() {
+
+	for (unsigned short i = 0; i < DIMENSIONS; i++)
+		dim[i] = 0;
+	value      = 0;
+
+}
+
+/****************************************************************
+ * Constructor													*
+ ****************************************************************/
+
+peakfindingGeometryElement::peakfindingGeometryElement(const peakfindingGeometryElement& value) {
+
+	for (unsigned short i = 0; i < DIMENSIONS; i++)
+		this->dim[i] = value.dim[i];
+	this->value      = value.value;
+
+}
+peakfindingGeometryElement::peakfindingGeometryElement(short dim1, short dim2, short dim3, unsigned short value) {
+
+	set(dim1, DIM1);
+	set(dim2, DIM2);
+	set(dim3, DIM3);
+	setValue(value);
+
+}
+
+/****************************************************************
+ * Destructor													*
+ ****************************************************************/
+
+peakfindingGeometryElement::~peakfindingGeometryElement() {
+}
+
+/****************************************************************
+ * operator = ()												*
+ ****************************************************************/
+
+const peakfindingGeometryElement& peakfindingGeometryElement::operator = (const peakfindingGeometryElement& value) {
+
+	for (unsigned short i = 0; i < DIMENSIONS; i++)
+		this->dim[i] = value.dim[i];
+	this->value      = value.value;
+
+	return *this;
+
+}
+
+/****************************************************************
+ * returns the value in the dimension							*
+ ****************************************************************/
+
+short peakfindingGeometryElement::get(unsigned short dimension) {
+
+	short returnValue = 0;
+
+	if (dimension < DIMENSIONS)
+		returnValue = dim[dimension];
+	else
+		throw notExistingDimensionError(dimension, DIMENSIONS);
+
+	return returnValue;
+
+}
+std::string peakfindingGeometryElement::getString(unsigned short dimension) {
+
+	std::string returnValue;
+	char        buffer[shortConversionDigits+1];
+
+	if (dimension < DIMENSIONS) {
+		returnValue  = "000";
+		addRadix(RADIX, returnValue);
+		stos(dim[dimension], buffer, RADIX, shortConversionDigits);
+		returnValue += buffer;
+	}
+	else
+		throw notExistingDimensionError(dimension, DIMENSIONS);
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * get value													*
+ ****************************************************************/
+
+unsigned short peakfindingGeometryElement::getValue() {
+
+	return value;
+
+}
+std::string peakfindingGeometryElement::getValueString() {
+
+	std::string returnValue;
+	char        buffer[shortConversionDigits+1];
+
+	returnValue  = "000";
+	addRadix(RADIX, returnValue);
+	ustos(value, buffer, RADIX, shortConversionDigits);
+	returnValue += buffer;
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * sets the value in the dimension								*
+ ****************************************************************/
+
+void peakfindingGeometryElement::set(short set, unsigned short dimension) {
+
+	if (dimension < DIMENSIONS)
+		dim[dimension] = set;
+	else
+		throw notExistingDimensionError(dimension, DIMENSIONS);
+
+}
+void peakfindingGeometryElement::setString(std::string& set, unsigned short dimension) {
+
+	std::string temp;
+	int         radix;
+
+	temp           = set;
+	extractRadix(&radix, &temp);
+	dim[dimension] = stos(temp, radix);
+
+}
+
+/****************************************************************
+ * set value													*
+ ****************************************************************/
+
+void peakfindingGeometryElement::setValue(unsigned short value) {
+
+	this->value = value;
+
+}
+void peakfindingGeometryElement::setValueString(std::string& value) {
+
+	std::string temp;
+	int         radix;
+
+	temp         = value;
+	extractRadix(&radix, &temp);
+	this->value  = stous(temp, radix);
+
+}
+
+/****************************************************************
+ * increment value												*
+ ****************************************************************/
+
+void peakfindingGeometryElement::incrementValue(unsigned short increment) {
+
+	this->value += increment;
+
+}
+
+/****************************************************************
+ * This method compares to objects of this class. The result is	*
+ * - zero, if the object is equal to the element				*
+ * - else, if the object is not equal to the element			*
+ ****************************************************************/
+
+int peakfindingGeometryElement::compare(peakfindingGeometryElement& element) {
+
+	int returnValue;
+
+	returnValue = sqr(this->get(DIM3) - element.get(DIM3)) + sqr(this->get(DIM2) - element.get(DIM2)) + sqr(this->get(DIM1) - element.get(DIM1));
+
+	if ((this->get(DIM3) < element.get(DIM3)) || ((this->get(DIM3) == element.get(DIM3)) && (this->get(DIM2) < element.get(DIM2))) || ((this->get(DIM3) == element.get(DIM3)) && (this->get(DIM2) == element.get(DIM2)) && (this->get(DIM1) < element.get(DIM1))))
+		returnValue = - returnValue;
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * This method compares to objects of this class. The result is	*
+ * - true, if the object is bigger than the element				*
+ * - false, if the object is not bigger than the element		*
+ ****************************************************************/
+
+bool peakfindingGeometryElement::isBigger(peakfindingGeometryElement& element) {
+
+	bool returnValue;
+
+	if (compare(element) < 0)
+		returnValue = true;
+	else
+		returnValue = false;
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * returns a pointer to this object								*
+ ****************************************************************/
+
+peakfindingGeometryElement* peakfindingGeometryElement::getPointer() {
+
+	return this;
+
+}
+
+/****************************************************************
+ * returns a string representation for this object				*
+ ****************************************************************/
+
+std::string peakfindingGeometryElement::toString() {
+
+	std::string returnValue;
+	char        buffer[shortConversionDigits+1];
+
+	returnValue  = "dim1 = ";
+	stos(get(DIM1), buffer, 10, shortConversionDigits);
+	returnValue += buffer;
+	returnValue += ", dim2 = ";
+	stos(get(DIM2), buffer, 10, shortConversionDigits);
+	returnValue += buffer;
+	returnValue += ", dim3 = ";
+	stos(get(DIM3), buffer, 10, shortConversionDigits);
+	returnValue += buffer;
+	returnValue += " : value = ";
+	ustos(getValue(), buffer, 10, shortConversionDigits);
+	returnValue += buffer;
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * This method returns the size of the reserved memory for		*
+ * the source data.												*
+ ****************************************************************/
+
+double peakfindingGeometryElement::getReservedSizeOfData(unsigned short dimension) {
+
+	double returnValue;
+
+	returnValue  = sizeof(dim);
+
+	returnValue  = (returnValue / (1 << (10 * dimension)));
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * This method returns the size of the allocated memory for		*
+ * the source data.												*
+ ****************************************************************/
+
+double peakfindingGeometryElement::getAllocatedSizeOfData(unsigned short dimension) {
+
+	double returnValue;
+
+	returnValue  = 0;
+
+	returnValue  = (returnValue / (1 << (10 * dimension)));
+
+	return returnValue;
+
+}
+
+/****************************************************************
+ * This method returns the size of the used memory for			*
+ * the source data.												*
+ ****************************************************************/
+
+double peakfindingGeometryElement::getUsedSizeOfData(unsigned short dimension) {
+
+	double returnValue;
+
+	returnValue  = sizeof(dim);
+
+	returnValue  = (returnValue / (1 << (10 * dimension)));
+
+	return returnValue;
+
+}

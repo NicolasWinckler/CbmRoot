ecaaaa840deeb03428d2b5989bc548df697e1486
elena <elena@0381ead4-6506-0410-b988-94b70fbc4730>
2008-10-20 09:13:55 +0000

    New class for field symmetry 1/2
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@3840 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ field/CbmFieldMapSym1.cxx
@@ -0,0 +1,191 @@
+// -------------------------------------------------------------------------
+// -----                  CbmFieldMapSym1 source file                  -----
+// -----                 Created 22/09/08  by E.Litvinenko             -----
+// -------------------------------------------------------------------------
+#include "CbmFieldMapSym1.h"
+
+#include "CbmFieldPar.h"
+
+#include "TArrayF.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+// -------------   Default constructor  ----------------------------------
+CbmFieldMapSym1::CbmFieldMapSym1() { 
+  fType = 5;
+}
+// ------------------------------------------------------------------------
+
+
+
+// -------------   Standard constructor   ---------------------------------
+CbmFieldMapSym1::CbmFieldMapSym1(const char* mapName, 
+				 const char* fileType)
+  : CbmFieldMap(mapName, fileType) { 
+  fType = 5;
+}
+// ------------------------------------------------------------------------
+
+
+
+// ------------   Constructor from CbmFieldPar   --------------------------
+CbmFieldMapSym1::CbmFieldMapSym1(CbmFieldPar* fieldPar) 
+  : CbmFieldMap(fieldPar) {
+  fType = 5;
+}
+// ------------------------------------------------------------------------
+
+
+
+// ------------   Destructor   --------------------------------------------
+CbmFieldMapSym1::~CbmFieldMapSym1() { }
+// ------------------------------------------------------------------------
+
+
+
+// -----------   Get x component of the field   ---------------------------
+Double_t CbmFieldMapSym1::GetBx(Double_t x, Double_t y, Double_t z) {
+
+  Int_t ix    = 0;
+  Int_t iy    = 0;
+  Int_t iz    = 0;
+  Double_t dx = 0.;
+  Double_t dy = 0.;
+  Double_t dz = 0.;
+
+  if ( IsInside(x, y, z, ix, iy, iz, dx, dy, dz) ) {
+
+  // Get Bx field values at grid cell corners
+  fHa[0][0][0] = fBx->At(ix    *fNy*fNz + iy    *fNz + iz);
+  fHa[1][0][0] = fBx->At((ix+1)*fNy*fNz + iy    *fNz + iz);
+  fHa[0][1][0] = fBx->At(ix    *fNy*fNz + (iy+1)*fNz + iz);
+  fHa[1][1][0] = fBx->At((ix+1)*fNy*fNz + (iy+1)*fNz + iz);
+  fHa[0][0][1] = fBx->At(ix    *fNy*fNz + iy    *fNz + (iz+1));
+  fHa[1][0][1] = fBx->At((ix+1)*fNy*fNz + iy    *fNz + (iz+1));
+  fHa[0][1][1] = fBx->At(ix    *fNy*fNz + (iy+1)*fNz + (iz+1));
+  fHa[1][1][1] = fBx->At((ix+1)*fNy*fNz + (iy+1)*fNz + (iz+1));
+
+  // Return interpolated field value
+  return Interpolate(dx, dy, dz) * fHemiX ;
+
+  }
+
+  return 0.;
+}
+// ------------------------------------------------------------------------
+
+
+
+// -----------   Get y component of the field   ---------------------------
+Double_t CbmFieldMapSym1::GetBy(Double_t x, Double_t y, Double_t z) {
+
+  Int_t ix    = 0;
+  Int_t iy    = 0;
+  Int_t iz    = 0;
+  Double_t dx = 0.;
+  Double_t dy = 0.;
+  Double_t dz = 0.;
+
+  if ( IsInside(x, y, z, ix, iy, iz, dx, dy, dz) ) {
+
+  // Get By field values at grid cell corners
+  fHa[0][0][0] = fBy->At(ix    *fNy*fNz + iy    *fNz + iz);
+  fHa[1][0][0] = fBy->At((ix+1)*fNy*fNz + iy    *fNz + iz);
+  fHa[0][1][0] = fBy->At(ix    *fNy*fNz + (iy+1)*fNz + iz);
+  fHa[1][1][0] = fBy->At((ix+1)*fNy*fNz + (iy+1)*fNz + iz);
+  fHa[0][0][1] = fBy->At(ix    *fNy*fNz + iy    *fNz + (iz+1));
+  fHa[1][0][1] = fBy->At((ix+1)*fNy*fNz + iy    *fNz + (iz+1));
+  fHa[0][1][1] = fBy->At(ix    *fNy*fNz + (iy+1)*fNz + (iz+1));
+  fHa[1][1][1] = fBy->At((ix+1)*fNy*fNz + (iy+1)*fNz + (iz+1));
+
+  // Return interpolated field value
+  return Interpolate(dx, dy, dz);
+
+  }
+
+  return 0.;
+}
+// ------------------------------------------------------------------------
+
+
+
+// -----------   Get z component of the field   ---------------------------
+Double_t CbmFieldMapSym1::GetBz(Double_t x, Double_t y, Double_t z) {
+
+  Int_t ix    = 0;
+  Int_t iy    = 0;
+  Int_t iz    = 0;
+  Double_t dx = 0.;
+  Double_t dy = 0.;
+  Double_t dz = 0.;
+
+  if ( IsInside(x, y, z, ix, iy, iz, dx, dy, dz) ) {
+
+  // Get Bz field values at grid cell corners
+  fHa[0][0][0] = fBz->At(ix    *fNy*fNz + iy    *fNz + iz);
+  fHa[1][0][0] = fBz->At((ix+1)*fNy*fNz + iy    *fNz + iz);
+  fHa[0][1][0] = fBz->At(ix    *fNy*fNz + (iy+1)*fNz + iz);
+  fHa[1][1][0] = fBz->At((ix+1)*fNy*fNz + (iy+1)*fNz + iz);
+  fHa[0][0][1] = fBz->At(ix    *fNy*fNz + iy    *fNz + (iz+1));
+  fHa[1][0][1] = fBz->At((ix+1)*fNy*fNz + iy    *fNz + (iz+1));
+  fHa[0][1][1] = fBz->At(ix    *fNy*fNz + (iy+1)*fNz + (iz+1));
+  fHa[1][1][1] = fBz->At((ix+1)*fNy*fNz + (iy+1)*fNz + (iz+1));
+
+  // Return interpolated field value
+  return Interpolate(dx, dy, dz) ;
+  }
+
+  return 0.;
+}
+// ------------------------------------------------------------------------
+
+
+
+// -----------   Check whether a point is inside the map   ----------------
+Bool_t CbmFieldMapSym1::IsInside(Double_t x, Double_t y, Double_t z,
+				 Int_t& ix, Int_t& iy, Int_t& iz,
+				 Double_t& dx, Double_t& dy, 
+				 Double_t& dz) {
+
+  // --- Transform into local coordinate system
+  Double_t xl = x - fPosX;
+  Double_t yl = y - fPosY;
+  Double_t zl = z - fPosZ;
+
+  // ---  Reflect w.r.t. symmetry axes
+  fHemiX = 1.;
+  if ( xl < 0. ) {
+    fHemiX = -1.;
+    xl     = -1. * xl;
+  }
+
+  // ---  Check for being outside the map range
+  if ( ! ( xl >= fXmin && xl < fXmax && yl >= fYmin && yl < fYmax &&
+	   zl >= fZmin && zl < fZmax ) ) {
+    ix = iy = iz = 0;
+    dx = dy = dz = 0.;
+    return kFALSE;
+  }
+ 
+  // --- Determine grid cell
+  ix = Int_t( (xl-fXmin) / fXstep );
+  iy = Int_t( (yl-fYmin) / fYstep );
+  iz = Int_t( (zl-fZmin) / fZstep );
+
+
+  // Relative distance from grid point (in units of cell size)
+  dx = (xl-fXmin) / fXstep - Double_t(ix);
+  dy = (yl-fYmin) / fYstep - Double_t(iy);
+  dz = (zl-fZmin) / fZstep - Double_t(iz);
+
+  return kTRUE;
+
+}
+// ------------------------------------------------------------------------
+
+
+ClassImp(CbmFieldMapSym1)

b98f41b007f57e639687f732a65d2a6c1d222301
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2008-11-25 10:55:00 +0000

    small refactoring
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@4034 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/rich/draw_qa.C
+++ macro/rich/draw_qa.C
@@ -5,20 +5,41 @@
     E-mail : S.Lebedev@gsi.de
 */
 
+TH1D* divide_hist(TH1D* h1, TH1D* h2, char* histTitle, Int_t nofBins, Double_t min, Double_t max)
+{
+    h1->SetBins(nofBins, min, max);
+    h2->SetBins(nofBins, min, max);       
+	h1->Sumw2();
+	h2->Sumw2();
+	TH1D* h3 = new TH1D("h3",histTitle ,nofBins, min, max);
+	h3->Divide(h1,h2,1,1,"B");
+	return h3;
+}
+
+void draw_diff_true_fake(TH1D* h1, TH1D* h2, char* xTitle)
+{
+	if (h1->GetMaximum() < h2->GetMaximum()) h1->SetMaximum(h2->GetMaximum()*1.05);
+	
+	h1->SetLineWidth(2.);
+	h2->SetLineWidth(2.);
+	h1->SetLineStyle(9);
+	h2->SetLineStyle(2);
+    h1->GetXaxis()->SetTitle(xTitle);
+    h1->GetYaxis()->SetTitle("Entries");  
+	
+	h1->Draw();
+	h2->Draw("same");
+}
+
 void draw_qa(){
-	//TFile *file = new TFile("/d/cbm02/slebedev/ebelolap/compact/urqmd.reco.rich.40.root");
-	TFile *file = new TFile("/d/cbm02/slebedev/rich/MAY08/electronId/urqmd.0000.recorich.root");
+	TFile *file = new TFile("/d/cbm02/slebedev/ebelolap/compact/urqmd.reco.rich.40.root");
+	//TFile *file = new TFile("/d/cbm02/slebedev/rich/MAY08/electronId/urqmd.0000.recorich.root");
 	
 	TDirectory *current = gDirectory;
 	current->cd("RichRingQaHist");
    // rich->cd();
-   gROOT->SetStyle("Plain");
-    gStyle->SetPalette(1,0);   
-   // gStyle->SetOptStat(0000);
-    Double_t minMom = 0;
-    Double_t maxMom = 15;
-    Double_t nofBins = 30;
-
+    gROOT->SetStyle("Plain");
+    gStyle->SetPalette(1,0);
 		
 	gStyle->SetHistLineWidth(3);
 	gROOT->ForceStyle();
@@ -28,103 +49,37 @@ void draw_qa(){
     TCanvas *c1 = new TCanvas("CbmRichQa1","c1",1200,1000);
     c1->Divide(4,4);
     c1->cd(1);
-    fh_FakeNofHits->SetLineStyle(2);
-    fh_TrueElNofHits->SetLineWidth(2);
-    fh_TrueElNofHits->SetLineStyle(9);
-    fh_TrueElNofHits->GetXaxis()->SetTitle("Number of Hits");
-    fh_TrueElNofHits->GetYaxis()->SetTitle("Entries");  
-    fh_TrueElNofHits->Draw();
-    fh_FakeNofHits->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElNofHits, fh_FakeNofHits, "Number of Hits");
     
     c1->cd(2);
-    fh_FakeDistance->SetLineStyle(2);
-    fh_TrueElDistance->SetLineWidth(2);
-    fh_TrueElDistance->SetLineStyle(9);
-    fh_TrueElDistance->GetXaxis()->SetTitle("ring-track distance, [cm]");
-    fh_TrueElDistance->GetYaxis()->SetTitle("Entries"); 
-    fh_TrueElDistance->Draw();  
-    fh_FakeDistance->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElDistance, fh_FakeDistance, "ring-track distance, [cm]");
     
     c1->cd(3);  
-    fh_FakeAngle->SetLineStyle(2);
-    fh_TrueElAngle->SetLineWidth(2);
-    fh_TrueElAngle->SetLineStyle(9);
-    fh_TrueElAngle->GetXaxis()->SetTitle("Angle, [rad]");
-    fh_TrueElAngle->GetYaxis()->SetTitle("Entries");            
-    fh_TrueElAngle->Draw(); 
-    fh_FakeAngle->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElAngle, fh_FakeAngle, "Angle, [rad]");
     
     c1->cd(4);
-    fh_FakeNofHitsOnRing->SetLineStyle(2);
-    fh_TrueElNofHitsOnRing->SetLineWidth(2);
-    fh_TrueElNofHitsOnRing->SetLineStyle(9);
-    fh_TrueElNofHitsOnRing->GetXaxis()->SetTitle("# hits on ring");
-    fh_TrueElNofHitsOnRing->GetYaxis()->SetTitle("Entries");            
-    fh_TrueElNofHitsOnRing->Draw();
-    fh_FakeNofHitsOnRing->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElNofHitsOnRing, fh_FakeNofHitsOnRing,"# hits on ring");   
     
-    c1->cd(5);  
-    fh_FakeRadPos->SetLineStyle(2);
-    fh_TrueElRadPos->SetLineWidth(2);
-    fh_TrueElRadPos->SetLineStyle(9);   
-    fh_TrueElRadPos->GetXaxis()->SetTitle("Radial posotion, [cm]");
-    fh_TrueElRadPos->GetYaxis()->SetTitle("Entries");   
-    fh_TrueElRadPos->Draw();    
-    fh_FakeRadPos->Draw("SAME");
+    c1->cd(5); 
+    draw_diff_true_fake(fh_TrueElRadPos, fh_FakeRadPos, "Radial posotion, [cm]");    
     
     c1->cd(6);  
-    fh_FakeChi2->SetLineStyle(2);
-    fh_TrueElChi2->SetLineWidth(2);
-    fh_TrueElChi2->SetLineStyle(9);     
-    fh_TrueElChi2->GetXaxis()->SetTitle("chi2");
-    fh_TrueElChi2->GetYaxis()->SetTitle("Entries"); 
-    fh_TrueElChi2->Draw();  
-    fh_FakeChi2->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElChi2, fh_FakeChi2, "chi2");
 
     c1->cd(7);    
-    fh_FakeRadius->SetLineStyle(2);
-    fh_TrueElRadius->SetLineWidth(2);
-    fh_TrueElRadius->SetLineStyle(9);    
-    fh_TrueElRadius->GetXaxis()->SetTitle("radius, [cm]");
-    fh_TrueElRadius->GetYaxis()->SetTitle("Entries");        
-    fh_TrueElRadius->Draw();  
-    fh_FakeRadius->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElRadius, fh_FakeRadius, "radius, [cm]");
     
     c1->cd(8);  
-    fh_FakeA->SetLineStyle(2);
-    fh_TrueElA->SetLineWidth(2);
-    fh_TrueElA->SetLineStyle(9);   
-    fh_TrueElA->GetXaxis()->SetTitle("A axis, [cm]");
-    fh_TrueElA->GetYaxis()->SetTitle("Entries");   
-    fh_TrueElA->Draw();    
-    fh_FakeA->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElA, fh_FakeA, "A axis, [cm]");
     
     c1->cd(9);  
-    fh_FakeB->SetLineStyle(2);
-    fh_TrueElB->SetLineWidth(2);
-    fh_TrueElB->SetLineStyle(9);     
-    fh_TrueElB->GetXaxis()->SetTitle("B axis, [cm]");
-    fh_TrueElB->GetYaxis()->SetTitle("Entries"); 
-    fh_TrueElB->Draw();  
-    fh_FakeB->Draw("SAME");
+    draw_diff_true_fake(fh_TrueElB, fh_FakeB, "B axis, [cm]");
 
-    c1->cd(10);    
-    fh_FakePhi->SetLineStyle(2);
-    fh_TrueElPhi->SetLineWidth(2);
-    fh_TrueElPhi->SetLineStyle(9);       
-    fh_TrueElPhi->GetXaxis()->SetTitle("phi, [rad]");
-    fh_TrueElPhi->GetYaxis()->SetTitle("Entries");     
-    fh_TrueElPhi->Draw();  
-    fh_FakePhi->Draw("SAME"); 
+    c1->cd(10);   
+    draw_diff_true_fake(fh_TrueElPhi, fh_FakePhi, "Entries");
 
     c1->cd(11);    
-    fh_FakeStsMom->SetLineStyle(2);
-    fh_TrueElStsMom->SetLineWidth(2);
-    fh_TrueElStsMom->SetLineStyle(9);    
-    fh_TrueElStsMom->GetXaxis()->SetTitle("mom, [Gev/c]");
-    fh_TrueElStsMom->GetYaxis()->SetTitle("Entries");         
-    fh_TrueElStsMom->Draw();  
-    fh_FakeStsMom->Draw("SAME"); 
+    draw_diff_true_fake(fh_TrueElStsMom, fh_FakeStsMom, "mom, [Gev/c]");
             
     c1->cd(12);  
     fh_TrueElPhiVsRadAngle->GetXaxis()->SetTitle("phi, [rad]");
@@ -136,7 +91,7 @@ void draw_qa(){
     fh_FakePhiVsRadAngle->GetYaxis()->SetTitle("rad angle, [rad]");               
     fh_FakePhiVsRadAngle->Draw("COLZ");
     
-     c1->cd(14);         
+    c1->cd(14);         
     fh_TrueElRadiusVsMom->Draw("COLZ");
     
     c1->cd(15);         
@@ -144,49 +99,32 @@ void draw_qa(){
 
                
 	TCanvas *c3 = new TCanvas("CbmRichQa3","c3",1200,1000);
-    fh_TrueFoundElRingsProjHitCutMom->SetBins(nofBins,minMom, maxMom);
-    fh_MCElRingsProjHitCutMom->SetBins(nofBins,minMom, maxMom);     
-	fh_TrueFoundElRingsProjHitCutMom->Sumw2();
-	fh_MCElRingsProjHitCutMom->Sumw2();	
-	TH1D* th1 = new TH1D("th1","Efficiency, electrons vs momentum;momentum, GeV;efficiency",nofBins,minMom, maxMom);
-	th1->Divide(fh_TrueFoundElRingsProjHitCutMom,fh_MCElRingsProjHitCutMom,1,1,"B");
+	TH1D* th1 = divide_hist(fh_TrueFoundElRingsProjHitCutMom, fh_MCElRingsProjHitCutMom, 
+			"Efficiency, electrons vs momentum;momentum, GeV;efficiency, %", 30, 0, 15);
 	th1->Draw();
 	
 	
 	TCanvas *c4 = new TCanvas("CbmRichQa4","c4",1200,1000);
-    fh_TrueFoundElRingsProjHitCutRadPos->SetBins(20,0,150);
-    fh_MCElRingsProjHitCutRadPos->SetBins(20,0,150);    
-	fh_TrueFoundElRingsProjHitCutRadPos->Sumw2();
-	fh_MCElRingsProjHitCutRadPos->Sumw2();
-	TH1D* th2 = new TH1D("th1","Efficiency, electrons vs RadialPosition;radial position, cm;efficiency",20,0,150);
-	th2->Divide(fh_TrueFoundElRingsProjHitCutRadPos,fh_MCElRingsProjHitCutRadPos,1,1,"B");
+	TH1D* th2 = divide_hist(fh_TrueFoundElRingsProjHitCutRadPos, fh_MCElRingsProjHitCutRadPos, 
+			"Efficiency, electrons vs RadialPosition;radial position, cm;efficiency, %", 20, 0, 150);
 	th2->Draw();
     
     TCanvas *c5 = new TCanvas("CbmRichQa5","c5",1200,1000);
-    fh_TrueMatchElMom->SetBins(20,0,10);    
-    fh_TrueMatchElMom->Sumw2();
-    //fh_MCElRingsProjHitCutRadPos->Sumw2();
-    TH1D* th3 = new TH1D("th3","Efficiency, ring-track matching vs momentum;momentum, GeV;efficiency",nofBins,minMom, maxMom);
-    th3->Divide(fh_TrueMatchElMom,fh_TrueFoundElRingsProjHitCutMom,1,1,"B");
+	TH1D* th3 = divide_hist(fh_TrueMatchElMom, fh_TrueFoundElRingsProjHitCutMom, 
+			"Efficiency, ring-track matching vs momentum;momentum, GeV;efficiency, %", 30, 0, 15);
     th3->Draw();   
 	
     TCanvas *c6 = new TCanvas("CbmRichQa6","c6",1200,1000);    
     fh_StartVertexXZ->Draw("COLZ");
      
     TCanvas *c7 = new TCanvas("CbmRichQa7","c7",1200,1000); 
-    fh_TrueFoundElRingsProjHitCutNofHits->Sumw2();
-    fh_MCElRingsProjHitCutNofHits->Sumw2();
-	TH1D* thNofHits = new TH1D("thNofHits","Efficiency, electrons vs NofHits;Nof Hits; efficiency, %",20,0,40);
-	thNofHits->Divide(fh_TrueFoundElRingsProjHitCutNofHits,fh_MCElRingsProjHitCutNofHits,1,1,"B");
+	TH1D* thNofHits = divide_hist(fh_TrueFoundElRingsProjHitCutNofHits, fh_MCElRingsProjHitCutNofHits, 
+			"Efficiency, electrons vs NofHits;Nof Hits; efficiency, %", 20, 0, 40);
 	thNofHits->Draw();
-	thNofHits->Draw();
-	
 	
     TCanvas *c8 = new TCanvas("CbmRichQa8","c8",1200,1000); 
-    fh_MCElRingsProjHitCutBoverA->Sumw2();
-    fh_TrueFoundElRingsProjHitCutBoverA->Sumw2();
-	TH1D* thBoverA = new TH1D("BoverA","Efficiency, electrons vs B/A;B/A; efficiency, %",20,0,1);
-	thBoverA->Divide(fh_TrueFoundElRingsProjHitCutBoverA,fh_MCElRingsProjHitCutBoverA,1,1,"B");
+	TH1D* thBoverA = divide_hist(fh_TrueFoundElRingsProjHitCutBoverA, fh_MCElRingsProjHitCutBoverA, 
+			"Efficiency, electrons vs B/A;B/A; efficiency, %", 20, 0, 1);
 	thBoverA->Draw();
 
     TCanvas *c9 = new TCanvas("CbmRichQa9","c9",1200,1000); 

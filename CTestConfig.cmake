5bcd2c304f0c0e59e04b5b9ec96175553352fa98
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2008-12-22 06:32:57 +0000

    Add configuration file for CTest. Replacement for the old DartConfig.cmake file.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@4234 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ CTestConfig.cmake
@@ -0,0 +1,17 @@
+# Dashboard is opened for submissions for a 24 hour period starting at
+# the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
+set(CTEST_PROJECT_NAME "CbmRoot")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
+
+#SET (CTEST_DROP_METHOD "http")
+#SET (CTEST_DROP_SITE "fairroot.gsi.de")
+#SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
+#SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/TriggerSiteDart2AndCDash.cgi")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "fairroot.gsi.de")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=CbmRoot")
+set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_TRIGGER_SITE "")
+
+set(CTEST_TESTING_TIMEOUT 60)

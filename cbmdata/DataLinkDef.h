ac41076ffe85ce91dc83a97acd0889d10597911e
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2009-02-12 15:04:53 +0000

    Add CMakLists.txt and LinkDef.h file for the new CbmData library
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@4614 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ cbmdata/DataLinkDef.h
@@ -0,0 +1,58 @@
+// $Id: EcalLinkDef.h,v 1.12 2006/09/18 07:58:04 prokudin Exp $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class CbmEcalPoint+;
+#pragma link C++ class CbmEcalHit+;
+#pragma link C++ class CbmEcalHitFastMC+;
+#pragma link C++ class CbmEcalPointLite+;
+#pragma link C++ class CbmEcalCluster+;
+
+#pragma link C++ class CbmMuchCluster+;
+#pragma link C++ class CbmMuchHit+;
+#pragma link C++ class CbmMuchPoint+;
+#pragma link C++ class CbmMuchDigi+;
+#pragma link C++ class CbmMuchDigiMatch+;
+#pragma link C++ class CbmMuchTrack+;
+#pragma link C++ class CbmMuchTrackMatch+;
+
+#pragma link C++ class CbmMvdHit+;
+#pragma link C++ class CbmMvdHitMatch+;
+#pragma link C++ class CbmMvdPoint+;
+#pragma link C++ class CbmMvdDigi+;
+#pragma link C++ class CbmMvdCluster+;
+
+#pragma link C++ class CbmRichPoint+;
+#pragma link C++ class CbmRichHit+;
+#pragma link C++ class CbmRichRing+;
+#pragma link C++ class CbmRichRingMatch+;
+
+#pragma link C++ class CbmStsCluster+;
+#pragma link C++ class CbmStsDigi+;
+#pragma link C++ class CbmStsDigiMatch+;
+#pragma link C++ class CbmStsHit+;
+#pragma link C++ class CbmStsPoint+;
+#pragma link C++ class CbmStsTrack+;
+#pragma link C++ class CbmStsTrackMatch+;
+
+#pragma link C++ class CbmSttPoint+;
+#pragma link C++ class CbmSttHit+;
+#pragma link C++ class CbmSttTrack+;
+#pragma link C++ class CbmSttTrackMatch+;
+
+#pragma link C++ class CbmTofHit+;
+#pragma link C++ class CbmTofPoint+;
+
+#pragma link C++ class CbmTrdHit+;
+#pragma link C++ class CbmTrdPoint+;
+#pragma link C++ class CbmTrdTrack+;
+#pragma link C++ class CbmTrdTrackMatch+;
+
+#pragma link C++ class  CbmZdcPoint+;
+#pragma link C++ class  CbmZdcEvent+;
+
+#endif

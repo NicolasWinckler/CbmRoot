94b0488d6cdcb5a1c8d56efd657ee8520f75bafa
friese <friese@0381ead4-6506-0410-b988-94b70fbc4730>
2009-04-21 12:31:38 +0000

    Update from M. Prokudin
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@5322 0381ead4-6506-0410-b988-94b70fbc4730

--- KF/Interface/CbmEcalTrackExtrapolationKF.cxx
+++ KF/Interface/CbmEcalTrackExtrapolationKF.cxx
@@ -1,31 +1,3 @@
-/******************************************************************************
-*  $Id: CbmEcalTrackExtrapolationKF.cxx,v 1.2 2006/06/22 09:36:37 hoehne Exp $
-*
-*  Class  : CbmEcalTrackExtrapolationKF
-*  Description: This is the implementation of the TrackExtrapolation from MC points.
-*               It reads the PointArray with ImPlanePoints from MC and selects those to
-*               be projected to the Rich Photodetector
-*
-*  Author : Claudia Hoehne
-*  E-mail : c.hoehne@gsi.de
-*
-*******************************************************************************
-*  $Log: CbmEcalTrackExtrapolationKF.cxx,v $
-*  Revision 1.2  2006/06/22 09:36:37  hoehne
-*  check if idSTS > 0 (non-STS tracks in global track get index -1)
-*
-*  Revision 1.1  2006/02/17 23:13:37  sgorboun
-*  Update of KF&L1 classes
-*
-*  Revision 1.2  2006/01/30 11:00:55  hoehne
-*  bug fix: Point Array at z-plane was not filled correctly (counters not correct)
-*
-*  Revision 1.1  2006/01/26 09:53:22  hoehne
-*  initial version for track extrapolation (base class + concrete implementations + task) to z-plane in RICH
-*
-*
-*
-*******************************************************************************/
 
 #include <iostream>
 
@@ -77,20 +49,16 @@ CbmEcalTrackExtrapolationKF::~CbmEcalTrackExtrapolationKF()
 // -----   Public method Init   --------------------------------------------
 void CbmEcalTrackExtrapolationKF::Init()
 {
+  CbmEcalTrackExtrapolation::Init();
   //Get and check FairRootManager
   FairRootManager* ioman=FairRootManager::Instance();
   if (!ioman)
-  {
-    cout << "-E- CbmEcalTrackExtrapolationKF::Init: " << "RootManager not instantised!" << endl;
     Fatal("CbmEcalTrackExtrapolationKF::Init()", "Can't instantise Rootmanager");
-  }
 
   //Get STS track array
   fSTSArray=(TClonesArray*)ioman->GetObject("STSTrack");
   if (!fSTSArray)
     cout << "-W- CbmEcalTrackExtrapolationKF::Init: No sts track array!" << endl;
-
-
 }
 // -------------------------------------------------------------------------
 
@@ -120,6 +88,7 @@ Int_t CbmEcalTrackExtrapolationKF::DoExtrapolate(TClonesArray *gTrackArray, TClo
   FairTrackParam trpar;
   for(;i<n;i++)
   {
+    Map()[nTr]=-1111;
     tr=(CbmGlobalTrack*)gTrackArray->At(i);
     if (!tr) continue;
     if (tr->GetStsTrackIndex()<0||tr->GetTrdTrackIndex()<0)
@@ -127,6 +96,7 @@ Int_t CbmEcalTrackExtrapolationKF::DoExtrapolate(TClonesArray *gTrackArray, TClo
     kfTr.SetTrackParam(*(tr->GetParamLast()));
     kfTr.Extrapolate(Str()->GetEcalInf()->GetZPos());
     kfTr.GetTrackParam(trpar);
+    Map()[nTr]=i;
     new ((*fTrackParamArray)[nTr++]) FairTrackParam(trpar);
   }
   

1beb22b23c3e4ddde5cb6262dac8d1cb4896b545
mryzhins <mryzhins@0381ead4-6506-0410-b988-94b70fbc4730>
2009-04-27 09:06:15 +0000

    Added sample macro for manual segmentation much_seg_manual.C
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@5363 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/much/much_seg_manual.C
@@ -0,0 +1,124 @@
+// --------------------------------------------------------------------------
+//
+// Macro for segmentation of MUCH stations
+//
+// To be defined as arguments:
+//    MC transport file
+//    Output digitization file ("*.root")
+//    Number of events to be processed (default = 2)
+//    First event to be processed (default = 0)
+//
+// M.Ryzhinskiy
+//
+// --------------------------------------------------------------------------
+
+void much_seg(const char* mcFile = "data/mc.root",
+              const char* digiFile = "data/digi.root",
+	          Int_t nEvents = 100)
+{
+  // ========================================================================
+  //          Adjust this part according to your requirements
+
+  if (mcFile == "") {
+    mcFile = "data/Jpsi.auau.25gev.centr.mc.root";
+  }
+  if (digiFile == "") {
+    digiFile = "data/much_digi.root";
+  }
+
+  // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
+  Int_t iVerbose = 0;
+
+  // Dummy ROOT file (neede as an output)
+  TString outFile  = "data/dummy.root";
+
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+  basiclibs();
+  gSystem->Load("libGeoBase");
+  gSystem->Load("libParBase");
+  gSystem->Load("libBase");
+  gSystem->Load("libCbmBase");
+  gSystem->Load("libCbmData");
+  gSystem->Load("libField");
+  gSystem->Load("libGen");
+  gSystem->Load("libPassive");
+//  gSystem->Load("libMvd");
+  gSystem->Load("libSts");
+  gSystem->Load("libRich");
+  gSystem->Load("libTrd");
+  gSystem->Load("libTof");
+  gSystem->Load("libEcal");
+  gSystem->Load("libGlobal");
+  gSystem->Load("libKF");
+  gSystem->Load("libL1");
+  gSystem->Load("libMuch");
+  // ------------------------------------------------------------------------
+
+  // -----   Analysis run   -------------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+  fRun->SetInputFile(mcFile);
+  fRun->SetOutputFile(outFile);
+  // ------------------------------------------------------------------------
+
+  // -----  Parameter database   --------------------------------------------
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open(gFile);
+  rtdb->setFirstInput(parIo1);
+  rtdb->setOutput(parIo1);
+  rtdb->saveOutput();
+  fRun->LoadGeometry();
+  // ------------------------------------------------------------------------
+
+
+  // -----  Segmentation task  ----------------------------------------------
+  CbmMuchSegmentManual* seg = new CbmMuchSegmentManual(digiFile);
+  // Number of stations
+  seg->SetNStations(6);
+
+  // Number of channels for each station
+  Int_t nChannels[] = {128, 128, 128, 128, 128, 128};
+  seg->SetNChannels(nChannels);
+
+  // Number of regions for each station
+  Int_t nRegions[] = {5, 3, 1, 1, 1, 1};
+  seg->SetNRegions(nRegions);
+
+  // Set region radii for each station
+  Double_t st0_rad[] = {13.99, 19.39, 24.41, 31.51, 64.76};
+  Double_t st1_rad[] = {22, 24, 90.95};
+  Double_t st2_rad[] = {110.95};
+  Double_t st3_rad[] = {135.95};
+  Double_t st4_rad[] = {163.45};
+  Double_t st5_rad[] = {228.45};
+  seg->SetRegionRadii(0, st0_rad);
+  seg->SetRegionRadii(1, st1_rad);
+  seg->SetRegionRadii(2, st2_rad);
+  seg->SetRegionRadii(3, st3_rad);
+  seg->SetRegionRadii(4, st4_rad);
+  seg->SetRegionRadii(5, st5_rad);
+
+  // Set minimum pad size/resolution [cm] in the center region for each station
+  Double_t padLx[] = {0.1386, 0.4, 0.8, 0.8 ,0.8, 0.8};
+  Double_t padLy[] = {0.1386, 0.4, 0.8, 0.8 ,0.8, 0.8};
+  seg->SetMinPadLx(padLx);
+  seg->SetMinPadLy(padLy);
+
+  // Set maximum allowed pad size/resolution [cm] for each station
+  Double_t sigmaX[] = {0.32, 0.32, 0.32, 0.32, 0.32, 0.32};
+  Double_t sigmaY[] = {0.32, 0.32, 0.32, 0.32, 0.32, 0.32};
+  seg->SetMaxSigmaX(sigmaX);
+  seg->SetMaxSigmaY(sigmaY);
+
+  fRun->AddTask(seg);
+  // ------------------------------------------------------------------------
+
+  // Run segmentation
+  fRun->Init();
+  //fRun->Run(0,nEvents);
+
+  cout << " Test passed" << endl;
+  cout << " All ok " << endl;
+  exit(0);
+}

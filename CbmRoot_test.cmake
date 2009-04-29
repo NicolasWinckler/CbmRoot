edb62c2092d7b2664c3059869c718816f7602702
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2009-04-29 15:14:01 +0000

    Changes concerning the automatic testing procedures. The Dart.sh script now reads the configuration of the machine (pathes etc) from a config file. If there are conflicting or localy modified files these files are also send to the dashboard. This feature helps to understand changes made on different sites if there is no admin available to get the info from.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@5381 0381ead4-6506-0410-b988-94b70fbc4730

--- CbmRoot_test.cmake
+++ CbmRoot_test.cmake
@@ -10,7 +10,29 @@ SET (CTEST_UPDATE_CHECKOUT  " ${CTEST_UPDATE_COMMAND} update ")
 SET (BUILD_COMMAND "make")
 SET (CTEST_BUILD_COMMAND "${BUILD_COMMAND} -j$ENV{number_of_processors}")
 
-CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
+#if($ENV{ctest_model} MATCHES Nightly)
+  # get the information about conflicting or localy modified files
+  # from svn, extract the relavant information about the file name
+  # and put the result in the output variable
+  execute_process(COMMAND svn stat -u  
+                  COMMAND grep ^[CM]
+                  COMMAND cut -c21- 
+                  OUTPUT_VARIABLE FILELIST
+                  )
+
+  # create out of the output a cmake list. This step is done to convert the
+  # stream into seperated filenames.
+  # The trick is to exchange an "\n" by an ";" which is the separartor in
+  # a list created by cmake 
+  STRING(REGEX REPLACE "\n" ";" _result "${FILELIST}")
+
+  FOREACH(_file ${_result})
+    SET (CTEST_NOTES_FILES ${CTEST_NOTES_FILES} "${CTEST_SOURCE_DIRECTORY}/${_file}")
+  ENDFOREACH(_file ${_result})
+
+  CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
+
+#endif($ENV{ctest_model} MATCHES Nightly)
 
 CTEST_START ($ENV{ctest_model})
 CTEST_UPDATE (SOURCE "${CTEST_SOURCE_DIRECTORY}")

b337f8f8d76ad0200d716729960e7a2fd56a74bd
friese <friese@0381ead4-6506-0410-b988-94b70fbc4730>
2009-05-11 17:59:09 +0000

    CbmDigi moved to cbmdata directory. New class CbmMCEvent
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@5482 0381ead4-6506-0410-b988-94b70fbc4730

--- cbmdata/DataLinkDef.h
+++ cbmdata/DataLinkDef.h
@@ -3,8 +3,10 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
- 
+
+#pragma link C++ class CbmDigi+;
 #pragma link C++ class CbmHit+;
+#pragma link C++ class CbmMCEvent+;
 #pragma link C++ class CbmMCTrack+;
 #pragma link C++ class CbmTrack+;
 #pragma link C++ class CbmTrackMatch+;

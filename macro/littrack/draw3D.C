3b7665e90d23c511260b1b9d17dc2b944c3f30a8
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2009-05-12 13:54:44 +0000

    macro renamed
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@5496 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/draw3D.C
@@ -0,0 +1,24 @@
+void draw3D()
+{
+  TString dir = "/home/d/andrey/test/trunk/global_mu/";
+  TString mcFile = dir + "mc.0000.root";
+
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+  basiclibs();
+  gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/cbmrootlibs.C");
+  cbmrootlibs();
+
+  TFile* f = new TFile(mcFile);
+  TGeoManager *geoMan = (TGeoManager*) f->Get("FAIRGeom");
+  geoMan->SetVisLevel(0);
+
+  // Check overlaps
+  geoMan->CheckOverlaps(0.0000001);
+  geoMan->PrintOverlaps();
+
+  TGeoVolume* master = geoMan->GetMasterVolume();
+  master->Draw("ogl");
+
+//  TGeoVolume* much = master->FindNode("much_0")->GetVolume();
+//  much->Draw("ogl");
+}

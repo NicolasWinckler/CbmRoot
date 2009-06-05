c55827d5b1162a1ec15b6f21c5be9e1ac4566913
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2009-06-05 13:25:27 +0000

    many corrections
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@5760 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/trd_elid_trainANN_txt.C
+++ macro/trd/trd_elid_trainANN_txt.C
@@ -7,6 +7,18 @@
  */
 #include <vector>
 
+	Int_t fNofLayers = 12;
+	TString mlpString = "x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12:12:x13";
+	TString mlpWeightsFile = "Neural_Net_Weights.txt";
+	///vector to store and transform data from file
+	std::vector<Float_t> inVector;
+	Float_t x13;///output value
+	Double_t ANNCoef1 = 1.06;
+	Double_t ANNCoef2 = 0.57;
+	Bool_t doTraining = true;
+	//cut on the output value of ANN
+	Double_t ANNCut = 0.98;
+
 void coeffCalc(double mom, double* coeff1, double* coeff2) {
 	double momAr[] = { 1., 1.5, 2., 3., 4., 5., 7., 9., 11., 13. };
 	double coeffAr1[] = { 1.04, 1.105, 1.154, 1.277, 1.333, 1.394, 1.47, 1.50,
@@ -22,8 +34,23 @@ void coeffCalc(double mom, double* coeff1, double* coeff2) {
 	}
 }
 
+void transform1()
+{
+	for (Int_t j = 0; j<inVector.size(); j++) {
+		inVector[j]=inVector[j]*1e6;
+		inVector[j]=(inVector[j]-ANNCoef1)/ANNCoef2 -0.225;
+	}
+	sort(inVector.begin(), inVector.end());
+	for (Int_t j = 0; j<inVector.size(); j++)
+		inVector[j]=TMath::LandauI(inVector[j]);
+}
+
 void trd_elid_trainANN_txt() {
-	TString fileNum = "0001";
+	///load libraries for neural net
+	if (!gROOT->GetClass("TMultiLayerPerceptron"))
+		gSystem->Load("libMLP");
+
+	TString fileNum = "0002";
 	TString geoType = "st";
 	TString inputDir = "/d/cbm02/slebedev/trd/JUN09/"+geoType+"/";
 	TString fileNameEl = inputDir + geoType+"_"+"electrons_mom_"+fileNum+".txt";
@@ -31,21 +58,16 @@ void trd_elid_trainANN_txt() {
 	TString fileNameTestEl = inputDir + geoType+"_"+"electrons_mom_"+fileNum+".txt";
 	TString fileNameTestPi = inputDir + geoType+"_"+"pions_mom_"+fileNum+".txt";
 
+//	TString fileNameEl = "/d/cbm02/slebedev/trd/MAY08/electrons_mom_8.txt";
+//	TString fileNamePi = "/d/cbm02/slebedev/trd/MAY08/pions_mom_8.txt";
+//	TString fileNameTestEl = "/d/cbm02/slebedev/trd/MAY08/electrons_mom_8.txt";
+//	TString fileNameTestPi = "/d/cbm02/slebedev/trd/MAY08/pions_mom_8.txt";
 
 	//input files for electrons and pions
-	//these files could be produced with pions.C macro
 	std::ifstream finEl((const char*) fileNameEl);
 	std::ifstream finPi((const char*) fileNamePi);
 
-	///load libraries for neural net
-	if (!gROOT->GetClass("TMultiLayerPerceptron"))
-		gSystem->Load("libMLP");
-
-	Int_t fNofLayers = 6;
-	///vector to store and transform data from file
-	std::vector<Float_t> inVector;
 	inVector.resize(fNofLayers);///12 input elements
-	Float_t x13;///output value
 
 	///init TTree as input data to neural net
 	TTree *simu = new TTree ("MonteCarlo","MontecarloData");
@@ -55,89 +77,73 @@ void trd_elid_trainANN_txt() {
 	simu->Branch("x4", &inVector[3], "x4/F");
 	simu->Branch("x5", &inVector[4], "x5/F");
 	simu->Branch("x6", &inVector[5], "x6/F");
-	//simu->Branch("x7", &inVector[6], "x7/F");
-	//simu->Branch("x8", &inVector[7], "x8/F");
-	//simu->Branch("x9", &inVector[8], "x9/F");
-	//simu->Branch("x10", &inVector[9], "x10/F");
-	//simu->Branch("x11", &inVector[10], "x11/F");
-	//simu->Branch("x12", &inVector[11], "x12/F");
+	simu->Branch("x7", &inVector[6], "x7/F");
+	simu->Branch("x8", &inVector[7], "x8/F");
+	simu->Branch("x9", &inVector[8], "x9/F");
+	simu->Branch("x10", &inVector[9], "x10/F");
+	simu->Branch("x11", &inVector[10], "x11/F");
+	simu->Branch("x12", &inVector[11], "x12/F");
 
 	simu->Branch("x13", &x13, "x13/F");
 
 	Int_t NofPi = 0, NofEl = 0;
 	//Max number of electrons and pions for training samples
 	Int_t MaxNofPi = 3000, MaxNofEl = 3000;
-	Double_t mom;
-	Double_t ANNCoef1 = 3.88;
-	Double_t ANNCoef2 = 1.28;
-
+	Double_t dEdX, tr, mom;
 	std::vector<Float_t> inVectorTemp;
 	inVectorTemp.resize(12);
 
-	Double_t dEdX, tr;
-	while ( !finPi.eof() ) {
-		//cout << "--";
-		for (Int_t iStation = 0; iStation < 12; iStation++) {
-			finPi >> dEdX >> tr >> inVectorTemp[iStation];
-			//finPi >> inVector[iStation];
-			//cout <<inVector[iStation] << " ";
-		}
-		for (int i = 0; i < fNofLayers; i++)
-			inVector[i] = inVectorTemp[i];
-		finPi >> mom;
-		x13 = -1.;
-		// coeffCalc(mom, &ANNCoef1, &ANNCoef2);
-		///------------------transform Data BEGIN--------------------------
-		for (Int_t j = 0; j<inVector.size(); j++) {
-			inVector[j]=inVector[j]*1e6;
-			inVector[j]=(inVector[j]-ANNCoef1)/ANNCoef2 -0.225;
-		}
-		sort(inVector.begin(), inVector.end());
-		for (Int_t j = 0; j<inVector.size(); j++)
-			inVector[j]=TMath::LandauI(inVector[j]);
-		///------------------transform Data END----------------------------
+	if (doTraining){
 
-		simu->Fill();
-		NofPi++;
-		if (NofPi >= MaxNofPi) {
-			break;
+		while ( !finPi.eof() ) {
+			//cout << "--";
+			for (Int_t iStation = 0; iStation < 12; iStation++) {
+				finPi >> dEdX >> tr >> inVectorTemp[iStation];
+				//finPi >> inVector[iStation];
+				//cout <<inVector[iStation] << " ";
+			}
+			for (int i = 0; i < fNofLayers; i++)
+				inVector[i] = inVectorTemp[i];
+			finPi >> mom;
+			x13 = -1.;
+			// coeffCalc(mom, &ANNCoef1, &ANNCoef2);
+			transform1();
+
+			simu->Fill();
+			NofPi++;
+			if (NofPi >= MaxNofPi) {
+				break;
+			}
 		}
-	}
 
-	while ( !finEl.eof() ) {
-		for (Int_t iStation = 0; iStation < 12; iStation++) {
-			finEl >> dEdX >> tr >> inVectorTemp[iStation];
-			//finEl >> inVector[iStation];
-		}
-		for (int i = 0; i < fNofLayers; i++)
-			inVector[i] = inVectorTemp[i];
-		finEl >> mom;
+		while ( !finEl.eof() ) {
+			for (Int_t iStation = 0; iStation < 12; iStation++) {
+				finEl >> dEdX >> tr >> inVectorTemp[iStation];
+				//finEl >> inVector[iStation];
+			}
+			for (int i = 0; i < fNofLayers; i++)
+				inVector[i] = inVectorTemp[i];
+			finEl >> mom;
 
-		x13 = 1.;
-		// coeffCalc(mom, &ANNCoef1, &ANNCoef2);
+			x13 = 1.;
+			// coeffCalc(mom, &ANNCoef1, &ANNCoef2);
+			transform1();
 
-		///------------------transform Data BEGIN--------------------------
-		for (Int_t j = 0; j<inVector.size(); j++) {
-			inVector[j]=inVector[j]*1e6;
-			inVector[j]=(inVector[j]-ANNCoef1)/ANNCoef2 -0.225;
-		}
-		sort(inVector.begin(), inVector.end());
-		for (Int_t j = 0; j<inVector.size(); j++)
-			inVector[j]=TMath::LandauI(inVector[j]);
-		///------------------transform Data END----------------------------
-
-		simu->Fill();
-		NofEl++;
-		if (NofEl >= MaxNofEl) {
-			break;
+			simu->Fill();
+			NofEl++;
+			if (NofEl >= MaxNofEl) {
+				break;
+			}
 		}
 	}
 
-	TMultiLayerPerceptron* fNN = new TMultiLayerPerceptron("x1,x2,x3,x4,x5,x6:12:x13",simu,"(Entry$+1)");
-
-	//fNN->LoadWeights("Neural_Net_Weights_El_ID_MB_10.txt");
-	fNN->Train(250, "text,update=1");
-    fNN->DumpWeights("Neural_Net_Weights_El_ID_MB_6.txt");
+	TMultiLayerPerceptron* fNN = new TMultiLayerPerceptron(mlpString,simu,"(Entry$+1)");
+	if (doTraining){
+		fNN->Train(250, "+text,update=1");
+		fNN->DumpWeights((const char*)mlpWeightsFile);
+	}else{
+		fNN->LoadWeights((const char*)mlpWeightsFile);
+	}
 
 	/////////////////////////////////////////////////////////////////////////
 	//////////////////////////TEST///////////////////////////////////////////
@@ -152,9 +158,6 @@ void trd_elid_trainANN_txt() {
 	TH1F* hCumProbEl = new TH1F("hCumProbEl","ANN optput;ANN output;Cumulative probability",100, minEval, maxEval);
 	TH1F* hANNErrorPi =	new TH1F("hANNErrorPi","Error = ANN output - true;Error;Entries",100, -0.5,0.5);
 	TH1F* hANNErrorEl =	new TH1F("hANNErrorEl","Error = ANN output - true;Error;Entries",100, -0.5,0.5);
-
-	//cut on the output value of ANN
-	Double_t ANNCut = 0.79;
 
 	//input data for testing
 	std::ifstream finPiTest((const char*) fileNameTestPi);
@@ -174,15 +177,8 @@ void trd_elid_trainANN_txt() {
 			inVector[i] = inVectorTemp[i];
 		finElTest >> mom;
 		//coeffCalc(mom, &ANNCoef1, &ANNCoef2);
-		///------------------transform Data BEGIN--------------------------
-		for (Int_t j = 0; j<inVector.size(); j++) {
-			inVector[j]=inVector[j]*1e6;
-			inVector[j]=(inVector[j]-ANNCoef1)/ANNCoef2 - 0.225;
-		}
-		sort(inVector.begin(), inVector.end());
-		for (Int_t j = 0; j<inVector.size(); j++)
-			inVector[j]=TMath::LandauI(inVector[j]);
-		///------------------transform Data END----------------------------
+		transform1();
+
 		for (UInt_t k = 0; k < inVector.size(); k++)
 			params[k] = inVector[k];
 		Double_t nnEval = fNN->Evaluate(0, params);
@@ -207,15 +203,8 @@ void trd_elid_trainANN_txt() {
 		finPiTest >> mom;
 
 		//coeffCalc(mom, &ANNCoef1, &ANNCoef2);
-		///------------------transform Data BEGIN--------------------------
-		for (Int_t j = 0; j<inVector.size(); j++) {
-			inVector[j]=inVector[j]*1e6;
-			inVector[j]=(inVector[j]-ANNCoef1)/ANNCoef2 -0.225;
-		}
-		sort(inVector.begin(), inVector.end());
-		for (Int_t j = 0; j<inVector.size(); j++)
-			inVector[j]=TMath::LandauI(inVector[j]);
-		///------------------transform Data END----------------------------
+		transform1();
+
 		for (UInt_t k = 0; k < inVector.size(); k++)
 			params[k] = inVector[k];
 		Double_t nnEval = fNN->Evaluate(0, params);

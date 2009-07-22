e93be760a433453fd9f38539f1c44b051e56556a
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2009-07-22 08:42:13 +0000

    Use correct geometry- and digi-files. Remove exit statements.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6100 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/htrack/runHoughStsTrackFinder.C
+++ macro/htrack/runHoughStsTrackFinder.C
@@ -45,8 +45,8 @@
   TString anaFile      = outputFolder + "/" + system + "." + beam + "." + trigger + ".ana.root";
 
   // Digitisation file
-  //  TString digiFile     = "sts_standard.digi.par";
-  TString digiFile = "sts_Standard_s3055AAFK5.SecD.digi.par";
+  TString digiFile     = "sts_standard.digi.par";
+  //TString digiFile = "sts_Standard_s3055AAFK5.SecD.digi.par";
 
   // Number of events to process
   Int_t nEvents        = 2;
@@ -150,7 +150,7 @@
 
   cout << " Test passed" << endl;
   cout << " All ok " << endl;
-  exit(0);
+//  exit(0);
 
   // ------------------------------------------------------------------------
 
--- macro/htrack/runProduceFile.C
+++ macro/htrack/runProduceFile.C
@@ -49,8 +49,8 @@
   Double_t fieldScale = 1.;      // field scaling factor
 
   // STS geometry
-  //  TString stsGeom     = "sts_standard.geo";
-  TString stsGeom    = "sts_Standard_s3055AAFK5.SecD.geo";
+  TString stsGeom     = "sts_standard.geo";
+  //TString stsGeom    = "sts_Standard_s3055AAFK5.SecD.geo";
 
 
   // In general, the following parts need not be touched
@@ -177,7 +177,7 @@
 
   cout << " Test passed" << endl;
   cout << " All ok " << endl;
-  exit(0);
+//  exit(0);
 
   // ------------------------------------------------------------------------
 

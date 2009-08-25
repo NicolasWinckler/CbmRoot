2a05bcbf49e3d713c94e495f812e9d1b7be98eec
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2009-08-25 09:54:37 +0000

    directory renamed
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6338 0381ead4-6506-0410-b988-94b70fbc4730

--- littrack/base/CbmLitDefaultSettings.h
+++ littrack/base/CbmLitDefaultSettings.h
@@ -12,7 +12,7 @@
 namespace lit {
 
 //Track propagation settings
-const myf LINE_EXTRAPOLATION_START_Z = 450.; // z coordinate [cm] after which line extrapolation starts
+const myf LINE_EXTRAPOLATION_START_Z = 450.; // z coordinate [cm] after which linear track extrapolation starts
 const myf ENERGY_LOSS_CONST = 0.00354; // energy lost constant [GeV/c] used in the simple energy loss calculation
 const myf MINIMUM_PROPAGATION_DISTANCE = 1e-3; // minimum propagation distance [cm]
 const myf MAXIMUM_TGEO_NAVIGATION_DISTANCE = 30.; // maximum distance used in the TGeo navigation

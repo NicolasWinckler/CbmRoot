b337b5fe695e57e69ffcce474ac59adc1c2feabc
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2009-09-02 09:13:11 +0000

    add selection dir to cmake
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6384 0381ead4-6506-0410-b988-94b70fbc4730

--- rich/RichLinkDef.h
+++ rich/RichLinkDef.h
@@ -7,49 +7,56 @@
 #pragma link off all functions;
 
 #pragma link C++ class  CbmRich+;
-
 #pragma link C++ class  CbmRichHitProducer+;
+#pragma link C++ class CbmGeoRich+;
+#pragma link C++ class CbmRichMatchRings+;
+#pragma link C++ class CbmRichElectronIdAnn+;
+#pragma link C++ class CbmRichContFact;
+#pragma link C++ class CbmGeoRichPar;
+
+//fitter
+#pragma link C++ class CbmRichRingFitterCircle+;
+#pragma link C++ class CbmRichRingFitterCOP+;
+#pragma link C++ class CbmRichRingFitterRobustCOP+;
+#pragma link C++ class CbmRichRingFitterTAU+;
+#pragma link C++ class CbmRichFitRings+;
+#pragma link C++ class CbmRichRingFitterImpl+;
+#pragma link C++ class CbmRichRingFitterEllipseTau+;
+#pragma link C++ class CbmRichRingFitterQa+;
+#pragma link C++ class CbmRichRingFitterEllipseBase+;
+#pragma link C++ class CbmRichRingFitterEllipse+;
+
+//finder
+#pragma link C++ class CbmRichRingFinderHough+;
+#pragma link C++ class CbmRichRingFinderTrack+;
+#pragma link C++ class CbmRichRingFinderIdeal+;
+#pragma link C++ class CbmRichFindRings+;
+#pragma link C++ class CbmRichRingFinderHoughBase;
+#pragma link C++ class CbmRichRingFinderHoughParallel;
+#pragma link C++ class CbmRichFuzzyKE+;
+//#pragma link C++ class CbmRichRingFinderHoughSimd;
+
+
+//qa
+#pragma link C++ class  CbmRichTestSim+;
+#pragma link C++ class  CbmRichTestHits+;
+#pragma link C++ class  CbmRichRingQa+;
+#pragma link C++ class CbmRichGeoTest+;
+#pragma link C++ class CbmRichElectronsQa+;
+
+//selection
+#pragma link C++ class CbmRichRingSelectImpl+;
+#pragma link C++ class CbmRichRingSelectNeuralNet+;
+#pragma link C++ class CbmRichSelectRings+;
+
+//tracks
 #pragma link C++ class  CbmRichProjectionProducer+;
 #pragma link C++ class  CbmRichTrackExtrapolationIdeal+;
 #pragma link C++ class  CbmRichTrackExtrapolationMirrorIdeal+;
 #pragma link C++ class  CbmRichExtrapolateTracks+;
-#pragma link C++ class  CbmGeoRich+;
-#pragma link C++ class  CbmRichRingFinderHough+;
-#pragma link C++ class  CbmRichRingFinderTrack+;
-#pragma link C++ class  CbmRichRingFinderIdeal+;
-#pragma link C++ class  CbmRichFindRings+;
-#pragma link C++ class  CbmRichRingFitterCircle+;
-#pragma link C++ class  CbmRichRingFitterCOP+;
-#pragma link C++ class  CbmRichRingFitterRobustCOP+;
-#pragma link C++ class  CbmRichRingFitterTAU+;
-#pragma link C++ class  CbmRichFitRings+;
-#pragma link C++ class  CbmRichMatchRings+;
 #pragma link C++ class  CbmRichRingTrackAssignClosestD+;
 #pragma link C++ class  CbmRichRingTrackAssignIdeal+;
 #pragma link C++ class  CbmRichAssignTrack+;
-#pragma link C++ class  CbmRichTestSim+;
-#pragma link C++ class  CbmRichTestHits+;
-#pragma link C++ class  CbmRichRingQa+;
-
-#pragma link C++ class CbmRichRingSelectImpl+;
-#pragma link C++ class CbmRichRingSelectNeuralNet+;
-#pragma link C++ class CbmRichSelectRings+;
-#pragma link C++ class CbmRichRingFitterEllipse+;
-#pragma link C++ class CbmRichGeoTest+;
-#pragma link C++ class CbmRichRingFitterImpl+;
-#pragma link C++ class CbmRichRingFitterEllipseTau+;
-#pragma link C++ class CbmRichRingFitterQa+;
-#pragma link C++ class CbmRichFuzzyKE+;
-#pragma link C++ class CbmRichElectronsQa+;
-#pragma link C++ class CbmRichElectronIdAnn+;
-#pragma link C++ class CbmRichRingFitterEllipseBase+;
-#pragma link C++ class CbmRichRingFinderHoughBase;
-#pragma link C++ class CbmRichRingFinderHoughParallel;
-
-//#pragma link C++ class CbmRichRingFinderHoughSimd;
-
-#pragma link C++ class CbmRichContFact;
-#pragma link C++ class CbmGeoRichPar;
 
 //#pragma link C++ class LhcbRingFinder;
 

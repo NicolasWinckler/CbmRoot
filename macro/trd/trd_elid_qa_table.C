a1524c3bad425a4579ffc4ebc01e6a6ac07e73c9
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2009-09-16 13:37:41 +0000

    add functionality
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6539 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/trd_elid_qa_table.C
+++ macro/trd/trd_elid_qa_table.C
@@ -2,6 +2,7 @@
 
 void trd_elid_qa_table()
 {
+	gROOT->LoadMacro("trd_elid_qa_draw.C");
 	gStyle->SetPalette(1,0);
     gStyle->SetHistLineWidth(3);
 	Int_t nofFiles = 10;
@@ -9,12 +10,18 @@ void trd_elid_qa_table()
     std::vector<Double_t> fPiELossSum;
     std::vector<Double_t> fElELossSum;
     std::vector<Double_t> fElNofZeroTR;
+    std::vector<Double_t> fFWHM;
+    std::vector<Double_t> fMP;
 
     fPiELossSum.resize(nofFiles);
     fElELossSum.resize(nofFiles);
     fElNofZeroTR.resize(nofFiles);
+    fFWHM.resize(nofFiles);
+    fMP.resize(nofFiles);
 
 	char recoFileName[150];
+	double* par;
+//fill arrays
 	for (Int_t iFile = 0; iFile < nofFiles; iFile++){
         sprintf(recoFileName,"/d/cbm02/slebedev/trd/JUL09/st/piel.%.4i.reco.root",iFile);
 		TFile* file = new TFile(recoFileName);
@@ -22,7 +29,44 @@ void trd_elid_qa_table()
 		fPiELossSum[iFile] = fhPiELossSum->GetMean();
 		fElELossSum[iFile] = fhElELossSum->GetMean();
 		fElNofZeroTR[iFile] = fhElNofZeroTR->GetMean();
+		par = getPiEloss(fhPiELoss);
+		fFWHM[iFile] = par[3];
+		fMP[iFile] = par[4];
+	}
 
+//print arrays
+	cout << endl;
+	cout <<"------------------------------------------------------------------" << endl;
+	cout.width(2);
+	cout << "# ";
+	cout.width(15);
+	cout << "pi eloss sum ";
+	cout.width(15);
+	cout << "el eloss sum ";
+	cout.width(15);
+	cout << "el-pi eloss ";
+	cout.width(15);
+	cout << "nof zero TR ";
+	cout.width(15);
+	cout << "FWHM/4.02 ";
+	cout.width(15);
+	cout << "MP " << endl;
+	for (Int_t iFile = 0; iFile < nofFiles; iFile++){
+		cout.precision(3);
+		cout.width(2);
+		cout << iFile << " ";
+		cout.width(15);
+		cout << fPiELossSum[iFile] << " ";
+		cout.width(15);
+		cout << fElELossSum[iFile] << " ";
+		cout.width(15);
+		cout << fElELossSum[iFile] - fPiELossSum[iFile] << " ";
+		cout.width(15);
+		cout << fElNofZeroTR[iFile] << " ";
+		cout.width(15);
+		cout << fFWHM[iFile]/4.02 << " ";
+		cout.width(15);
+		cout << fMP[iFile] << " " << endl;
 	}
 
 }

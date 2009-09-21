f47111cdf4cfde08facedc08465ac990ee5808ae
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2009-09-21 10:16:53 +0000

    macro which loads libraries required for much analysis
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6565 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/much/muchlibs.C
@@ -0,0 +1,21 @@
+void muchlibs(){
+  gSystem->Load("libGeoBase");
+  gSystem->Load("libParBase");
+  gSystem->Load("libBase");
+  gSystem->Load("libCbmBase");
+  gSystem->Load("libCbmData");
+  gSystem->Load("libField");
+  gSystem->Load("libGen");
+  gSystem->Load("libPassive");
+  gSystem->Load("libMvd");
+  gSystem->Load("libSts");
+  gSystem->Load("libRich");
+  gSystem->Load("libTrd");
+  gSystem->Load("libTof");
+  gSystem->Load("libEcal");
+  gSystem->Load("libGlobal");
+  gSystem->Load("libKF");
+  gSystem->Load("libL1");
+  gSystem->Load("libMuch");
+  gSystem->Load("libAnalysis");
+}

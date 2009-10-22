2277d39e0b572c0c145fbb7b9e71cbc136582f63
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2009-10-22 12:48:36 +0000

    add RMS calc
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6776 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/trd_elid_draw_beamdata.C
+++ macro/trd/trd_elid_draw_beamdata.C
@@ -1,8 +1,27 @@
-void trd_elid_draw_beamdata()
+void calculateRMS(Float_t* eloss_exp, Float_t* yield, TH1D* fhElELoss)
+{
+	Double_t rms = 0.;
+	for (Int_t i = 0; i < 100; i++) {
+		if (eloss_exp[i] < 5.) continue;
+		Int_t binNum  = fhElELoss->FindBin(eloss_exp[i]/10e5);
+		Double_t binCont = fhElELoss->GetBinContent(binNum);
+		Double_t d = yield[i] - binCont;
+		rms += d*d;
+	}
+	rms = sqrt(rms/100.);
+	cout<<endl << "rms = " << rms << endl;
+}
+void trd_elid_draw_beamdata(Int_t trdNFoils = 0, Float_t trdDFoils = 0.0, Float_t trdDGap = 0.0)
 {
 	Bool_t doSave = true;
-	TString fileNamePi = "trunck_pi_theta00.gif";
-	TString fileNameEl = "trunck_el_theta00.gif";
+
+	TString outFile = "/d/cbm02/slebedev/trd/JUL09/simtest/piel.0000.reco.root";
+//	TString dir = "/d/cbm02/slebedev/trd/JUL09/tr_params/";
+//	char txt[200];
+//	sprintf(txt,"_%f_%f_%f", trdNFoils, trdDFoils, trdDGap);
+//	TString outFile = dir + "piel.0001.reco."+txt+".root";
+//	TString fileNamePi = dir+ "pi"+ txt +".gif";
+//	TString fileNameEl = dir+"el"+ txt +".gif";
 
 	FILE *occfile = fopen("q-p1.5.dat", "r");
 	char xline[256];
@@ -14,8 +33,8 @@ void trd_elid_draw_beamdata()
 		for (Int_t i = 0; i < 100; i++) {
 			fscanf(occfile, "%f %f %f %f %f", &eloss_exp[i], &yield_pim[i],
 					&err_yield_pim[i], &yield_em[i], &err_yield_em[i]);
-			printf("%f %f %f %f %f\n", eloss_exp[i], yield_pim[i],
-					err_yield_pim[i], yield_em[i], err_yield_em[i]);
+//			printf("%f %f %f %f %f\n", eloss_exp[i], yield_pim[i],
+//					err_yield_pim[i], yield_em[i], err_yield_em[i]);
 			err_eloss_exp[i] = 0;
 			if (yield_em[i] > maxExpEl)	maxExpEl = yield_em[i];
 			if (yield_pim[i] > maxExpPi)maxExpPi = yield_pim[i];
@@ -32,8 +51,7 @@ void trd_elid_draw_beamdata()
 	pim_yield->SetLineWidth(2);
 	em_yield->SetLineWidth(2);
 
-	TFile *simFileHist = new TFile(	"/d/cbm02/slebedev/trd/JUL09/simtest/piel.0000.reco.root");
-
+	TFile *simFileHist = new TFile(	outFile);
 	fhElELoss->Scale(maxExpEl / fhElELoss->GetMaximum());
 	fhPiELoss->Scale(maxExpPi / fhPiELoss->GetMaximum());
 	Float_t eloss_sim[100], yield_sim_el[100], yield_sim_pi[100], err_eloss_sim[100],
@@ -58,9 +76,11 @@ void trd_elid_draw_beamdata()
 	graphPiSim->SetLineColor(kBlue);
 	graphPiSim->SetLineWidth(2);
 
+	calculateRMS(eloss_exp, yield_em, fhElELoss);
+
 	TCanvas* c1 = new TCanvas("c1", "c1");
-	em_yield->Draw("AC*");
-	graphElSim->Draw("C");
+	em_yield->Draw("AL*");
+	graphElSim->Draw("L");
 	gPad->SetGridx(true);
 	gPad->SetGridy(true);
 	if (fileNamePi == "" || fileNameEl == ""){
@@ -70,8 +90,8 @@ void trd_elid_draw_beamdata()
 	}
 
 	TCanvas* c2 = new TCanvas("c2", "c2");
-	pim_yield->Draw("AC*");
-	graphPiSim->Draw("C");
+	pim_yield->Draw("AL*");
+	graphPiSim->Draw("L");
 	gPad->SetGridx(true);
 	gPad->SetGridy(true);
 	if(doSave){

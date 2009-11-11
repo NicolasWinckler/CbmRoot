d43c4267890c968595b5f1fa3c16709f9e2fdf97
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2009-11-11 16:04:15 +0000

    macroc for loading cbm libs
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6941 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/rich/cbmlibs.C
@@ -0,0 +1,23 @@
+void cbmlibs()
+{
+	  gSystem->Load("/u/slebedev/Baykal/tbb21_017oss/libtbb");
+	  gSystem->Load("libGeoBase");
+	  gSystem->Load("libParBase");
+	  gSystem->Load("libBase");
+	  gSystem->Load("libCbmBase");
+	  gSystem->Load("libCbmData");
+	  gSystem->Load("libField");
+	  gSystem->Load("libGen");
+	  gSystem->Load("libPassive");
+	  gSystem->Load("libMvd");
+	  gSystem->Load("libSts");
+	  gSystem->Load("libRich");
+	  gSystem->Load("libTrd");
+	  gSystem->Load("libTof");
+	  gSystem->Load("libEcal");
+	  gSystem->Load("libGlobal");
+	  gSystem->Load("libKF");
+	  gSystem->Load("libL1");
+	  gSystem->Load("libMinuit2"); // Nedded for rich ellipse fitter
+
+}

65fd96ad46fba86c51754f0faf688cd29b6f5005
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2009-11-13 09:42:10 +0000

    new ANN configuration
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6972 0381ead4-6506-0410-b988-94b70fbc4730

--- rich/selection/CbmRichRingSelectNeuralNet.cxx
+++ rich/selection/CbmRichRingSelectNeuralNet.cxx
@@ -68,12 +68,12 @@ void CbmRichRingSelectNeuralNet::Init ()
     simu->Branch("x5", &x5,"x5/F");
     simu->Branch("x6", &x6,"x6/F");
     simu->Branch("x7", &x7,"x7/F");
-    simu->Branch("x8", &x8,"x8/F");
-    simu->Branch("x9", &x9,"x9/F");
+    //simu->Branch("x8", &x8,"x8/F");
+    //simu->Branch("x9", &x9,"x9/F");
     simu->Branch("x10", &x10,"x10/F");
     simu->Branch("x11", &x11,"x11/F");
 
-    fNN = new TMultiLayerPerceptron("x1,x3,x4,x5,x6,x7,x8,x9,x10:20:x11",simu);
+    fNN = new TMultiLayerPerceptron("x1,x3,x4,x5,x6,x7,x10:10:x11",simu);
     cout << "-I- CbmRichRingSelectNeuralNet: get NeuralNet weight parameters from: " << fNeuralNetWeights << endl;
     fNN->LoadWeights(fNeuralNetWeights);
 }
@@ -106,9 +106,9 @@ void CbmRichRingSelectNeuralNet::DoSelect(CbmRichRing* ring)
     nnPar[3] =  ring->GetRadialPosition();
     nnPar[4] =  ring->GetAaxis();
     nnPar[5] =  ring->GetBaxis();
-    nnPar[6] =  ring->GetPhi();
-    nnPar[7] =  ring->GetRadialAngle();
-    nnPar[8] =  ring->GetChi2() / ring->GetNDF();
+    //nnPar[6] =  ring->GetPhi();
+    //nnPar[7] =  ring->GetRadialAngle();
+    nnPar[6] =  ring->GetChi2() / ring->GetNDF();
 
     Double_t nnEval = fNN->Evaluate(0,nnPar);
     if (fVerbose > 1) cout <<"nnEval = "<<nnEval<<endl;

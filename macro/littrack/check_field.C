9f1d18d933794a89d278f7dafdca8b72dc38ca9c
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2009-11-17 10:20:55 +0000

    macro to check the field approximation added
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@6994 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/check_field.C
@@ -0,0 +1,72 @@
+/** check_field.C
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2009
+ * @version 1.0
+ *
+ **/
+
+void check_field(Int_t nEvents = 1)
+{
+	TString script = TString(gSystem->Getenv("SCRIPT"));
+
+	TString dir, mcFile, parFile, emptyFile;
+	// Output directory
+	dir  = "/home/d/andrey/std_10mu/";
+	// MC transport file
+	mcFile = dir + "mc.0000.root";
+	// Parameter file
+	parFile = dir + "param.0000.root";
+
+	emptyFile = dir + "empty.root";
+
+	Int_t iVerbose = 1;
+	TStopwatch timer;
+	timer.Start();
+
+	gSystem->Load("/home/soft/tbb22_004oss/libtbb");
+
+	gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+	basiclibs();
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/cbmrootlibs.C");
+	cbmrootlibs();
+
+	// -----   Reconstruction run   -------------------------------------------
+	FairRunAna *run= new FairRunAna();
+	run->SetInputFile(mcFile);
+	run->SetOutputFile(emptyFile);
+	// ------------------------------------------------------------------------
+
+	CbmLitCheckField* checkField = new CbmLitCheckField();
+	checkField->SetXangle(35.);
+	checkField->SetYangle(25.);
+	checkField->SetNofBinsX(30);
+	checkField->SetNofBinsY(30);
+	checkField->SetUseEllipseAcc(true);
+	checkField->SetPolynomDegree(3);
+	checkField->SetOutputDir("field/muon/ellipse_x35_y25/");
+	run->AddTask(checkField);
+
+	// -----  Parameter database   --------------------------------------------
+	FairRuntimeDb* rtdb = run->GetRuntimeDb();
+	FairParRootFileIo* parIo1 = new FairParRootFileIo();
+	parIo1->open(parFile.Data());
+	rtdb->setFirstInput(parIo1);
+	rtdb->setOutput(parIo1);
+	rtdb->saveOutput();
+	// ------------------------------------------------------------------------
+
+	// -----   Initialize and run   --------------------------------------------
+	run->Init();
+	run->Run(0, nEvents);
+	// ------------------------------------------------------------------------
+
+	// -----   Finish   -------------------------------------------------------
+	timer.Stop();
+	cout << endl << endl;
+	cout << "Macro finished successfully." << endl;
+	cout << "Output file is " << emptyFile << endl;
+	cout << "Parameter file is " << parFile << endl;
+	cout << "Real time " << timer.RealTime() << " s, CPU time " << timer.CpuTime() << " s" << endl;
+	cout << endl;
+	// ------------------------------------------------------------------------
+}

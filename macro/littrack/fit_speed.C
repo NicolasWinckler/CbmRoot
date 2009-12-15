f1850760a039614f8b9d1b9fe0af21bb62b506e2
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2009-12-15 14:16:05 +0000

    Macro to test track fit speed added.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@7350 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/fit_speed.C
@@ -0,0 +1,67 @@
+void fit_speed(Int_t nEvents = 10000)
+{
+	TString dir = "/d/cbm02/andrey/std13_10mu_new/";
+	TString mcFile = dir + "mc.0000.root";
+	TString globalTracksFile = dir + "global.tracks.ideal.0000.root";
+	TString parFile = dir + "param.0000.root";
+	TString outFile = dir + "fit.speed.0000.root";
+
+	TStopwatch timer;
+	timer.Start();
+
+	gSystem->Load("/u/andrey/soft/tbb/Lenny64/libtbb");
+
+	gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+	basiclibs();
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/cbmrootlibs.C");
+	cbmrootlibs();
+
+	FairRunAna *run= new FairRunAna();
+	run->SetInputFile(mcFile);
+	run->AddFriend(globalTracksFile);
+	run->SetOutputFile(outFile);
+
+//	FairGeane* Geane = new FairGeane(inFile.Data());
+//	FairGeane* Geane = new FairGeane(parFile.Data());
+//	FairGeane *Geane = new FairGeane();
+//	run->AddTask(Geane);
+
+	// -------------------------------------------------------------------------
+	CbmLitTrackFitSpeedTest* test = new CbmLitTrackFitSpeedTest();
+	test->SetNofPlanes(13);
+	test->SetNofTrdHits(0);
+	test->SetNofMuchHits(13);
+	test->SetNofTofHits(0);
+	run->AddTask(test);
+	// -------------------------------------------------------------------------
+	TString parDir = TString(gSystem->Getenv("VMCWORKDIR")) + TString("/parameters");
+	TString stsDigiFile = parDir+ "/sts/sts_standard.digi.par";
+	// -----  Parameter database   --------------------------------------------
+	FairRuntimeDb* rtdb = run->GetRuntimeDb();
+	FairParRootFileIo* parIo1 = new FairParRootFileIo();
+	FairParAsciiFileIo* parIo2 = new FairParAsciiFileIo();
+	parIo1->open(parFile.Data());
+	parIo2->open(stsDigiFile.Data(),"in");
+	rtdb->setFirstInput(parIo1);
+	rtdb->setSecondInput(parIo2);
+	rtdb->setOutput(parIo1);
+	rtdb->saveOutput();
+	// ------------------------------------------------------------------------
+
+	// -----   Intialise and run   --------------------------------------------
+	run->LoadGeometry();
+	run->Init();
+//	Geane->SetField(run->GetField());
+	run->Run(0,nEvents);
+	// ------------------------------------------------------------------------
+
+	// -----   Finish   -------------------------------------------------------
+	timer.Stop();
+	cout << endl << endl;
+	cout << "Macro finished succesfully." << endl;
+	cout << "Output file is "    << outFile << endl;
+	cout << "Parameter file is " << parFile << endl;
+	cout << "Real time " << timer.RealTime() << " s, CPU time " << timer.CpuTime() << " s" << endl;
+	cout << endl;
+	// ------------------------------------------------------------------------
+}

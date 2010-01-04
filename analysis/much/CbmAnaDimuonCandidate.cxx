a91c4ac10d4b13ee597cea7e9ea952fbfe364c3d
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2010-01-04 12:15:49 +0000

    Class for dimuon candidates
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@7402 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ analysis/much/CbmAnaDimuonCandidate.cxx
@@ -0,0 +1,30 @@
+#include "CbmAnaDimuonCandidate.h"
+#include "CbmAnaMuonCandidate.h"
+#include "CbmStsTrack.h"
+#include "CbmKFTrack.h"
+#include "TLorentzVector.h"
+
+CbmAnaDimuonCandidate::CbmAnaDimuonCandidate(
+    CbmAnaMuonCandidate muPlus, 
+    CbmAnaMuonCandidate muMinus){
+  
+  fMuPlus  = CbmAnaMuonCandidate(muPlus);
+  fMuMinus = CbmAnaMuonCandidate(muMinus);
+}
+
+TLorentzVector CbmAnaDimuonCandidate::GetMomentumMC(){
+  TLorentzVector* p1 = fMuPlus.GetMomentumMC();
+  TLorentzVector* p2 = fMuMinus.GetMomentumMC();
+  TLorentzVector p = (*p1)+(*p2);
+  return p;
+}
+
+TLorentzVector CbmAnaDimuonCandidate::GetMomentumRC(){
+  TLorentzVector* p1 = fMuPlus.GetMomentumRC();
+  TLorentzVector* p2 = fMuMinus.GetMomentumRC();
+  TLorentzVector p = (*p1)+(*p2);
+  return p;
+}
+
+
+ClassImp(CbmAnaDimuonCandidate);
---
+++ analysis/much/CbmAnaDimuonCandidate.h
@@ -0,0 +1,27 @@
+#ifndef CBMANADIMUONCANDIDATE_H
+#define CBMANADIMUONCANDIDATE_H
+
+#include "CbmAnaMuonCandidate.h"
+class TLorentzVector;
+
+class CbmAnaDimuonCandidate : public TObject{
+  public:
+    CbmAnaDimuonCandidate() {};
+    CbmAnaDimuonCandidate(CbmAnaMuonCandidate muPlus, CbmAnaMuonCandidate muMinus);
+    CbmAnaMuonCandidate* GetMuPlus()  { return &fMuPlus;  }
+    CbmAnaMuonCandidate* GetMuMinus() { return &fMuMinus; }
+    CbmAnaMuonCandidate* GetMu(Bool_t sign) { return sign ? &fMuPlus: &fMuMinus; }
+    TLorentzVector GetMomentumMC();
+    TLorentzVector GetMomentumRC();
+    inline Bool_t IsReconstructed(Int_t nStsHitsCut=8, Double_t chiToVertexCut=3.){ 
+      return fMuPlus.IsReconstructed(nStsHitsCut,chiToVertexCut)*
+             fMuMinus.IsReconstructed(nStsHitsCut,chiToVertexCut); 
+    }
+    virtual ~CbmAnaDimuonCandidate(){};
+  private:
+  CbmAnaMuonCandidate fMuPlus;
+  CbmAnaMuonCandidate fMuMinus;
+  ClassDef(CbmAnaDimuonCandidate,1);
+};
+
+#endif

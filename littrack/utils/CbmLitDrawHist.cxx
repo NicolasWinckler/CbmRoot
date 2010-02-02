055df2499dfad39f3b9115daa61b39eb2a89e423
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-02-02 17:24:26 +0000

    Add additional functions to draw histograms.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@7667 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ littrack/utils/CbmLitDrawHist.cxx
@@ -0,0 +1,124 @@
+/** CbmLitDrawHist.cxx
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2008
+ **/
+#include "CbmLitDrawHist.h"
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TPad.h"
+#include "TLegend.h"
+#include "TStyle.h"
+
+/* Set default styles for histograms. */
+void SetStyles()
+{
+	gStyle->SetOptStat("");
+	gStyle->SetOptFit(0);
+	gStyle->SetOptTitle(0);
+
+	gStyle->SetCanvasColor(kWhite);
+	gStyle->SetFrameFillColor(kWhite);
+	gStyle->SetPadColor(kWhite);
+	gStyle->SetStatColor(kWhite);
+	gStyle->SetTitleFillColor(kWhite);
+	gStyle->SetPalette(1);
+}
+
+
+/* Draws 1D histogram.*/
+void DrawHist1D(
+		TH1* hist,
+		const std::string& titleX,
+		const std::string& titleY,
+		Int_t color,
+		Int_t lineWidth,
+		Int_t lineStyle,
+		Int_t markerSize,
+		Int_t markerStyle,
+		Bool_t logx,
+		Bool_t logy,
+		const std::string& drawOpt)
+{
+	Double_t textSize = 0.06;
+	hist->GetXaxis()->SetTitle(titleX.c_str());
+	hist->GetYaxis()->SetTitle(titleY.c_str());
+	hist->SetLineColor(color);
+	hist->SetLineWidth(lineWidth);
+	hist->SetLineStyle(lineStyle);
+	hist->SetMarkerColor(color);
+	hist->SetMarkerSize(markerSize);
+	hist->SetMarkerStyle(markerStyle);
+	if (logx) gPad->SetLogx();
+	if (logy) gPad->SetLogy();
+	hist->GetXaxis()->SetLabelSize(textSize);
+	hist->GetXaxis()->SetNdivisions(505, kTRUE);
+	hist->GetYaxis()->SetLabelSize(textSize);
+	hist->GetXaxis()->SetTitleSize(textSize);
+	hist->GetYaxis()->SetTitleSize(textSize);
+	hist->GetXaxis()->SetTitleOffset(1.0);
+	hist->GetYaxis()->SetTitleOffset(1.3);
+	gPad->SetLeftMargin(0.17);
+	gPad->SetBottomMargin(0.15);
+	hist->Draw(drawOpt.c_str());
+}
+
+/* Draws 1D histogram.*/
+void DrawHist2D(
+		TH2* hist,
+		const std::string& titleX,
+		const std::string& titleY,
+		const std::string& titleZ,
+		Bool_t logx,
+		Bool_t logy,
+		Bool_t logz,
+		const std::string& drawOpt)
+{
+	hist->GetXaxis()->SetTitle(titleX.c_str());
+	hist->GetYaxis()->SetTitle(titleY.c_str());
+	hist->GetYaxis()->SetTitle(titleZ.c_str());
+	if (logx) gPad->SetLogx();
+	if (logy) gPad->SetLogy();
+	if (logz) gPad->SetLogz();
+	hist->Draw(drawOpt.c_str());
+}
+
+/* Draws up to 3 1D histograms. If hist == NULL than histogram will not be drawn. */
+void DrawHist1D(
+		TH1* hist1,
+		TH1* hist2,
+		TH1* hist3,
+		const std::string& legendLabel,
+		const std::string& xAxisLabel,
+		const std::string& yAxisLabel,
+		const std::string& hist1label,
+		const std::string& hist2label,
+		const std::string& hist3label,
+		Bool_t logx,
+		Bool_t logy,
+		Bool_t drawLegend,
+		Double_t x1,
+		Double_t y1,
+		Double_t x2,
+		Double_t y2)
+{
+	if (hist1 != NULL) DrawHist1D(hist1, xAxisLabel, yAxisLabel,
+			LIT_COLOR1, LIT_LINE_WIDTH, LIT_LINE_STYLE1, LIT_MARKER_SIZE,
+			LIT_MARKER_STYLE1, logx, logy, "");
+
+	if (hist2 != NULL) DrawHist1D(hist2, xAxisLabel, yAxisLabel,
+			LIT_COLOR2, LIT_LINE_WIDTH, LIT_LINE_STYLE2, LIT_MARKER_SIZE,
+						LIT_MARKER_STYLE2, logx, logy, "SAME");
+
+	if (hist3 != NULL) DrawHist1D(hist3, xAxisLabel, yAxisLabel,
+			LIT_COLOR3, LIT_LINE_WIDTH, LIT_LINE_STYLE3, LIT_MARKER_SIZE,
+						LIT_MARKER_STYLE3, logx, logy, "SAME");
+
+	TLegend* l1 = new TLegend(x1, y1, x2, y2);
+	l1->SetFillColor(kWhite);
+	l1->SetHeader(legendLabel.c_str());
+	if (hist1 != NULL) l1->AddEntry(hist1,hist1label.c_str(),"lp");
+	if (hist2 != NULL) l1->AddEntry(hist2,hist2label.c_str(),"lp");
+	if (hist3 != NULL) l1->AddEntry(hist3,hist3label.c_str(),"lp");
+	if (drawLegend) l1->Draw();
+}
--- littrack/utils/CbmLitDrawHist.h
+++ littrack/utils/CbmLitDrawHist.h
@@ -1,13 +1,32 @@
-/** draw_hist.C
+/** CbmLitDrawHist.h
  * @author Andrey Lebedev <andrey.lebedev@gsi.de>
  * @since 2008
  * @version 1.0
  * Some help functions for drawing 1D and 2D histograms.
  **/
+#ifndef CBMLITDRAWHIST_H_
+#define CBMLITDRAWHIST_H_
 
 #include "TH1.h"
 #include "TH2.h"
 #include "TPad.h"
+#include "TLegend.h"
+
+// Set of standard settings for drawing
+const Int_t LIT_COLOR1 = kRed;
+const Int_t LIT_COLOR2 = kBlue;
+const Int_t LIT_COLOR3 = kGreen+3;
+const Int_t LIT_LINE_WIDTH = 1;
+const Int_t LIT_LINE_STYLE1 = 1;
+const Int_t LIT_LINE_STYLE2 = 2;
+const Int_t LIT_LINE_STYLE3 = 3;
+const Int_t LIT_MARKER_SIZE = 1;
+const Int_t LIT_MARKER_STYLE1 = kOpenCircle;
+const Int_t LIT_MARKER_STYLE2 = kOpenSquare;
+const Int_t LIT_MARKER_STYLE3 = kOpenTriangleUp;
+
+/* Set default styles for histograms. */
+void SetStyles();
 
 /* Draws 1D histogram.
  * @param hist Pointer to the histogram.
@@ -33,31 +52,9 @@ void DrawHist1D(
 		Int_t markerStyle = kDot,
 		Bool_t logx = false,
 		Bool_t logy = false,
-		const std::string& drawOpt = ""
-		)
-{
-	Double_t textSize = 0.06;
-	hist->GetXaxis()->SetTitle(titleX.c_str());
-	hist->GetYaxis()->SetTitle(titleY.c_str());
-	hist->SetLineColor(color);
-	hist->SetLineWidth(lineWidth);
-	hist->SetLineStyle(lineStyle);
-	hist->SetMarkerColor(color);
-	hist->SetMarkerSize(markerSize);
-	hist->SetMarkerStyle(markerStyle);
-	if (logx) gPad->SetLogx();
-	if (logy) gPad->SetLogy();
-	hist->GetXaxis()->SetLabelSize(textSize);
-	hist->GetXaxis()->SetNdivisions(505, kTRUE);
-	hist->GetYaxis()->SetLabelSize(textSize);
-	hist->GetXaxis()->SetTitleSize(textSize);
-	hist->GetYaxis()->SetTitleSize(textSize);
-	hist->GetXaxis()->SetTitleOffset(1.0);
-	hist->GetYaxis()->SetTitleOffset(1.3);
-	gPad->SetLeftMargin(0.17);
-	gPad->SetBottomMargin(0.15);
-	hist->Draw(drawOpt.c_str());
-}
+		const std::string& drawOpt = "");
+
+
 
 /* Draws 1D histogram.
  * @param hist Pointer to the histogram.
@@ -77,15 +74,44 @@ void DrawHist2D(
 		Bool_t logx = false,
 		Bool_t logy = false,
 		Bool_t logz = false,
-		const std::string& drawOpt = ""
-		)
-{
-	hist->GetXaxis()->SetTitle(titleX.c_str());
-	hist->GetYaxis()->SetTitle(titleY.c_str());
-	hist->GetYaxis()->SetTitle(titleZ.c_str());
-	if (logx) gPad->SetLogx();
-	if (logy) gPad->SetLogy();
-	if (logz) gPad->SetLogz();
-	hist->Draw(drawOpt.c_str());
-}
+		const std::string& drawOpt = "");
 
+
+
+/* Draws up to 3 1D histograms. If hist == NULL than histogram will not be drawn.
+ * @param hist1 Pointer to the first histogram.
+ * @param hist2 Pointer to the second histogram.
+ * @param hist3 Pointer to the third histogram.
+ * @param legendLabel Legend label text.
+ * @param xAxisLabel X axis label text.
+ * @param yAxisLabel Y axis label text.
+ * @param hist1label Label text for the first histogram.
+ * @param hist2label Label text for the second histogram.
+ * @param hist3label Label text for the third histogram.
+ * @param logx If true than X axis is plotted in logarithmic scale.
+ * @param logy If true than Y axis is plotted in logarithmic scale.
+ * @param drawLegend If true than legend will be drawn.
+ * @param x1 coordinates of the Legend in the current pad
+ * @param y1 coordinates of the Legend in the current pad
+ * @param x2 coordinates of the Legend in the current pad
+ * @param y2 coordinates of the Legend in the current pad
+ */
+void DrawHist1D(
+		TH1* hist1,
+		TH1* hist2,
+		TH1* hist3,
+		const std::string& legendLabel,
+		const std::string& xAxisLabel,
+		const std::string& yAxisLabel,
+		const std::string& hist1label,
+		const std::string& hist2label,
+		const std::string& hist3label,
+		Bool_t logx,
+		Bool_t logy,
+		Bool_t drawLegend,
+		Double_t x1,
+		Double_t y1,
+		Double_t x2,
+		Double_t y2);
+
+#endif

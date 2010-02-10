f517d017b3bf7aa7dae916318460a471464d7778
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-02-10 16:37:01 +0000

    Name of the macro changed.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@7778 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/littrack/pluto.sh
+++ macro/littrack/pluto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for X in 10 11 12 13 14 15 16 17 18 19 20; do
-   root -b -q "./thermal_JPsi_25GeV.C($X)"
+for X in 0 1 2 3 4 5 6 7 8 9; do
+   root -b -q "./pluto_jpsi_25gev.C($X)"
 done
  

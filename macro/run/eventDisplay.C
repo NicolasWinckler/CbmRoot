8445aff924f9ffffd7399732eab596a4de81f4a4
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2010-02-10 11:07:29 +0000

    New version of the macro which reads the geometry correctely from the runtime database.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@7772 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/run/eventDisplay.C
+++ macro/run/eventDisplay.C
@@ -2,9 +2,13 @@
 
 eventDisplay()
 {
+
+  TString  InputFile     ="data/test.mc.root";
+  TString  ParFile       ="data/params.root";
+
+
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
   basiclibs();  
-  gSystem->Load("libEve");
   gSystem->Load("libGeoBase");
   gSystem->Load("libParBase");
   gSystem->Load("libBase");
@@ -16,25 +20,28 @@ eventDisplay()
   gSystem->Load("libMvd");
   gSystem->Load("libSts");
   gSystem->Load("libRich");
-//  gSystem->Load("libTrd");
+  gSystem->Load("libTrd");
   gSystem->Load("libTof");
   gSystem->Load("libEcal");
+  gSystem->Load("libEve");
   gSystem->Load("libEventDisplay");
-  gSystem->Load("libGeom");
                                      
   // -----   Reconstruction run   -------------------------------------------
   FairRunAna *fRun= new FairRunAna();
-  fRun->SetInputFile("data/test.mc.root");
-  //fRun->SetInputFile("../dsk/sim_dsk.root");
- // fRun->SetInputFile("../dsk/sim_dsk.g4native.root");
-  fRun->SetOutputFile("test.root");
-  fRun->LoadGeometry();
+
+  fRun->SetInputFile(InputFile.Data());
+  fRun->SetOutputFile("data/test.root");
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open(ParFile.Data());
+  rtdb->setFirstInput(parInput1);
 
   FairEventManager *fMan= new FairEventManager();
   FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
   FairMCPointDraw *RichPoint =   new FairMCPointDraw ("RichPoint",kOrange,  kFullSquare);
   FairMCPointDraw *TofPoint =    new FairMCPointDraw ("TOFPoint",kBlue,  kFullSquare);
-  //FairMCPointDraw *TrdPoint= new FairMCPointDraw ("TRDPoint",kTeal,  kFullSquare);
+  FairMCPointDraw *TrdPoint= new FairMCPointDraw ("TRDPoint",kTeal,  kFullSquare);
   FairMCPointDraw *ECalPoint =    new FairMCPointDraw ("ECALPoint",kYellow,  kFullSquare);
   FairMCPointDraw *RefPlanePoint = new FairMCPointDraw ("RefPlanePoint",kPink,  kFullSquare);
   FairMCPointDraw *STSPoint = new FairMCPointDraw ("STSPoint",kCyan,  kFullSquare);
@@ -44,7 +51,7 @@ eventDisplay()
   fMan->AddTask(RichPoint);
   fMan->AddTask(ECalPoint);   
   fMan->AddTask(TofPoint);   
-  //fMan->AddTask(TrdPoint);
+  fMan->AddTask(TrdPoint);
   fMan->AddTask(RefPlanePoint);
   fMan->AddTask(STSPoint);
  

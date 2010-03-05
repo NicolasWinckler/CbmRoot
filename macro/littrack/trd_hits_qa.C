ffe99765a2b68e73744566c849892d373d9d8167
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-03-05 20:57:05 +0000

    Macro for running CbmTRDHitsQa added.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@8020 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/trd_hits_qa.C
@@ -0,0 +1,66 @@
+/** trd_hits_qa.C
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2010
+ * @version 1.0
+ **/
+
+void trd_hits_qa(Int_t nEvents = 10000)
+{
+	// Output directory
+	TString dir  = "/d/cbm02/andrey/monotrd_1pi/";
+	// MC transport file
+	TString mcFile = dir + "mc.0000.root";
+	// Parameter file
+	TString parFile = dir + "param.0000.root";
+	// File with reconstructed STS tracks, STS, MUCH, TRD and TOF hits and digis.
+	TString globalHitsFile = dir + "global.hits.0000.root";
+	// Output file with TRD hits qa histograms
+	TString trdHitsQaFile = dir + "trd.hits.qa.0000.root";
+
+	Int_t iVerbose = 1;
+	TStopwatch timer;
+	timer.Start();
+
+	gSystem->Load("/u/andrey/soft/tbb/Lenny64/libtbb");
+
+	gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+	basiclibs();
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/cbmrootlibs.C");
+	cbmrootlibs();
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/determine_setup.C");
+
+	FairRunAna *run= new FairRunAna();
+	run->SetInputFile(mcFile);
+	run->AddFriend(globalHitsFile);
+	run->SetOutputFile(trdHitsQaFile);
+
+	//------------------------------------------------
+	CbmTrdHitsQa* trdHitsQa = new CbmTrdHitsQa();
+	trdHitsQa->SetOutputDir("./");
+	run->AddTask(trdHitsQa);
+	//------------------------------------------------
+
+	// -----  Parameter database   --------------------------------------------
+	FairRuntimeDb* rtdb = run->GetRuntimeDb();
+	FairParRootFileIo* parIo1 = new FairParRootFileIo();
+	parIo1->open(parFile.Data());
+	rtdb->setFirstInput(parIo1);
+	rtdb->setOutput(parIo1);
+	rtdb->saveOutput();
+	// ------------------------------------------------------------------------
+
+	// -----   Intialise and run   --------------------------------------------
+	run->Init();
+	run->Run(0, nEvents);
+	// ------------------------------------------------------------------------
+
+	// -----   Finish   -------------------------------------------------------
+	timer.Stop();
+	cout << endl << endl;
+	cout << "Macro finished succesfully." << endl;
+	cout << "Output file is " << trdHitsQaFile << endl;
+	cout << "Parameter file is " << parFile << endl;
+	cout << "Real time " << timer.RealTime() << " s, CPU time " << timer.CpuTime() << " s" << endl;
+	cout << endl;
+	// ------------------------------------------------------------------------
+}

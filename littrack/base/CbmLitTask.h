b2813da637db0f6fd43efd7b85d4a5f66769275d
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-04-01 13:10:41 +0000

    base class for Littrack tasks added.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@8254 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ littrack/base/CbmLitTask.h
@@ -0,0 +1,47 @@
+/* CbmLitTask.h
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2010
+ * @version 1.0
+ *
+ * Base task class for Littrack tasks.
+ * Add some common functionality to FairTask,
+ * which is used in the Littrack tasks.
+ */
+
+#ifndef CBMLITTASK_H_
+#define CBMLITTASK_H_
+
+#include "FairTask.h"
+
+class CbmLitTask : public FairTask {
+public:
+	/* Constructor */
+	CbmLitTask();
+
+	/* Destructor */
+	virtual ~CbmLitTask();
+
+	/* Sets the output directory for images.
+     * @param dir Directory name.
+     */
+	void SetOutputDir(const std::string& dir) { fOutputDir = dir;}
+
+protected:
+	/* Determines current CBM setup. */
+	void DetermineSetup();
+
+	Bool_t fIsElectronSetup; // If "electron" setup detected than true
+	Bool_t fIsSts; // If STS detected than true
+	Bool_t fIsTrd; // If TRD detected than true
+	Bool_t fIsMuch; // If MUCH detected than true
+	Bool_t fIsTof; // If TOF detected than truer
+
+	Int_t fEventNo; // Event counter
+	Int_t fVerbose; // Verbose level
+
+	std::string fOutputDir; // output directory for images
+
+	ClassDef(CbmLitTask, 1);
+};
+
+#endif /* CBMLITTASK_H_ */

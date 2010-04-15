53f72546eb67262af2f7ed1f82ef951821d5ea4e
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-04-15 13:10:54 +0000

    missing file added
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@8386 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ littrack/base/CbmLitTask.cxx
@@ -0,0 +1,40 @@
+/* CbmLitTask.cxx
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2010
+ * @version 1.0.
+ */
+#include "CbmLitTask.h"
+#include "CbmLitEnvironment.h"
+
+#include <iostream>
+
+CbmLitTask::CbmLitTask()
+{
+
+}
+
+CbmLitTask::~CbmLitTask()
+{
+
+}
+
+
+void CbmLitTask::DetermineSetup()
+{
+    CbmLitEnvironment* env = CbmLitEnvironment::Instance();
+    fIsElectronSetup = env->IsElectronSetup();
+	fIsSts = env->IsSts();
+    fIsTrd = env->IsTrd();
+    fIsMuch = env->IsMuch();
+    fIsTof = env->IsTof();
+
+    if (fIsElectronSetup) std::cout << "-I- CBM electron setup detected" << std::endl;
+    else std::cout << "-I- CBM muon setup detected" << std::endl;
+    std::cout << "-I- The following detectors were found in the CBM setup:" << std::endl;
+    if (fIsSts) std::cout << "STS" << std::endl;
+    if (fIsTrd) std::cout << "TRD" << std::endl;
+    if (fIsMuch) std::cout << "MUCH" << std::endl;
+    if (fIsTof) std::cout << "TOF" << std::endl;
+}
+
+ClassImp(CbmLitTask);

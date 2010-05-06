60f22e1069861112416276a1a4910bf4131039ee
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-05-06 12:39:37 +0000

    efficiency drawing macro updated
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@8557 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/littrack/draw_rec_qa.C
+++ macro/littrack/draw_rec_qa.C
@@ -1,162 +1,60 @@
-TString dir  = "/home/d/andrey/events/much/compact/10mu/mu_urqmd/";
-TFile *file = new TFile(dir + "much.tracks.branch.0000.root");
-//TString dir  = "/home/d/andrey/events/trd/monolithic/10e/e_urqmd/";
-//TFile *file = new TFile(dir + "trd.tracks.branch.0000.root");
+#include "../../littrack/utils/CbmLitDrawHist.h"
+#include "../../littrack/utils/CbmLitDrawHist.cxx"
+#include "../../littrack/utils/CbmLitUtils.cxx"
 
-Int_t lineWidth = 3;
-Int_t markerSize = 3;
-
-Int_t lineStyleGhost = 2;
-Int_t lineStyleTrue = 1;
-Int_t colorGhost = 2;
-Int_t colorTrue = 4;
-
-Bool_t isMuons = dir.Contains("much");
+const TString dir  = "/home/d/andrey/phd/much1_10mu_urqmd/";
+const TFile *file1 = new TFile(dir + "global.tracks.branch.0000.root");
+const TFile *file2 = new TFile(dir + "global.tracks.nn.0000.root");
+const TFile *file3 = new TFile(dir + "global.tracks.weight.0000.root");
 
 void draw_rec_qa()
 {
-	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/draw_hist.C");
+	SetStyles();
 
-	gStyle->SetOptStat("");
-	gStyle->SetOptFit(0);
-	gStyle->SetOptTitle(0);
-
-	draw_eff();
-    draw_params();
-    draw_params_2D();
+	DrawEffMethods();
 }
 
-void draw_eff()
+void DrawEffMethods()
 {
-  TCanvas *c1 = new TCanvas("momentum efficiency","momentum efficiency",1200,1000);
-  c1->SetGrid();
-  if (isMuons) draw_eff(hMomEffAll, hMomEffMuons);
-  else draw_eff(hMomEffAll, hMomEffElectrons);
+	TCanvas *canvas = new TCanvas("rec_qa_eff_methods_signal","rec_qa_eff_methods_signal",1200,1000);
+	canvas->SetGrid();
+	TH1* eff1 = (TH1*)file1->Get("hRecMomEffMuon");
+	TH1* eff2 = (TH1*)file2->Get("hRecMomEffMuon");
+	TH1* eff3 = (TH1*)file3->Get("hRecMomEffMuon");
 
-  TCanvas *c2 = new TCanvas("np efficiency","np efficiency",1200,1000);
-  c2->SetGrid();
-  if (isMuons) draw_eff(hNpEffAll, hNpEffMuons);
-  else draw_eff(hNpEffAll, hNpEffElectron);
+	TH1* acc1 = (TH1*)file1->Get("hRecMomAccMuon");
+	TH1* acc2 = (TH1*)file2->Get("hRecMomAccMuon");
+	TH1* acc3 = (TH1*)file3->Get("hRecMomAccMuon");
+
+	TH1* rec1 = (TH1*)file1->Get("hRecMomRecMuon");
+	TH1* rec2 = (TH1*)file2->Get("hRecMomRecMuon");
+	TH1* rec3 = (TH1*)file3->Get("hRecMomRecMuon");
+
+	std::string hname1, hname2, hname3;
+	hname1 = "branch (" + CalcEfficiency(rec1, acc1) + ")";
+	hname2 = "nn (" + CalcEfficiency(rec2, acc2) + ")";
+	hname3 = "weight (" + CalcEfficiency(rec3, acc3) + ")";
+
+	canvas->cd();
+	eff1->SetMinimum(0.);
+	eff1->SetMaximum(1.);
+	DrawHist1D(eff1, eff2, eff3,
+			"Efficiency", "Momentum [GeV/c]", "Efficiency", hname1, hname2, hname3,
+			false, false, true, 0.3,0.3,0.85,0.6);
+	SaveCanvasAsImage(canvas, "./test/");
 }
 
-void draw_eff(
-		TH1* hist1,
-		TH1* hist2)
+std::string CalcEfficiency(
+		TH1* histRec,
+		TH1* histAcc)
 {
-  hist1->SetMaximum(1);
-  hist1->SetMinimum(0);
-  draw_hist_1D(hist1, "Momentum, GeV/c", "Efficiency",
-		  kBlue, lineWidth, 1, markerSize, kCircle, false, false, "");
-  hist1->GetXaxis()->SetLabelSize(0.055);
-  hist1->GetYaxis()->SetLabelSize(0.055);
-
-  draw_hist_1D(hist2, "Momentum, GeV/c", "Efficiency",
-  		  kRed, lineWidth, 1, markerSize, kFullDotLarge, false, false, "SAME");
-
-  TLegend* l1 = new TLegend(0.5,0.7,0.9,0.9);
-  l1->SetHeader("Efficiency");
-  l1->AddEntry(hist1,"all tracks","lp");
-  if (isMuons) l1->AddEntry(hist2,"muon tracks","lp");
-  else l1->AddEntry(hist2,"electron tracks","lp");
-  l1->Draw();
-}
-
-void draw_params()
-{
-  TCanvas *c1 = new TCanvas("parameters","parameters", 1200, 1000);
-  c1->Divide(3,2);
-  c1->SetGrid();
-
-  c1->cd(1);
-  draw_param_hist(hChi2True, hChi2Ghost, "Chi2/NDF", "Counter", "Chi2/NDF");
-  c1->cd(2);
-  draw_param_hist(hMomTrue, hMomGhost, "Momentum", "Counter", "Momentum, GeV/c");
-  c1->cd(3);
-  draw_param_hist(hNofHitsTrue, hNofHitsGhost, "Number of hits", "Counter", "Number of hits");
-  c1->cd(4);
-  draw_param_hist(hRadialPosTrue, hRadialPosGhost, "Radial position", "Counter", "Radial position, cm");
-  c1->cd(5);
-  draw_param_hist(hLastPlaneIdTrue, hLastPlaneIdGhost, "Last plane id", "Counter", "Last plane id");
-  c1->cd(6);
-  draw_nof_hits();
-}
-
-void draw_params_2D()
-{
-	gStyle->SetPalette(1, 0);
-
-  Int_t lsGhost = 2;
-  Int_t lsTrue = 1;
-  Int_t width = 3;
-  Int_t size = 3;
-  Int_t colorGhost = 2;
-  Int_t colorTrue = 4;
-
-  Double_t max;
-
-  TCanvas *c1 = new TCanvas("parameters2D","parameters2D", 1200, 1000);
-  c1->Divide(2,1);
-  c1->SetGrid();
-
-  c1->cd(1);
-  hMomChi2True->GetXaxis()->SetTitle("momentum, GeV");
-  hMomChi2True->GetYaxis()->SetTitle("chi2");
-  hMomChi2True->GetZaxis()->SetTitle("counter");
-  hMomChi2True->SetMarkerColor(colorTrue);
-  hMomChi2True->SetLineColor(colorTrue);
-  hMomChi2True->SetLineWidth(lineWidth);
-  hMomChi2True->SetMarkerSize(markerSize);
-  hMomChi2True->Draw("COLZ");
-
-  c1->cd(2);
-  hMomChi2Ghost->GetXaxis()->SetTitle("momentum, GeV");
-  hMomChi2Ghost->GetYaxis()->SetTitle("chi2");
-  hMomChi2Ghost->GetZaxis()->SetTitle("counter");
-  hMomChi2Ghost->SetMarkerColor(colorGhost);
-  hMomChi2Ghost->SetLineColor(colorGhost);
-  hMomChi2Ghost->SetLineWidth(lineWidth);
-  hMomChi2Ghost->SetMarkerSize(markerSize);
-  hMomChi2Ghost->Draw("COLZ");
-}
-
-void draw_nof_hits()
-{
-	draw_hist_1D(hNofHits, "nof hits", "counter",
-			  kBlack, lineWidth, 3, markerSize, kDot, false, true, "");
-
-	draw_hist_1D(hNofGoodHits, "nof hits", "counter",
-			colorTrue, lineWidth, lineStyleTrue, markerSize, kDot, false, true, "SAME");
-
-	draw_hist_1D(hNofBadHits, "nof hits", "counter",
-			colorGhost, lineWidth, lineStyleGhost, markerSize, kDot, false, true, "SAME");
-
-	TLegend* l1 = new TLegend(0.5,0.7,0.9,0.9);
-	l1->SetHeader("nof hits");
-	l1->AddEntry(hNofHits,"nof hits","l");
-	l1->AddEntry(hNofGoodHits,"nof good hits","l");
-	l1->AddEntry(hNofBadHits,"nof bad hits","l");
-	l1->Draw();
-}
-
-void draw_param_hist(
-		TH1* h1,
-		TH1* h2,
-		const std::string& header,
-		const std::string& titleX,
-		const std::string& titleY)
-{
-	draw_hist_1D(h1, titleX.c_str(), titleY.c_str(),
-			  colorTrue, lineWidth, lineStyleTrue, markerSize, kDot, false, true, "");
-	draw_hist_1D(h2, titleX.c_str(), titleY.c_str(),
-			  colorGhost, lineWidth, lineStyleGhost, markerSize, kDot, false, true, "SAME");
-
-	Double_t max = (h1->GetMaximum() > h2->GetMaximum()) ? h1->GetMaximum() : h2->GetMaximum();
-  	h1->SetMaximum(max);
-
-  	TLegend* l1 = new TLegend(0.5,0.7,0.9,0.9);
-  	l1->SetHeader(header.c_str());
-  	l1->AddEntry(h1,"true tracks","l");
-  	l1->AddEntry(h2,"ghost tracks","l");
-  	l1->Draw();
+	if (histAcc->GetEntries() == 0) return "0.0";
+	else {
+		Double_t eff = Double_t(histRec->GetEntries()) / Double_t(histAcc->GetEntries());
+		std::stringstream ss;
+		ss.precision(3);
+		ss << eff;
+		return ss.str();
+	}
 }
 

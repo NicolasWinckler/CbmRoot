24987b99d479f076e1faeedab2052ad9e93d76bf
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2010-06-23 14:03:05 +0000

    Use the correct version again. The version comitted was not for oficial use now.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9057 0381ead4-6506-0410-b988-94b70fbc4730

--- cbmbase/CbmDetectorList.h
+++ cbmbase/CbmDetectorList.h
@@ -17,21 +17,8 @@ enum DetectorId {kREF,  kMVD, kSTS, kRICH, kMUCH, kTRD, kTOF,
 
 
 enum DataType {  
-  kUnknown=0, kMCTrack=1,
-  kMvdPoint=10, kMvdDigi=11, kMvdCluster=12, kMvdHit=13,
-  kStsPoint=20, kStsDigi=21, kStsCluster=22, kStsHit=23,
-  kRichPoint=30, kRichRing=31,
-  kMuchPoint=40, kMuchDigi=41, kMuchCluster=42, kMuchPixelHit=43, 
-    kMuchStrawHit=44,
-  kTrdPoint=50, kTrdDigi=51, kTrdCluster=52, kTrdHit=53,
-  kTofPoint=60, kTofHit=61,
-  kEcalPoint=70, kEcalPointLite=71, kEcalCluster=72, kEcalHit=73,  
-    kEcalHitFastMC=74, 
-  kZdcPoint=80,
-  kSttPoint=90, kSttHit=91,
-  kTutDetPoint=100,
-  kStsTrack=110, kTrdTrack=111, kMuchTrack=112, kSttTrack=113,
-    kGlobalTrack=114
+  kUnknown, kMCTrack,
+  kStsPoint, kStsDigi, kStsCluster, kStsHit
 };
 
 

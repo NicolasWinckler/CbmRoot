52ab36a4729885799706c7534c03d1c214400e7b
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-07-15 09:32:47 +0000

    header name changed
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9404 0381ead4-6506-0410-b988-94b70fbc4730

--- littrack/parallel/LitTypes.h
+++ littrack/parallel/LitTypes.h
@@ -2,9 +2,9 @@
 #define LITTYPES_H_
 
 #ifdef HAVE_SSE
-  #include "../L1/L1Algo/vectors/P4_F32vec4.h"
+  #include "../../L1/L1Algo/vectors/P4_F32vec4.h"
 #else
-  #include "../L1/L1Algo/vectors/PSEUDO_F32vec1.h"
+  #include "../../L1/L1Algo/vectors/PSEUDO_F32vec1.h"
 #endif
 
 #endif /* LITTYPES_H_ */

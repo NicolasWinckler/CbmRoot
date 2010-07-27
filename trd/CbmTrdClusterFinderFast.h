cd58a638d7e192cf3874c55570db6a2fffd91b72
bergmann <bergmann@0381ead4-6506-0410-b988-94b70fbc4730>
2010-07-27 17:29:43 +0000

    Added CbmTrdClusterFinderFast.h
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9556 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ trd/CbmTrdClusterFinderFast.h
@@ -0,0 +1,100 @@
+// -------------------------------------------------------------------------
+// -----          CbmTrdClusterFinderFast header file                  -----
+// -----   Created 27/07/10  by Lorenz Quack and Cyrano Bergmann       -----
+// -------------------------------------------------------------------------
+
+
+#ifndef CBMTRDCLUSTERFINDERFAST_H
+#define CBMTRDCLUSTERFINDERFAST_H 
+
+#include "FairTask.h"
+#include "CbmTrdDetectorId.h"
+#include <map>
+#include <vector>
+#include <set>
+#include <list>
+
+class CbmTrdDigiPar;
+class CbmTrdModule;
+class TClonesArray;
+
+typedef struct MyDigi
+{
+  Int_t digiId;
+  Int_t rowId;
+  Int_t colId;
+  Int_t combiId;
+} MyDigi;
+
+typedef std::list<MyDigi*> MyDigis;
+typedef std::vector<MyDigis*> ClusterList;
+
+class RowCluster
+{
+ public:
+  RowCluster()
+    {
+      digis = new MyDigis;
+    }
+  ~RowCluster()
+    {
+      if (digis)
+	delete digis;
+    }
+  Int_t minCol;
+  Int_t maxCol;
+  Int_t row;
+  MyDigis *digis;
+  std::list<RowCluster*> parents;
+  std::list<RowCluster*> children;
+};
+
+
+class CbmTrdClusterFinderFast : public FairTask
+{
+
+ public:
+
+  /**
+   * Default constructor.
+   */
+  CbmTrdClusterFinderFast();
+  
+  /**
+   * Default destructor.
+   */
+  virtual ~CbmTrdClusterFinderFast();
+ 
+  /** Initialisation **/
+  virtual InitStatus ReInit();
+  virtual InitStatus Init();
+  virtual void SetParContainers();
+  
+  /** Executed task **/
+  virtual void Exec(Option_t * option);
+
+  /** Finish task **/
+  virtual void Finish();
+
+
+ private:
+  ClusterList *clusterModule(MyDigis *digis);  
+  void mergeRowCluster(RowCluster *currentCluster,
+		       std::list<RowCluster*> *openList);
+  ClusterList *findCluster(std::list<RowCluster*> *rowClusterList);
+  void walkCluster(std::list<RowCluster*> *rowClusterList, 
+		   RowCluster *currentRowCluster,
+		   MyDigis *cluster);
+  void DrawCluster(Int_t moduleId, ClusterList *clusterList);
+
+  TClonesArray*     fDigis;       /** Input array of CbmTrdDigi **/
+  TClonesArray*     fClusters;    /** Output array of CbmTrdCluster **/
+
+  CbmTrdDigiPar *fDigiPar;   //!
+  CbmTrdModule  *fModuleInfo; //!
+  
+  CbmTrdDetectorId fTrdId; //!
+
+  ClassDef(CbmTrdClusterFinderFast,2);
+};
+#endif

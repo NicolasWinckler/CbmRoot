180d065566939066a9f5b16dfa8beaa303671005
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-08-16 14:09:20 +0000

    Added macro which creates data for standalone tracking
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9675 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/create_data.C
@@ -0,0 +1,78 @@
+/** global_reco_qa.C
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2007
+ * @version 3.0
+ * Macro draws histograms produced by CbmLitReconstructionQa class.
+ **/
+
+void create_data(Int_t nEvents = 100)
+{
+	TString script = TString(gSystem->Getenv("SCRIPT"));
+
+   	TString dir, mcFile, parFile, globalHitsFile, globalTracksFile, trackingType;
+
+   	// Output directory
+	dir  = "/d/cbm02/andrey/test_muons/";
+	// MC transport file
+	mcFile = dir + "mc.0000.root";
+	// Parameter file
+	parFile = dir + "param.0000.root";
+	// File with reconstructed STS tracks, STS, MUCH, TRD and TOF hits and digis
+	globalRecoFile = dir + "global.reco.0000.root";
+	// Output file with histograms
+    dummyFile = dir + "dummy.root";
+
+
+	TStopwatch timer;
+	timer.Start();
+
+	gSystem->Load("/home/soft/tbb/libtbb");
+	gSystem->Load("/u/andrey/soft/tbb/Lenny64/libtbb");
+//	gSystem->Load("/u/andrey/soft/tbb/Etch32/libtbb");
+
+	gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+	basiclibs();
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/cbmrootlibs.C");
+	cbmrootlibs();
+
+
+	// -----   Reconstruction run   -------------------------------------------
+	FairRunAna *run= new FairRunAna();
+	run->SetInputFile(mcFile);
+	run->AddFriend(globalRecoFile);
+	run->SetOutputFile(dummyFile);
+	// ------------------------------------------------------------------------
+
+	// ------------------------------------------------------------------------
+	CbmLitCreateStandaloneData* creator = new CbmLitCreateStandaloneData();
+	run->AddTask(creator);
+	// ------------------------------------------------------------------------
+
+	// -----  Parameter database   --------------------------------------------
+	FairRuntimeDb* rtdb = run->GetRuntimeDb();
+	FairParRootFileIo* parIo1 = new FairParRootFileIo();
+	parIo1->open(parFile.Data());
+	rtdb->setFirstInput(parIo1);
+	rtdb->setOutput(parIo1);
+	rtdb->saveOutput();
+	// ------------------------------------------------------------------------
+
+	// -----   Initialize and run   --------------------------------------------
+	run->LoadGeometry();
+	run->Init();
+	run->Run(0, nEvents);
+	// ------------------------------------------------------------------------
+
+	// -----   Finish   -------------------------------------------------------
+	timer.Stop();
+	cout << endl << endl;
+	cout << "Macro finished successfully." << endl;
+	cout << "Output file is " << dummyFile << endl;
+	cout << "Parameter file is " << parFile << endl;
+	cout << "Real time " << timer.RealTime() << " s, CPU time " << timer.CpuTime() << " s" << endl;
+	cout << endl;
+	// ------------------------------------------------------------------------
+
+}
+
+

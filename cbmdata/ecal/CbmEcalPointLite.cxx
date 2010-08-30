93a5c4822515a6eb1b03a7c3945fe988af7662a8
prokudin <prokudin@0381ead4-6506-0410-b988-94b70fbc4730>
2010-08-30 16:04:13 +0000

    Compatibitity with recent changes in framework
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9736 0381ead4-6506-0410-b988-94b70fbc4730

--- cbmdata/ecal/CbmEcalPointLite.cxx
+++ cbmdata/ecal/CbmEcalPointLite.cxx
@@ -1,3 +1,12 @@
 #include "CbmEcalPointLite.h"
 
+#include <iostream>
+
+using namespace std;
+
+void CbmEcalPointLite::Print(const Option_t* opt) const
+{
+  cout << "CbmEcalPointLite: DetectorID=" << GetDetectorID() << ", TrackID=" << GetTrackID() << ", Eloss=" << GetEnergyLoss() << ", Time=" << GetTime() << endl;
+}
+
 ClassImp(CbmEcalPointLite)
--- cbmdata/ecal/CbmEcalPointLite.h
+++ cbmdata/ecal/CbmEcalPointLite.h
@@ -3,17 +3,21 @@
 #ifndef CBMECALPOINTLITE_H
 #define CBMECALPOINTLITE_H
 
-#include "TObject.h"
+#include "FairMCPoint.h"
 
-class CbmEcalPointLite : public TObject
+class CbmEcalPointLite : public FairMCPoint
 {
 public:
 	//Default constuctor
-	CbmEcalPointLite() :
-	  fTrackID(-10), fDetectorID(-10), fTime(-10), fELoss(-10) {};
+	CbmEcalPointLite()
+	  : FairMCPoint(-1111, -1111, TVector3(-1111, -1111, -1111), TVector3(-1111, -1111, -1111), -1111, -1111, -1111)
+        {} ;
 
-	CbmEcalPointLite(Int_t trackID, Int_t detID, Double32_t tof, Double32_t eloss)
-	  : fTrackID(trackID), fDetectorID(detID), fTime(tof), fELoss(eloss) {};
+	CbmEcalPointLite(Int_t trackID, Int_t detID, Double32_t tof, Double32_t eloss, UInt_t EventId=0)
+	  : FairMCPoint(trackID, detID, TVector3(-1111, -1111, -1111), TVector3(-1111, -1111, -1111), tof, -1111, eloss, EventId) 
+	{} ;
+//	  : fTrackID(trackID), fDetectorID(detID), fTime(tof), fELoss(eloss) {};
+/*
 	//Getters
 	Int_t GetTrackID() const	{return fTrackID;}
 	Int_t GetDetectorID() const	{return fDetectorID;}
@@ -27,15 +31,16 @@ public:
 	void SetEnergyLoss(Double32_t eloss)	{fELoss=eloss;}
 
 	//Need implementation
-	void Print() {};
+*/
+	virtual void Print(const Option_t* opt=NULL) const;
 
 	virtual ~CbmEcalPointLite() {};
 private:
-	Int_t fTrackID;		//Index of track
-	Int_t fDetectorID;	//Number of volume in calorimeter system
-	Double32_t fTime;	//Time since interaction moment 
-	Double32_t fELoss;	//Energy deposition in cell
-	ClassDef(CbmEcalPointLite,1)
+//	Int_t fTrackID;		//Index of track
+//	Int_t fDetectorID;	//Number of volume in calorimeter system
+//	Double32_t fTime;	//Time since interaction moment 
+//	Double32_t fELoss;	//Energy deposition in cell
+	ClassDef(CbmEcalPointLite,2)
 };
 
 #endif

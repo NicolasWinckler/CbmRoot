b1a31ef0f85517c2d49a525523a6072190cab9eb
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2010-09-06 15:29:30 +0000

    Fast track fit for electron CBM setup added.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9778 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ littrack/parallel/electron/LitTrackFitterElectron.h
@@ -0,0 +1,61 @@
+/** LitTrackFitterElectron.h
+ * @author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * @since 2009
+ * @version 1.0
+ *
+ * Functions perform kalman filter based track fit for
+ * 'electron' setup of CBM.
+ **/
+
+#ifndef LITTRACKFITTERELECTRON_H_
+#define LITTRACKFITTERELECTRON_H_
+
+#include "LitDetectorGeometryElectron.h"
+#include "parallel/LitTypes.h"
+#include "parallel/LitTrackParam.h"
+#include "parallel/LitExtrapolation.h"
+#include "parallel/LitFiltration.h"
+#include "parallel/LitAddMaterial.h"
+
+/* Performs SIMDized/scalar fast track fit.
+ * Number of hits must be equal to the number of stations.
+ */
+template<class T>
+inline void LitTrackFitterElectron(
+		LitTrack<T>& track,
+		const LitDetectorLayoutElectron<T>& layout)
+{
+    //   track.paramLast = track.paramFirst;
+	LitTrackParam<T>& par = track.paramLast;
+	unsigned char ihit = 0;
+
+    for (unsigned char ivp = 0; ivp < layout.GetNofVirtualPlanes()-1; ivp++) {
+    	LitVirtualPlaneElectron<T>& vp1 = layout.virtualPlanes[ivp];
+    	LitVirtualPlaneElectron<T>& vp2 = layout.virtualPlanes[ivp+1];
+
+    	LitRK4ExtrapolationElectron(par, vp2.Z, vp1.fieldSlice, vp1.fieldSliceMid, vp2.fieldSlice);
+    	LitAddMaterialElectron(par, vp2.material);
+    }
+
+	for (unsigned char isg = 0; isg < layout.GetNofStationGroups(); isg++) {
+		LitStationGroupElectron<T>& stationGroup = layout.stationGroups[isg];
+
+	    for (unsigned char ist = 0; ist < stationGroup.GetNofStations(); ist++) {
+	    	LitStationElectron<T>& station = stationGroup.stations[ist];
+
+	    	LitLineExtrapolation(par, station.Z);
+
+	    	for (unsigned char im = 0; im < station.GetNofMaterialsBefore(); im++)
+	    		LitAddMaterialElectron(par, station.materialsBefore[im]);
+
+	    	LitPixelHit<T>* hit = track.hits[ihit];
+	        LitFiltration(par, *hit);
+			ihit++;
+
+			for (unsigned char im = 0; im < station.GetNofMaterialsAfter(); im++)
+				LitAddMaterialElectron(par, station.materialsAfter[im]);
+	    }
+	}
+}
+
+#endif /* LITTRACKFITTERELECTRON_H_ */

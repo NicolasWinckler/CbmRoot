7bf4c31b8776e10897cb14bb30184e79374008e6
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2010-09-20 14:39:39 +0000

    testing scripts
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9895 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/rich/run/sim.sh
+++ macro/rich/run/sim.sh
@@ -4,34 +4,11 @@ cd $MYBUILDDIR
 cd -
 
 
-#root -b -q "./global_sim.C($NEVENTS)"
+root -b -q "./run_sim.C($NEVENTS)"
 
-#root -b -q "./global_hits.C($NEVENTS)"
+root -b -q "./run_reco.C($NEVENTS)"
 
-#root -b -q "./global_tracking.C($NEVENTS)"
+#root -b -q "./run_reco_rich.C($NEVENTS)"
 
-root -b -q "../rich/run/run_reco_rich.C($NEVENTS)"
-
-#root -b -q "../rich/electronId/run_electrons_qa.C($NEVENTS)"
-
-#root -b -q "./global_reco.C($NEVENTS)"
-
-
-#FOR TRD TEST
-
-#for X in 100 200 300 400 500 600 700 800 900 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 30000 50000 70000 100000; do
-#for X in 1000; do
-#	export TRDHITERR=$X
-#	export GLOBALHITSFILE=$DIR/global.hits.trd$TRDHITERR.0000.root
-#	export GLOBALTRACKSFILE=$DIR/global.tracks.trd$TRDHITERR.0000.root
-#	export GLOBALTRACKSIDEALFILE=$DIR/global.tracks.ideal.trd$TRDHITERR.0000.root
-#	export PROPANAFILE=$DIR/propagation.ana.trd$TRDHITERR.0000.root
-#	export IMAGEDIR=./test/trdstd_urqmd/trd$TRDHITERR/
-#	rm -r $IMAGEDIR
-#	mkdir $IMAGEDIR
-#	root -b -q "./global_hits.C($NEVENTS)"
-#	root -b -q "./global_tracking.C($NEVENTS)"
-#	root -b -q "./global_reco_ideal.C($NEVENTS)"
-#	root -b -q "./prop_ana.C($NEVENTS)"
-#done
+#root -b -q "../electronId/run_electrons_qa.C($NEVENTS)"
 

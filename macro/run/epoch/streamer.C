2615080538d87d3c901fed069f001f13f203e7a5
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2010-10-05 13:58:00 +0000

    Macro to run CbmMCStreamer task
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@9995 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/run/epoch/streamer.C
@@ -0,0 +1,37 @@
+void streamer(Int_t nEvents=5){
+  TString dir = "data";
+  TString inFile         = dir + "/mc.root";
+  TString parFile        = dir + "/param.root";
+  TString outFile        = dir + "/epoch.root";
+  Double_t interactionRate = 1.e7; // Hz
+  Int_t beamProfile = 1;           // 1 - exponential
+  Double_t epochLength = 10000;    // ns
+  
+  
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+  basiclibs();
+  gSystem->Load("libGeoBase");
+  gSystem->Load("libParBase");
+  gSystem->Load("libBase");
+  gSystem->Load("libCbmBase");
+  gSystem->Load("libCbmData");
+  gSystem->Load("libRun");
+  
+  CbmRunAna *fRun= new CbmRunAna();
+  fRun->SetInputFile(inFile);
+  fRun->SetOutputFile(outFile);
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open(parFile.Data());
+  rtdb->setFirstInput(parIo1);
+  rtdb->setOutput(parIo1);
+  rtdb->saveOutput();
+
+  fRun->SetAsync(kTRUE);
+  CbmMCStreamer* streamer = new CbmMCStreamer(interacionRate,beamProfile,epochLength);
+  fRun->AddTask(streamer);
+  
+  fRun->Init();
+  fRun->Run(nEvents);
+}

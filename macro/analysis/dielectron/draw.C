74f62d0d2e6b4bb0008e35486edd8dcb205e73e0
ebelolap <ebelolap@0381ead4-6506-0410-b988-94b70fbc4730>
2010-10-27 11:32:39 +0000

    new histogramms
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@10191 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/analysis/dielectron/draw.C
+++ macro/analysis/dielectron/draw.C
@@ -8,8 +8,32 @@ void draw(){
     gROOT->SetStyle("Plain");
     gStyle->SetPalette(1,0);
 
-    TCanvas *c1 = new TCanvas("c1","c1",600,600);
+    TCanvas *c1 = new TCanvas("c1","c1",1000,500);
+    c1->Divide(2,1);
+    c1->cd(1);
     fh_mc_signal_pty->Draw("COLZ");
+    c1->cd(2);
+    fh_acc_signal_pty->Draw("COLZ");
+
+    TCanvas *c2 = new TCanvas("c2","c2",500,500);
+    fh_acc_signal_pty_eff->Draw("COLZ");
+
+    TCanvas *c3 = new TCanvas("c3", "c3", 1000, 500);
+    c3->Divide(2,1);
+    c3->cd(1);
+    fh_mc_signal_mom->SetLineColor(kRed);
+    fh_mc_signal_mom->Draw();
+    fh_acc_signal_mom->Draw("same");
+    c3->cd(2);
+   // fh_acc_signal_mom_eff->SetLineColor(kGreen);
+    fh_acc_signal_mom_eff->Draw();
+
+    TCanvas *c4 = new TCanvas("c4", "c4", 500, 500);
+    fh_mc_signal_minv->SetLineColor(kRed);
+    fh_mc_signal_minv->Draw();
+    fh_acc_signal_minv->Draw("same");
+    
+
 }
 
 

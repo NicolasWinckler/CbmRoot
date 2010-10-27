c521cdacfd4227909c2f45be5da7253a9d3f36f8
ebelolap <ebelolap@0381ead4-6506-0410-b988-94b70fbc4730>
2010-10-27 09:24:27 +0000

    pty histo added
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@10187 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/analysis/dielectron/draw.C
+++ macro/analysis/dielectron/draw.C
@@ -1,13 +1,15 @@
-#include "../../littrack/utils/CbmLitDrawHist.cxx"
-#include "../../littrack/utils/CbmLitUtils.cxx"
+#include "../../../littrack/utils/CbmLitDrawHist.cxx"
+#include "../../../littrack/utils/CbmLitUtils.cxx"
 
 void draw(){
-    TFile *file = new TFile("");
+    TFile *file = new TFile("/lustre/cbm/user/ebelolap/oct10/rho0/25gev/100_field/analysis.dilep.0000.root");
     gStyle->SetHistLineWidth(3);
-	SetStyles();
+	//SetStyles();
+    gROOT->SetStyle("Plain");
+    gStyle->SetPalette(1,0);
 
-    TCanvas *c1 = new TCanvas("c1","c1",400,600);
-
+    TCanvas *c1 = new TCanvas("c1","c1",600,600);
+    fh_mc_signal_pty->Draw("COLZ");
 }
 
 

48d9e630494f5bb4866ec0ec880af77b8e7792e0
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2011-01-06 14:13:21 +0000

    muchlibs updates. cbmrootlibs macro removed
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@10598 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/much/muchlibs.C
+++ macro/much/muchlibs.C
@@ -4,18 +4,23 @@ void muchlibs(){
   gSystem->Load("libBase");
   gSystem->Load("libCbmBase");
   gSystem->Load("libCbmData");
+  gSystem->Load("libCbmGenerators");
+  gSystem->Load("libRun");
   gSystem->Load("libField");
   gSystem->Load("libGen");
   gSystem->Load("libPassive");
-  gSystem->Load("libMvd");
+  gSystem->Load("libTrkBase");
+  gSystem->Load("libGeane");
   gSystem->Load("libSts");
   gSystem->Load("libRich");
+  gSystem->Load("libMuch");
   gSystem->Load("libTrd");
   gSystem->Load("libTof");
   gSystem->Load("libEcal");
   gSystem->Load("libGlobal");
   gSystem->Load("libKF");
   gSystem->Load("libL1");
-  gSystem->Load("libMuch");
+  gSystem->Load("libLittrack");
+  gSystem->Load("libMinuit2");
   gSystem->Load("libAnalysis");
 }

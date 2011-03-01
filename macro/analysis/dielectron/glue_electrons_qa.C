7f161ec32fe3d3620b44703371854e3b87671279
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2011-03-01 15:56:58 +0000

    glue macros updated
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@11017 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/analysis/dielectron/glue_electrons_qa.C
+++ macro/analysis/dielectron/glue_electrons_qa.C
@@ -1,14 +1,38 @@
-#include "../../../rich/utils/hadd.C"
-
-void glue_electrons_qa ()
+void glue_histo ()
 {
-    TString filePath = "/lustre/cbm/user/ebelolap/oct10/urqmd_rho0/25gev/100_field/real/elid.qa";//.0000.root";
+    TString filePath = "/lustre/cbm/user/ebelolap/oct10/urqmd_rho0/25gev/100_field/real/elid.qa";
     TString outFileName = filePath +".all.root";
     Int_t nEvents = 200;   // number of files to be added
 
-    TList *fileList = new TList();
-    TFile *target;
-    target = TFile::Open( outFileName, "RECREATE" );
+    TList* histList = new TList();
+
+//signal electron acceptance
+	histList->Add( new TH1D("fh_mc_el", "fh_mc_el;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_acc_sts_el", "fh_acc_sts_el;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_acc_rich_el", "fh_acc_rich_el;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_acc_trd_el", "fh_acc_trd_el;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_acc_tof_el", "fh_acc_tof_el;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+
+//signal electron matching
+	histList->Add( new TH1D("fh_match_rich", "fh_match_rich;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_match_trd", "fh_match_trd;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_match_tof", "fh_match_tof;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+
+//ELECTRON IDENTIFICATION AND PION SUPPRESSION
+	histList->Add( new TH1D("fh_elid_rich", "fh_elid_rich;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_elid_trd", "fh_elid_trd;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_elid_tof", "fh_elid_tof;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+
+	histList->Add( new TH1D("fh_acc_pi", "fh_acc_pi;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_pi_as_el_rich", "fh_pi_as_el_rich;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_pi_as_el_trd", "fh_pi_as_el_trd;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+	histList->Add( new TH1D("fh_pi_as_el_tof", "fh_pi_as_el_tof;momentum, GeV/c;Entries", nofBinsMom,0,15) );
+
+//test distributions
+	histList->Add( new TH1D("fh_nof_global", "fh_nof_global;nof global tracks/event;Entries", nofBinsMom,0,1000) );
+	histList->Add( new TH1D("fh_nof_sts_tracks", "fh_nof_sts_tracks;nof STS tracks/event;Entries", nofBinsMom,0,1000) );
+	histList->Add( new TH1D("fh_nof_rich_rings", "fh_nof_rich_rings;nof RICH rings/event;Entries", nofBinsMom,0,100) );
+	histList->Add( new TH1D("fh_nof_trd_tracks", "fh_nof_trd_tracks;nof TRD tracks/event;Entries", nofBinsMom,0,1000) );
 
     Int_t nofGoodFiles = 0;
     for(Int_t i = 0; i < nEvents; i++) {
@@ -18,26 +42,23 @@ void glue_electrons_qa ()
         cout << "-I- " + fileName << endl;
         TFile* file = new TFile(fileName);
         if (file->IsZombie()) continue;
-        file->Close();
 
-        fileList->Add( file);//TFile::Open(fileName) );
+        for (Int_t iH = 0; iH < histList->GetEntries(); iH++){
+        	TH1* h = (TH1*)histList->At(iH);
+        	h->Add((TH2D*) file->Get(h->GetName()));
+        }
 
         nofGoodFiles++;
+        file->Close();
     }
 
-    MergeRootfile( target, fileList );
-
-
-
-    /*
     Double_t scale = 1./(Double_t) nofGoodFiles;
-
-    fh_mc_signal_pty->Scale(scale);
-
-///WRITE HISTOGRAMMS TO OUTPUT FILE
     TFile* outFile = new TFile(outFileName, "RECREATE");
-    fh_mc_signal_pty->Write();
-*/
-    cout <<"nofGoodFiles = " << nofGoodFiles << endl;
+    for (Int_t iH = 0; iH < histList->GetEntries(); iH++){
+    	TH1* h = (TH1*)histList->At(iH);
+        h->Scale(scale);
+        h->Write();
+    }
 
-} 
+    cout <<"nofGoodFiles = " << nofGoodFiles << endl;
+}

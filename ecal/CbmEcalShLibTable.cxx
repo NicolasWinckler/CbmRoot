8bdd41587984e75d9f6b2380a5fb414e8172d433
prokudin <prokudin@0381ead4-6506-0410-b988-94b70fbc4730>
2011-04-12 08:06:56 +0000

    New version of reco and id
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@11482 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ ecal/CbmEcalShLibTable.cxx
@@ -0,0 +1,100 @@
+#include "CbmEcalShLibTable.h"
+
+#include "CbmEcalShLibRecord.h"
+#include "CbmEcalInf.h"
+
+#include "FairRootManager.h"
+
+#include "TSystem.h"
+#include "TString.h"
+
+#include <iostream>
+
+using namespace std;
+
+CbmEcalShLibTable::CbmEcalShLibTable(const char* name, Int_t verb)
+  : CbmEcalShLib(name, verb)
+{
+  Int_t i;
+
+  for(i=0;i<10;i++)
+    fRec[i]=NULL;
+}
+
+void CbmEcalShLibTable::Add(const char* name)
+{
+  TString str=name;
+  CbmEcalShLibRecord* rec;
+  static CbmEcalInf* inf=CbmEcalInf::GetInstance(NULL);
+  if (inf==NULL)
+  {
+    Fatal("CbmEcalShLibTable", "Can't find CbmEcalInf in the system.");
+    return;
+  }
+  Double_t msize;
+  Int_t num;
+
+  gSystem->ExpandPathName(str);
+  rec=new CbmEcalShLibRecord(str, fVerbose);
+  msize=inf->GetModuleSize();
+  num=(Int_t)((msize+0.001)/rec->CellSize());
+  if (num<0||num>9)
+  {
+    Fatal("CbmEcalShLibTable", "Wrong cell number. CellSize is %d.", rec->CellSize());
+    return;
+  }
+  fRec[num]=rec;
+}
+
+InitStatus CbmEcalShLibTable::Init()
+{
+  FairRootManager* fManager=FairRootManager::Instance();
+  if (fManager==NULL)
+  {
+    Fatal("Init", "Can't find IOManager in the system.");
+    return kFATAL;
+  }
+  fManager->Register("EcalShLib", "ECAL", this, kFALSE);
+  for(list<TString>::const_iterator p=fFiles.begin(); p!=fFiles.end();++p)
+    Add((*p).Data());
+
+  return kSUCCESS;
+}
+
+Float_t CbmEcalShLibTable::GetResponse(Float_t x, Float_t y, Float_t phi, Float_t theta, Float_t e, Int_t celltype)
+{
+//  Info("Getresponse", "Here");
+  if (fRec[celltype]==NULL)
+    return -1111;
+  Float_t tx=x;
+  Float_t ty=y;
+  Float_t tphi=phi;
+  Float_t t;
+  if (tphi>180.0)
+  {
+    tphi=360.0-tphi;
+    ty=-ty;
+  }
+  if (tphi>90.0)
+  {
+    tphi=180.0-tphi;
+    tx=-tx;
+  }
+  if (tphi>45.0)
+  {
+    tphi=90.0-tphi;
+    t=tx;
+    tx=ty;
+    ty=t;
+  }
+
+  return fRec[celltype]->GetVal(tx, ty, tphi, theta, e)*e;
+}
+
+CbmEcalShLibTable::~CbmEcalShLibTable()
+{
+  Int_t i;
+  for(i=0;i<10;i++)
+    delete fRec[i];
+}
+ClassImp(CbmEcalShLibTable)
---
+++ ecal/CbmEcalShLibTable.h
@@ -0,0 +1,42 @@
+#ifndef CBMECALSHLIBTABLE_H
+#define CBMECALSHLIBTABLE_H
+
+#include "CbmEcalShLib.h"
+
+#include "FairTask.h"
+
+#include <list>
+
+class CbmEcalShLibRecord;
+
+class CbmEcalShLibTable : public CbmEcalShLib
+{
+public:
+  CbmEcalShLibTable(const char* name, Int_t verb);
+  void AddFile(const char* name) {fFiles.push_back(name);}
+  CbmEcalShLibRecord* GetRec(Int_t i) const;
+
+  // Response to photon with energy e, polar angle phi (rad), impact angle
+  // theta (rad). X, Y are position vs. photons impact point
+  // Cell is a size os cell in cm.
+  virtual Float_t GetResponse(Float_t x, Float_t y, Float_t phi, Float_t theta, Float_t e, Int_t celltype=0);
+  virtual InitStatus Init();
+  void Exec(Option_t* exec) {};
+  void Finish() {};
+  virtual ~CbmEcalShLibTable();
+private:
+  void Add(const char* name);
+
+  std::list<TString> fFiles;
+  CbmEcalShLibRecord* fRec[10];
+
+  ClassDef(CbmEcalShLibTable, 1);
+};
+
+inline CbmEcalShLibRecord* CbmEcalShLibTable::GetRec(Int_t i) const
+{
+  if (i<0||i>9) return NULL;
+  return fRec[i];
+}
+
+#endif

160ac6f39fa4fd8b868a6f1da83e8c06ad64ee01
prokudin <prokudin@0381ead4-6506-0410-b988-94b70fbc4730>
2011-04-12 08:06:03 +0000

    New version of reco and id
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@11480 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ ecal/CbmEcalShLibRecord.cxx
@@ -0,0 +1,123 @@
+#include "CbmEcalShLibRecord.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH2D.h"
+
+#include <iostream>
+
+using namespace std;
+
+CbmEcalShLibRecord::CbmEcalShLibRecord(const char* filename, Int_t verb)
+{
+  TFile* f=new TFile(filename);
+  TTree* info;
+  TTree* data;
+  Float_t t;
+  Int_t i;
+
+  fVerbose=verb;
+  fE=NULL;
+  fTheta=NULL;
+  fPhi=NULL;
+
+  Info("CbmEcalShLibRecord", "Loading %s...", filename);
+  if (f->IsZombie())
+  {
+    Fatal("CbmEcalShLibRecord", "File %s not found.", filename);
+    return;
+  }
+  info=(TTree*)f->Get("info");
+  info->SetBranchAddress("nx", &fNX);
+  info->SetBranchAddress("startx", &fStartX);
+  info->SetBranchAddress("stepx", &fStepX);
+  info->SetBranchAddress("ny", &fNY);
+  info->SetBranchAddress("starty", &fStartY);
+  info->SetBranchAddress("stepy", &fStepY);
+  info->SetBranchAddress("ne", &fNE);
+  info->SetBranchAddress("nphi", &fNPhi);
+  info->SetBranchAddress("ntheta", &fNTheta);
+  info->SetBranchAddress("n", &fN);
+  info->SetBranchAddress("cellsize", &fCellSize);
+  info->GetEntry(0);
+  if (fN!=fNX*fNY*fNE*fNPhi*fNTheta)
+  {
+    Fatal("CbmEcalShLibRecord", "Consistency check failed.");
+    return;
+  }
+  fE=new Float_t[fNE];
+  fTheta=new Float_t[fNTheta];
+  fPhi=new Float_t[fNPhi];
+  info->SetBranchAddress("e", fE);
+  info->SetBranchAddress("phi", fPhi);
+  info->SetBranchAddress("theta", fTheta);
+  info->GetEntry(0);
+  fR=new Float_t[fN];
+  data=(TTree*)f->Get("shlib");
+  data->SetBranchAddress("dta", &t);
+  if (fN!=data->GetEntries())
+  {
+    Fatal("CbmEcalShLibRecord", "Info and data trees inconsitant.");
+    return;
+  }
+  for(i=0;i<fN;i++)
+  {
+    data->GetEntry(i);
+    fR[i]=t;
+  }
+  f->Close();
+
+  if (fVerbose>99)
+  {
+    cout << "Information tree read." << endl;
+    cout << "	n	= " << fN << endl;
+    cout << "	cellsize= " << fCellSize << endl;
+    cout << "	nx	= " << fNX << endl;
+    cout << "	startx	= " << fStartX << endl;
+    cout << "	stepx	= " << fStepX << endl;
+    cout << "	ny	= " << fNY << endl;
+    cout << "	starty	= " << fStartY << endl;
+    cout << "	stepy	= " << fStepY << endl;
+    cout << "	nphi	= " << fNPhi << " : (";
+    for(i=0;i<fNPhi;i++)
+      if (i!=0) cout << ", " << fPhi[i]; else cout << fPhi[i];
+    cout << ")" << endl; 
+    cout << "	ntheta	= " << fNTheta << " : (";
+    for(i=0;i<fNTheta;i++)
+      if (i!=0) cout << ", " << fTheta[i]; else cout << fTheta[i];
+    cout << ")" << endl; 
+    cout << "	ne	= " << fNE << " : (";
+    for(i=0;i<fNE;i++)
+      if (i!=0) cout << ", " << fE[i]; else cout << fE[i];
+    cout << ")" << endl; 
+  }
+}
+
+void CbmEcalShLibRecord::DrawTest(Float_t phi, Float_t theta, Float_t e)
+{
+  TH2D* h;
+  Float_t x;
+  Float_t y;
+  Int_t ix;
+  Int_t iy;
+  h=new TH2D("ht","", fNX, fStartX, fStartX+fNX*fStepX, fNY, fStartY, fStartY+fStepY*fNY);
+  h->SetStats(kFALSE);
+  for(ix=0;ix<fNX;ix++)
+  for(iy=0;iy<fNX;iy++)
+  {
+    x=(ix+0.5)*fStepX+fStartX;
+    y=(iy+0.5)*fStepY+fStartY;
+    h->SetBinContent(h->FindBin(x, y), GetVal(x, y, phi, theta, e));
+  }
+  h->DrawCopy("colz");
+  delete h;
+}
+CbmEcalShLibRecord::~CbmEcalShLibRecord()
+{
+  delete fR;
+  delete fE;
+  delete fTheta;
+  delete fPhi;
+}
+
+ClassImp(CbmEcalShLibRecord)
---
+++ ecal/CbmEcalShLibRecord.h
@@ -0,0 +1,122 @@
+#ifndef CBMECALSHLIBRECORD_H
+#define CBMECALSHLIBRECORD_H
+
+#include "TObject.h"
+
+class CbmEcalShLibRecord : public TObject
+{
+public:
+  CbmEcalShLibRecord(const char* filename, Int_t verb=0);
+  Float_t CellSize() const {return fCellSize;}
+  Float_t GetVal(Float_t x, Float_t y, Float_t phi, Float_t theta, Float_t e) const;
+  void DrawTest(Float_t phi, Float_t theta, Float_t e);
+  virtual ~CbmEcalShLibRecord();
+private:
+  Float_t GetValX(Int_t x, Float_t y, Float_t phi, Float_t theta, Float_t e) const;
+  Float_t GetValXY(Int_t x, Int_t y, Float_t phi, Float_t theta, Float_t e) const;
+  Float_t GetValXYTheta(Int_t x, Int_t y, Float_t phi, Int_t theta, Float_t e) const;
+  Float_t GetValXYThetaE(Int_t x, Int_t y, Float_t phi, Int_t theta, Int_t e) const;
+
+  Float_t fCellSize;
+  Int_t fNE;
+  Float_t* fE;			//!
+
+  Int_t fNTheta;
+  Float_t* fTheta;		//!
+
+  Int_t fNPhi;
+  Float_t* fPhi;		//!
+  
+  Int_t fNX;
+  Float_t fStepX;
+  Float_t fStartX;
+
+  Int_t fNY;
+  Float_t fStepY;
+  Float_t fStartY;
+
+  Int_t fVerbose;
+
+  Int_t fN;
+  Float_t* fR;			//!
+
+  ClassDef(CbmEcalShLibRecord, 1);
+};
+
+inline Float_t CbmEcalShLibRecord::GetValXYThetaE(Int_t x, Int_t y, Float_t phi, Int_t theta, Int_t e) const
+{
+  static Float_t dphi=fPhi[1]-fPhi[0];
+//  static Float_t oldphi=-1111;
+  Float_t t=phi/dphi;
+  Int_t n=(Int_t)(t);
+  Float_t d=t-n;
+  Int_t n1=x+fNX*(y+fNY*(n+fNPhi*(theta+fNTheta*e)));
+  Int_t n2=n1+fNX*fNY;
+/*
+  if (oldphi!=phi)
+  {
+    oldphi=phi;
+    cout << phi << "	" << n << " --- " << n1 << endl;
+  }
+  cout << x << ", " << y << ", " << n << ", " << theta << ", " << e << endl;
+*/
+  return fR[n1]*(1.0-d)+fR[n2]*d;
+}
+
+inline Float_t CbmEcalShLibRecord::GetValXYTheta(Int_t x, Int_t y, Float_t phi, Int_t theta, Float_t e) const
+{
+  Int_t i=0;
+  for(;i<fNE;i++)
+    if (fE[i]>=e)
+      break;
+//  cout << i << endl;
+  if (i==fNE) return GetValXYThetaE(x, y, phi, theta, fNE-1);
+  if (i==0) return GetValXYThetaE(x, y, phi, theta, 0);
+  Float_t t=fE[i]-fE[i-1];
+  Float_t d=(fE[i]-e)/t;
+  return GetValXYThetaE(x, y, phi, theta, i)*(1.0-d)+GetValXYThetaE(x, y, phi, theta, i-1)*d;
+}
+
+inline Float_t CbmEcalShLibRecord::GetValXY(Int_t x, Int_t y, Float_t phi, Float_t theta, Float_t e) const 
+{
+  Int_t i=0;
+  for(;i<fNTheta;i++)
+    if (fTheta[i]>=theta)
+      break;
+//  if (i==fNTheta||i==0) return 0;
+  if (i==fNTheta) return GetValXYTheta(x, y, phi, fNTheta-1, e);
+  if (i==0) return GetValXYTheta(x, y, phi, 0, e);
+  Float_t t=fTheta[i]-fTheta[i-1];
+  Float_t d=(fTheta[i]-theta)/t;
+//  cout << i << " --- " << cTheta[i] <<" "<< d << endl;
+  return GetValXYTheta(x, y, phi, i, e)*(1.0-d)+GetValXYTheta(x, y, phi, i-1, e)*d;
+}
+
+inline Float_t CbmEcalShLibRecord::GetValX(Int_t x, Float_t y, Float_t phi, Float_t theta, Float_t e) const
+{
+  Float_t t=(y-fStartY)/fStepY;
+  Int_t n=(Int_t)(t);
+  if (n<0||n>=fNY) return 0;
+  Float_t d=t-n;
+  if (n+1==fNY)
+    return GetValXY(x, n, phi, theta, e)*(1.0-d);
+  else
+    return GetValXY(x, n, phi, theta, e)*(1.0-d)+d*GetValXY(x, n+1, phi, theta, e);
+}
+
+inline Float_t CbmEcalShLibRecord::GetVal(Float_t x, Float_t y, Float_t phi, Float_t theta, Float_t e) const 
+{
+  Float_t t=(x-fStartX)/fStepX;
+  Int_t n=(Int_t)(t);
+  if (n<0||n>=fNX) return 0;
+  Float_t d=t-n;
+  if (n+1==fNX)
+    return GetValX(n, y, phi, theta, e)*(1.0-d);
+  else
+    return GetValX(n, y, phi, theta, e)*(1.0-d)+d*GetValX(n+1, y, phi, theta, e);
+}
+
+
+
+
+#endif

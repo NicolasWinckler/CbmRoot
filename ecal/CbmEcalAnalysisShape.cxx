9b4b75d104b3c562836c51213dbeb45fd710d1b7
prokudin <prokudin@0381ead4-6506-0410-b988-94b70fbc4730>
2011-04-12 08:05:23 +0000

    New version of reco and id
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@11479 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ ecal/CbmEcalAnalysisShape.cxx
@@ -0,0 +1,195 @@
+#include "CbmEcalAnalysisShape.h"
+
+#include "FairRootManager.h"
+
+#include "CbmMCTrack.h"
+
+#include "CbmEcalInf.h"
+#include "CbmEcalPoint.h"
+#include "CbmEcalPointLite.h"
+#include "CbmEcal.h"
+
+#include "TTree.h"
+#include "TMath.h"
+#include "TClonesArray.h"
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+Int_t CbmEcalAnalysisShape::GetNum(Double_t x, Double_t y) const
+{
+  Double_t dx=x; dx+=0.025; dx-=fDX; dx/=fStep;
+  Double_t dy=y; dy+=0.025; dy-=fDY; dy/=fStep;
+  Int_t ix=TMath::FloorNint(dx+0.00001);
+  Int_t iy=TMath::FloorNint(dy+0.00001);
+  if (fVerbose>99)
+  {
+    cout << "(x, y)=(" << x << ", " << y << "), (dx, dy)=(" << dx << ", " << dy << "), (ix, iy)=(" << ix << ", " << iy << ")" << endl; 
+  }
+  if (ix<0) return -1111;
+  if (iy<0) return -1111;
+  if (ix>=fXSize) return -1111;
+  if (iy>=fYSize) return -1111;
+  return ix+iy*fXSize;
+}
+/** Loop procedure **/
+void CbmEcalAnalysisShape::Exec(Option_t* option)
+{
+  fEv++;
+//  cout << fEv << endl;
+  InitTree();
+  InitE();
+
+  Int_t i;
+  Int_t n;
+  Int_t m;
+  CbmEcalPointLite* p;
+  Bool_t isPS;
+  Int_t ten;
+  Float_t x;
+  Float_t y;
+
+  if (fWriteEach)
+    for(i=0;i<fSize;i++)
+      fShape[i]=0.0;
+  n=fP->GetEntriesFast();
+  for(i=0;i<n;i++)
+  {
+    p=(CbmEcalPointLite*)fP->At(i);
+    if (!p) continue;
+    isPS=CbmEcal::GetCellCoord(p->GetDetectorID(), x, y, ten);
+    if (ten!=0) continue;
+    m=GetNum(x-fStartX, y-fStartY);
+    if (m<0)
+    {
+//      cout << m << endl;
+      continue;
+    }
+    fShape[m]+=p->GetEnergyLoss();
+  }
+  if (fWriteEach)
+  {
+    for(i=0;i<fSize;i++)
+      fShapeOut[i]=fShape[i];
+    fTree->Fill();
+  }
+} 
+
+
+CbmEcalAnalysisShape::CbmEcalAnalysisShape(const char* name, const Int_t iVerbose, const char* fname)
+  : FairTask(name, iVerbose)
+{
+  fEv=0;
+  fName=fname;
+  fDX=-5;
+  fDY=-5;
+  fStep=0.1;
+/*
+  fStartX=0.05;
+  fStartY=30.05;
+*/
+  fInf=CbmEcalInf::GetInstance(fName);
+  fXSize=25*7;
+  fYSize=25*7;
+  fTree=NULL;
+  fWriteEach=kFALSE;
+}
+
+/** Initing routine **/
+InitStatus CbmEcalAnalysisShape::Init()
+{
+  Int_t i;
+  FairRootManager* fManager = FairRootManager::Instance();
+
+  fMC=(TClonesArray*)fManager->GetObject("EcalPoint");
+  if (!fMC)
+  {
+    Fatal("Init()", "Can't find array of calorimeter points in the system.");
+    return kFATAL;
+  }
+  fP=(TClonesArray*)fManager->GetObject("EcalPointLite");
+  if (!fP)
+  {
+    Fatal("Init()", "Can't find array of lite points in the system.");
+    return kFATAL;
+  }
+  fMCTracks=(TClonesArray*)fManager->GetObject("MCTrack");
+  if (!fMCTracks)
+  {
+    Fatal("Init()", "Can't find array of MC tracks in the system.");
+    return kFATAL;
+  }
+  fStep=fInf->GetCellSize();
+  Info("Init", "Using %f cell size", fStep);
+  fSize=fXSize*fYSize;
+  fShape=new Double_t[fSize];
+  fShapeOut=new Float_t[fSize];
+  for(i=0;i<fSize;i++)
+    fShape[i]=0.0;
+  fE=-1111;
+  fPhi=-1111;
+  fTheta=-1111;
+
+  return kSUCCESS;
+}
+
+void CbmEcalAnalysisShape::InitTree()
+{
+  if (fTree) return;
+  fTree=new TTree("shape","shover shape");
+  fTree->Branch("e", &fE, "e/D");
+  fTree->Branch("phi", &fPhi, "phi/D");
+  fTree->Branch("theta", &fTheta, "theta/D");
+  fTree->Branch("dx", &fDX, "dx/D");
+  fTree->Branch("dy", &fDY, "dy/D");
+  fTree->Branch("step", &fStep, "step/D");
+  fTree->Branch("xsize", &fXSize, "xsize/I");
+  fTree->Branch("ysize", &fYSize, "ysize/I");
+  TString nm; nm="shape["; nm+=fSize; nm+="]/F";
+  fTree->Branch("shape", fShapeOut, nm);
+}
+
+void CbmEcalAnalysisShape::InitE()
+{
+  CbmEcalPoint* pt=(CbmEcalPoint*)fMC->At(0);
+  CbmMCTrack* mc=(CbmMCTrack*)fMCTracks->At(pt->GetTrackID());
+  TLorentzVector v;
+  mc->Get4Momentum(v);
+  fE=v.E();
+  fPhi=v.Phi();
+  fTheta=v.Theta();
+  fStartX=pt->GetX();
+  fStartY=pt->GetY();
+ 
+  if (fVerbose>9)
+    cout << "start: (" << fStartX << ", " << fStartY << ")" << endl;
+}
+/** Finishing routine **/
+void CbmEcalAnalysisShape::Finish()
+{
+  Int_t i;
+
+  if (fTree)
+  {
+    if (!fWriteEach)
+    {
+      for(i=0;i<fSize;i++)
+      {
+	fShape[i]/=fEv;
+        fShapeOut[i]=fShape[i];
+      }
+      fTree->Fill();
+    }
+    fTree->Write();
+  }
+}
+
+CbmEcalAnalysisShape::~CbmEcalAnalysisShape()
+{
+  delete fShape;
+  delete fShapeOut;
+}
+
+ClassImp(CbmEcalAnalysisShape)
---
+++ ecal/CbmEcalAnalysisShape.h
@@ -0,0 +1,78 @@
+#ifndef CBMECALANALYSISSHAPE_H
+#define CBMECALANALYSISSHAPE_H
+
+#include "FairTask.h"
+
+#include "TString.h"
+
+#include <list>
+
+class TTree;
+class CbmEcalInf;
+class TClonesArray;
+
+class CbmEcalAnalysisShape: public FairTask
+{
+public:
+  CbmEcalAnalysisShape(const char* name, const Int_t iVerbose, const char* fname);
+public:
+  /** Default constructor **/
+  CbmEcalAnalysisShape() {};
+
+  /** Initing routine **/
+  virtual InitStatus Init();
+
+  /** Loop procedure **/
+  virtual void Exec(Option_t* option);
+
+  /** Finishing routine **/
+  virtual void Finish();
+
+  /** Destructor **/
+  virtual ~CbmEcalAnalysisShape();
+/*
+  void SetE(Double_t e)  {fE=e;}
+  void SetPhi(Double_t phi)  {fPhi=phi;}
+  void SetTheta(Double_t theta)  {fTheta=theta;}
+*/
+  void SetXSize(Int_t xsize) {fXSize=xsize;}
+  void SetYSize(Int_t ysize) {fYSize=ysize;}
+  void SetDX(Double_t dx) {fDX=dx;}
+  void SetDY(Double_t dy) {fDY=dy;}
+//  void SetStartX(Double_t startx) {fStartX=startx;}
+//  void SetStartY(Double_t starty) {fStartY=starty;}
+  void SetWriteEach(Bool_t writeeach) {fWriteEach=writeeach;}
+  Int_t GetNum(Double_t x,Double_t y) const;
+private:
+  void InitTree();
+  void InitE();
+  TString fName;
+
+  Double_t fE;
+  Double_t fPhi;
+  Double_t fTheta;
+  Double_t* fShape;			//!
+  Float_t* fShapeOut;			//!
+  Int_t fXSize;
+  Int_t fYSize;
+  Int_t fSize;
+  Double_t fDX;
+  Double_t fDY;
+  Double_t fStep;
+  Double_t fStartX;
+  Double_t fStartY;
+  Bool_t fWriteEach;
+
+  Int_t fEv;
+
+  TTree* fTree;				//!
+  CbmEcalInf* fInf;			//!
+  TClonesArray* fMC;			//!
+  TClonesArray* fP;			//!
+  TClonesArray* fMCTracks;		//!
+
+  ClassDef(CbmEcalAnalysisShape,1)
+};
+
+#endif 
+

94919904b3575f372db33eb889ac0d6fb4e73558
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2011-05-25 11:26:53 +0000

    much_ana.C macro added (needed for dimuon analysis)
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@12037 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/analysis/much/much_ana.C
+++ macro/analysis/much/much_ana.C
@@ -1,39 +1,40 @@
-void much_ana(char_t* dir = "data", Int_t fileNo=0, Int_t nEvents=10){
-  TString inFile         = dir + Form("/%02i/",fileNo) + "mc.root";
-  TString stsFile        = dir + Form("/%02i/",fileNo) + "sts.reco.root";
-  TString muchHitsFile   = dir + Form("/%02i/",fileNo) + "much.hits.root";
-  TString muchTracksFile = dir + Form("/%02i/",fileNo) + "much.tracks.root";
-  TString outFile        = dir + Form("/%02i/",fileNo) + "ana.root";
-  TString histoFile      = dir + Form("/%02i/",fileNo) + "anaHisto.root";
-  
+void much_ana(TString dir="data", int nEvents=1000){
+  TString mcFile   = Form("%s/mc.root",dir.Data());
+  TString parFile  = Form("%s/par.root",dir.Data());
+  TString stsFile  = Form("%s/sts.root",dir.Data());
+  TString recFile  = Form("%s/rec.root",dir.Data());
+  TString outFile  = Form("%s/ana.root",dir.Data());
+  TString digiFile = Form("%s/much_digi.root",dir.Data());
+
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
   basiclibs();
   gROOT->LoadMacro("$VMCWORKDIR/macro/much/muchlibs.C");
   muchlibs();
-  gROOT->LoadMacro("$VMCWORKDIR/analysis/hyperon/hyperon_style.C");
-  hyperon_style();
-
   
   FairRunAna *fRun= new FairRunAna();
-  fRun->SetInputFile(inFile);
+  fRun->SetInputFile(mcFile);
   fRun->AddFriend(stsFile);
-  fRun->AddFriend(muchHitsFile);
-  fRun->AddFriend(muchTracksFile);
+  fRun->AddFriend(recFile);
   fRun->SetOutputFile(outFile);
 
+  TString muchDigiFile = gSystem->Getenv("VMCWORKDIR");
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
   FairParRootFileIo*  parIo1 = new FairParRootFileIo();
-  parIo1->open(gFile);
+  parIo1->open(parFile);
   rtdb->setFirstInput(parIo1);
   rtdb->setOutput(parIo1);
   rtdb->saveOutput();
+  fRun->LoadGeometry();
+  // ------------------------------------------------------------------------
 
-  CbmAnaMuch* ana = new CbmAnaMuch("MuchAnalysis",histoFile);
-  ana->SetVerbose(1);
+  CbmKF* kf = new CbmKF();
+  CbmAnaDimuonAnalysis* ana = new CbmAnaDimuonAnalysis("DimuonAnalysis",digiFile,10);
+  ana->SetVerbose(0);
   ana->SetStsPointsAccQuota(4);
   ana->SetStsTrueHitQuota(0.7);
-  fRun->AddTask(ana);
 
+  fRun->AddTask(kf);
+  fRun->AddTask(ana);
   fRun->Init();
   fRun->Run(0,nEvents);
 }

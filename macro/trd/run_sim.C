cc5d0367f87837d291eed16754834f4f5c996742
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2011-05-27 12:04:05 +0000

    fix path to TRD geometry
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@12088 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/run_sim.C
+++ macro/trd/run_sim.C
@@ -40,10 +40,9 @@ void run_sim(Int_t nEvents = 2)
   TString mvdGeom    = "mvd_standard.geo";
   TString stsGeom    = "sts_standard.geo";
   TString richGeom   = "rich_standard.geo";
-//  TString trdGeom    = "trd_squared_modules_jun10.geo";
 //  TString trdGeom    = "../macro/trd/trd_squared_modules.geo";
-  //TString trdGeom    = "trd_standard.geo";
-  TString trdGeom    = digipar + ".geo";
+//  TString trdGeom    = "trd/trd_v10b.geo";
+  TString trdGeom    = "trd/" + digipar + ".geo";
   TString tofGeom    = "tof_standard.geo";
   //TString ecalGeom   = "ecal_FastMC.geo";
   

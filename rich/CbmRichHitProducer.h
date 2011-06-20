da9d35d987f396b2be348eac459a80b68ea08204
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2011-06-20 11:55:06 +0000

    add cross talk hits in photodetector
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@12418 0381ead4-6506-0410-b988-94b70fbc4730

--- rich/CbmRichHitProducer.h
+++ rich/CbmRichHitProducer.h
@@ -30,105 +30,170 @@ class CbmRichHitProducer : public FairTask {
 
 public:
 
-  /** Default constructor **/
+  /* Default constructor */
   CbmRichHitProducer();
 
-/** Standard constructor
-   *@param pmt_rad      radius of photomultiplier [cm]
-   *@param pmt_dis      distance between PMT tubel [cm]
-   *@param det_type     detector type (choose: 1=Protvino, 2=CsI, 3=Hamamatsu
-   *@param noise        number of noise hits
-   *@param verbose      verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
-   *@param colleff      collection efficiency for photoelectrons in PMT optics
-   *@param s_mirror     additional scattering in mirror: results in smeared point in PMT plane
-   */
-  CbmRichHitProducer(Double_t pmt_rad, Double_t pmt_dist, Int_t det_type, Int_t noise, Int_t verbose, Double_t colleff, Double_t s_mirror);
+  /* Standard constructor
+   *@param pmt_rad Radius of photomultiplier [cm]
+   *@param pmt_dis Distance between PMT tubel [cm]
+   *@param det_type Detector type (choose: 1=Protvino, 2=CsI, 3=Hamamatsu
+   *@param noise Number of noise hits
+   *@param verbose Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
+   *@param colleff Collection efficiency for photoelectrons in PMT optics
+   *@param s_mirror Additional scattering in mirror: results in smeared point in PMT plane*/
+  CbmRichHitProducer(
+        Double_t pmt_rad,
+        Double_t pmt_dist,
+        Int_t det_type,
+        Int_t noise,
+        Int_t verbose,
+        Double_t colleff,
+        Double_t s_mirror);
 
-  /** Constructor with name and title. Puts default parameter values. */
+  /* Constructor with name and title. Puts default parameter values.*/
   CbmRichHitProducer(const char *name, const char *title);
 
-  /** Destructor **/
+  /* Destructor */
   virtual ~CbmRichHitProducer();
 
-  /** Initialization of the task **/
+  /* Initialization of the task */
   virtual void SetParContainers();
+
   virtual InitStatus Init();
 
-  /** Executed task **/
-  virtual void Exec(Option_t* option);
+  /* Executed task
+   * @param option*/
+  virtual void Exec(
+        Option_t* option);
 
-  /** Finish task **/
+  /* Add cross talk hits
+   * @param x X position of the central hit
+   * @param y Y position of the central hit
+   * @param pointInd Index of the Rich Point of the central hit
+   * @param RichDetID ID of the RICH detector*/
+  void AddCrossTalkHits(
+        Double_t x,
+        Double_t y,
+        Int_t pointInd,
+        Int_t RichDetID);
+
+  /* Finish task */
   virtual void Finish();
 
-  /** method AddHit
-   **
-   ** Adds a RichHit to the HitCollection
-   **/
-  void AddHit(TVector3 &posHit, TVector3 &posHitErr,
-	      Int_t detID, Int_t pmtID, Double_t ampl, Int_t index);
+  /* Adds a RichHit to the HitCollection
+   * @param posHit
+   * @param posHitErr
+   * @param detID
+   * @param pmtID
+   * @param ampl
+   * @param index*/
+  void AddHit(
+        TVector3 &posHit,
+        TVector3 &posHitErr,
+	     Int_t detID,
+	     Int_t pmtID,
+	     Double_t ampl,
+	     Int_t index);
 
-  /** method: FindRichHitPosition-> finds hit position in PMT plane **/
-  void FindRichHitPositionSinglePMT(Double_t xPoint, Double_t yPoint,
-			Double_t& xHit, Double_t& yHit, Int_t & pmtID);
-  void FindRichHitPositionMAPMT(Double_t sigma, Double_t xPoint, Double_t yPoint,
-			Double_t& xHit, Double_t& yHit, Int_t & pmtID);
-  void FindRichHitPositionCsI(Double_t xPoint, Double_t yPoint,
-			Double_t& xHit, Double_t& yHit, Int_t & pmtID);
+  /* Finds hit position in PMT plane
+   * @param xPoint
+   * @param yPoint
+   * @param xHit
+   * @param yHit
+   * @param pmtID*/
+  void FindRichHitPositionSinglePMT(
+        Double_t xPoint,
+        Double_t yPoint,
+        Double_t& xHit,
+        Double_t& yHit,
+        Int_t& pmtID);
 
-  Double_t OnePhotonAmplitude(Double_t x);
+  /* Finds hit position in MAMPT plane]
+   * @param sigma
+   * @param xPoint
+   * @param yPoint
+   * @param xHit
+   * @param yHit
+   * @param pmtID*/
+  void FindRichHitPositionMAPMT(
+        Double_t sigma,
+        Double_t xPoint,
+        Double_t yPoint,
+        Double_t& xHit,
+        Double_t& yHit,
+        Int_t & pmtID);
+
+  /* Finds hit position in MAMPT plane]
+   * @param xPoint
+   * @param yPoint
+   * @param xHit
+   * @param yHit
+   * @param pmtID*/
+  void FindRichHitPositionCsI(
+        Double_t xPoint,
+        Double_t yPoint,
+        Double_t& xHit,
+        Double_t& yHit,
+        Int_t& pmtID);
+
+  Double_t OnePhotonAmplitude(
+        Double_t x);
+
   Double_t GetAmplitude();
 
-  void SetPhotoDetPar(Int_t det_type, Double_t& lambda_min, Double_t& lambda_max, Double_t& lambda_step, Double_t efficiency[]);
+  void SetPhotoDetPar(
+        Int_t det_type,
+        Double_t& lambda_min,
+        Double_t& lambda_max,
+        Double_t& lambda_step,
+        Double_t efficiency[]);
 
-
-  /** Set the parameters to the default values. **/
+  /* Set the parameters to the default values. */
   void SetDefaultParameters();
-
-protected:
 
 private:
 
   TClonesArray* fListRICHpts; // RICH MC points
   TClonesArray* fHitCollection; // RICH hits
-  TClonesArray* fListStack;     // Tracks
-  Int_t fNHits;                // Number of hits
-  Int_t fNDoubleHits;           // Number of double hits
+  TClonesArray* fListStack; // Tracks
+  Int_t fNHits; // Number of hits
+  Int_t fNDoubleHits; // Number of double hits
 
-  Double_t c;         // speed of light
-  Double_t h;         // Planck constant
-  Double_t e;         // elementary charge
-  Double_t nrefrac;   // refraction index
-  Int_t detection;    // flag for detection
+  Double_t c; // speed of light
+  Double_t h; // Planck constant
+  Double_t e; // elementary charge
+  Double_t nrefrac; // refraction index
+  Int_t detection; // flag for detection
   Int_t nevents;
 
-  Double_t fDetX;       /** X-coordinate of photodetector */
-  Double_t fDetY;       /** Y-coordinate of photodetector */
-  Double_t fDetZ;       /** Z-coordinate of photodetector */
-  Double_t fDetZ_org;   /** X-coordinate of photodetector (original from parameter file)*/
-  Double_t fDetWidthX;  /** width of photodetector in x */
-  Double_t fDetWidthY;  /** width of photodetector in y */
-
+  Double_t fDetX; // X-coordinate of photodetector
+  Double_t fDetY; // Y-coordinate of photodetector
+  Double_t fDetZ; // Z-coordinate of photodetector
+  Double_t fDetZ_org; // X-coordinate of photodetector (original from parameter file)
+  Double_t fDetWidthX; // width of photodetector in x
+  Double_t fDetWidthY; // width of photodetector in y
 
   TObjArray *fSensNodes;
   TObjArray *fPassNodes;
-  CbmGeoRichPar *fPar;           //!
+  CbmGeoRichPar *fPar;
 
-  /** Verbosity level **/
-  Int_t fVerbose;
+  Int_t fVerbose; // Verbosity level
 
-  /** Parameters of photodetector **/
-  Double_t fPhotomulRadius;   //** radius of photomultiplier */
-  Double_t fPhotomulDist;     //** radius of photomultiplier */
-  Int_t  fDetType;            //** detector type */
-  Int_t  fNoise;              //** number of noise hits */
-  Double_t  fColl;            //** collection efficiency for photoelectrons in PMT optics */
-  Double_t  fSMirror;         //** additinal smearing of MC Points due to light scattering in mirror */
+  // Parameters of photodetector
+  Double_t fPhotomulRadius; // radius of photomultiplier
+  Double_t fPhotomulDist; // radius of photomultiplier
+  Int_t fDetType; // detector type
+  Int_t fNoise; // number of noise hits
+  Double_t fColl; // collection efficiency for photoelectrons in PMT optics
+  Double_t fSMirror; // additinal smearing of MC Points due to light scattering in mirror
 
-  Double_t theta;   //** theta = angle by which photodetector was tilted (around x-axis)*/
-  Double_t phi;   //** phi = angle by which photodetector was tilted (around y-axis)*/
+  Double_t theta; // theta = angle by which photodetector was tilted (around x-axis)
+  Double_t phi; // phi = angle by which photodetector was tilted (around y-axis)
+
+  Double_t fCrossTalkHitProb;// probability of cross talk hit
+  Int_t fNofCrossTalkHits; // Number of cross talk hits
 
   ClassDef(CbmRichHitProducer,1)
-
 };
 
 #endif

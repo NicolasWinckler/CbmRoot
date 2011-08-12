d337f1510fe988d6c5541aa60db2b06480cd88e7
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2011-08-12 11:36:11 +0000

    Add comments.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@13167 0381ead4-6506-0410-b988-94b70fbc4730

--- littrack/parallel/LitTrackParam.h
+++ littrack/parallel/LitTrackParam.h
@@ -1,11 +1,12 @@
-/** LitTrackParam.h
- * @author Andrey Lebedev <andrey.lebedev@gsi.de>
- * @since 2009
- * @version 1.0
+/**
+ * \file LitTrackParam.h
  *
- * Track parameters data class.
- **/
-
+ * \brief Track parameters data class.
+ *
+ * \author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * \date 2009
+ *
+ */
 #ifndef LITTRACKPARAM_H_
 #define LITTRACKPARAM_H_ 1
 
@@ -15,19 +16,43 @@
 namespace lit {
 namespace parallel {
 
+/**
+ * \class LitTrackParam
+ *
+ * \brief Track parameters data class.
+ *
+ * Track parameters consist of
+ * X and Y position of the track,
+ * tx and ty slopes,
+ * q/p charge (+1 or -1) over momentum and
+ * covariance matrix of this elements.
+ *
+ * \author Andrey Lebedev <andrey.lebedev@gsi.de>
+ * \date 2009
+ *
+ */
 template<class T>
 class LitTrackParam
 {
 public:
+   /**
+    * \brief Constructor.
+    */
    LitTrackParam():
       X(0.), Y(0.), Z(0.), Tx(0.), Ty(0.), Qp(0.),
       C0(0.), C1(0.), C2(0.), C3(0.), C4(0.), C5(0.),
       C6(0.), C7(0.), C8(0.), C9(0.), C10(0.), C11(0.),
       C12(0.), C13(0.), C14(0.) {}
 
+   /**
+    * \brief Destructor.
+    */
    virtual ~LitTrackParam() {}
 
-   /* Returns std::string representation for the class */
+   /**
+    * \brief Returns std::string representation of the class.
+    * \return Class representation as std::string.
+    */
    std::string ToString() const {
       return "LitTrackParam: X=" + lit::parallel::ToString<T>(X)
          + " Y=" + lit::parallel::ToString<T>(Y)
@@ -45,15 +70,24 @@ public:
          + lit::parallel::ToString<T>(C13) + " " + lit::parallel::ToString<T>(C14) + "\n";
    }
 
-   /* Operator << for convenient output to std::ostream */
+   /**
+    * \brief Operator << for convenient output to std::ostream.
+    * \return Insertion stream in order to be able to call a succession of insertion operations.
+    */
    friend std::ostream& operator<<(std::ostream& strm, const LitTrackParam& par) {
       strm << par.ToString();
       return strm;
    }
 
 public:
-   T X, Y, Z, Tx, Ty, Qp;
+   T X; // X position of the track
+   T Y; // Y position of the track
+   T Z; // Z position of the track
+   T Tx; // Slope which equals to dx/dz
+   T Ty; // Slope which equals to dy/dz
+   T Qp; // Charge (+1 or -1) over momentum
 
+   // Symetric covariance matrix
    T C0,  C1,  C2,  C3,  C4,
      C5,  C6,  C7,  C8,
      C9,  C10, C11,
@@ -61,8 +95,16 @@ public:
      C14;
 } _fvecalignment;
 
-/* Some typedefs for convenience */
+/**
+ * \typedef LitTrackParam<fvec> LitTrackParamVec
+ * \brief Vector version of \c LitTrackParam.
+ */
 typedef LitTrackParam<fvec> LitTrackParamVec;
+
+/**
+ * \typedef LitTrackParam<fscal> LitTrackParamScal
+ * \brief Scalar version of \c LitTrackParam.
+ */
 typedef LitTrackParam<fscal> LitTrackParamScal;
 
 } // namespace parallel

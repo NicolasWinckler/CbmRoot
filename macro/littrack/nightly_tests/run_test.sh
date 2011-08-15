9e9c2a5ac954ff7bfa4a31966145eabbe1f073b9
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2011-08-15 11:47:03 +0000

    Chanage directory for output ROOT files to /data.local1/andrey/tests/nightly for nightly tests on lxir039 machine.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@13190 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/littrack/nightly_tests/run_test.sh
+++ macro/littrack/nightly_tests/run_test.sh
@@ -15,9 +15,9 @@ cmake ../cbmroot
 . ./config.sh
 make -j6
 
-cp /d/cbm02/andrey/tests/field_v10e.root /d/cbm02/andrey/tests/nightly/trunk/cbmroot/input/field_v10e.root
+cp /d/cbm02/andrey/tests/field_v10e.root /data.local1/andrey/tests/nightly/trunk/cbmroot/input/field_v10e.root
 
-cd /d/cbm02/andrey/tests/nightly/trunk/cbmroot/macro/littrack/nightly_tests/
+cd /data.local1/andrey/tests/nightly/trunk/cbmroot/macro/littrack/nightly_tests/
 . ./run_test_electron.sh
 . ./run_test_muon.sh
 
@@ -26,5 +26,5 @@ cd /u/andrey/web-docs/tests
 rm -r -f $DAY
 mkdir $DAY
 
-cp -r /u/andrey/tests/nightly/trunk/cbmroot/macro/littrack/nightly_tests/results_electron /u/andrey/web-docs/tests/$DAY/results_electron
-cp -r /u/andrey/tests/nightly/trunk/cbmroot/macro/littrack/nightly_tests/results_muon /u/andrey/web-docs/tests/$DAY/results_muon
+cp -r /data.local1/andrey/tests/nightly/trunk/cbmroot/macro/littrack/nightly_tests/results_electron /u/andrey/web-docs/tests/$DAY/results_electron
+cp -r /data.local1/andrey/tests/nightly/trunk/cbmroot/macro/littrack/nightly_tests/results_muon /u/andrey/web-docs/tests/$DAY/results_muon

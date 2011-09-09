6caf9bfbb344ba5b149cc96b1e51ef4550907899
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2011-09-09 15:28:02 +0000

    set trd.v11b as default in whichTrdGeo
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@13396 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/copy_trd_geometries.sh
+++ macro/trd/copy_trd_geometries.sh
@@ -3,7 +3,11 @@
 #
 
 echo \# copy geometries
-cp -v geometry/trd.v11/*.geo      ../../geometry/trd
+cp -v geometry/trd.v11/v11?/*.geo* ../../geometry/trd
+#cp -v geometry/trd.v11/*.geo       ../../geometry/trd
 echo
 echo \# copy digi.par files
-cp -v geometry/trd.v11/*.digi.par ../../parameters/trd
+cp -v geometry/trd.v11/v11?/*.digi.par ../../parameters/trd
+cp -v geometry/trd.v11/v11b/trd_v11b_7.5mm.digi.par ../../parameters/trd/trd_v11b.digi.par
+cp -v geometry/trd.v11/v11a/trd_v11a_7.5mm.digi.par ../../parameters/trd/trd_v11a.digi.par
+#cp -v geometry/trd.v11/*.digi.par ../../parameters/trd

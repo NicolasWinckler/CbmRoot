ffc9d3084d561e2e02d2959c21d8c8faa52e9996
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2011-10-27 07:27:17 +0000

    Change URL of the old CDash server. The link fairroot.gsi.de now points to the new web-page.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@13586 0381ead4-6506-0410-b988-94b70fbc4730

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,13 +3,8 @@
 set(CTEST_PROJECT_NAME "CbmRoot")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
-#SET (CTEST_DROP_METHOD "http")
-#SET (CTEST_DROP_SITE "fairroot.gsi.de")
-#SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-#SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/TriggerSiteDart2AndCDash.cgi")
-
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "fairroot.gsi.de")
+set(CTEST_DROP_SITE "web-docs.gsi.de/~cbmrtwww/")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=CbmRoot")
 set(CTEST_DROP_SITE_CDASH TRUE)
 

3beaf09fc809fb2bc1a08718bade19918b0334d8
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2011-11-24 12:26:30 +0000

    add new histograms and code refactoring
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@13761 0381ead4-6506-0410-b988-94b70fbc4730

--- rich/qa/CbmRichGeoTest.cxx
+++ rich/qa/CbmRichGeoTest.cxx
@@ -5,43 +5,115 @@
 #include "CbmRichRingMatch.h"
 #include "CbmMCTrack.h"
 #include "CbmRichPoint.h"
+#include "CbmRichRingFitterCOP.h"
+#include "CbmRichRingFitterEllipseTau.h"
+#include "CbmGeoRichPar.h"
+#include "FairGeoTransform.h"
+#include "FairGeoNode.h"
+#include "FairRunAna.h"
+#include "FairRuntimeDb.h"
+#include "CbmRichHitProducer.h"
 
 #include "TH1D.h"
 #include "TH2D.h"
+#include "TCanvas.h"
+#include "TEllipse.h"
 #include "TClonesArray.h"
+#include "TMath.h"
 
 #include <iostream>
 #include <vector>
 #include <map>
+#include <sstream>
 
-using std::cout;
-using std::endl;
-using std::map;
-using std::vector;
+using namespace std;
 
 CbmRichGeoTest::CbmRichGeoTest()
    :FairTask("RichGeoTestQa")
 {
 	fEventNum = 0;
 
-	fhNofHitsEl = new TH1D("fhNofHitsEl", "fhNofHitsEl", 45, 0, 45);
-	fhAaxisEl = new TH1D("fhAaxisEl", "fhAaxisEl", 50, 0, 10);
-	fhBaxisEl = new TH1D("fhBaxisEl", "fhBaxisEl", 50, 0, 10);
-	fhBoverAEl = new TH1D("fhBoverAEl", "fhBoverAEl", 50, 0, 1);
-	fhBoverAXYEl = new TH2D("fhBoverAXYEl","fhBoverAXYEl", 150, -150, 150, 250, -250, 250);
-	fhXYEl = new TH2D("fhXYEl", "fhXYEl",150, -150, 150, 250, -250, 250);
-	fhMCEl = new TH1D("fhMCEl", "fhMCEl", 30, 0, 15);
-	fhAccEl = new TH1D("fhAccEl", "fhAccEl", 30, 0, 15);
-	fhBaxisVsMomEl = new TH2D("fhBaxisVsMomEl", "fhBaxisVsMomEl",30, 0, 15,25, 0, 10 );;
-	fhAaxisVsMomEl = new TH2D("fhAaxisVsMomEl", "fhAaxisVsMomEl",30, 0, 15,25, 0, 10 );;
-	fhNofPointsEl = new TH1D("fhNofPointsEl", "fhNofPointsEl", 50, 0, 250);;
+   fMinAaxis = 3.5;
+   fMaxAaxis = 6.5;
+   fMinBaxis = 3.1;
+   fMaxBaxis = 6.1;
+   fMinRadius = 3.7;
+   fMaxRadius = 6.1;
 
-	fhXYHits = new TH2D("fhXYHits", "fhXYHits",150, -150, 150, 250, -250, 250);
-	fhXYPoints = new TH2D("fhXYPoints", "fhXYPoints",150, -150, 150, 250, -250, 250);
-	fhXYHitsOneEvent = new TH2D("fhXYHitsOneEvent", "fhXYHitsOneEvent",150, -150, 150, 250, -250, 250);
-	fhXYPointsOneEvent = new TH2D("fhXYPointsOneEvent", "fhXYPointsOneEvent",150, -150, 150, 250, -250, 250);
-	fhNofHitsEvent= new TH1D("fhNofHitsEvent", "fhNofHitsEvent", 50, 0, 2500);
-	fhNofPointsEvent= new TH1D("fhNofPointsEvent", "fhNofPointsEvent", 50, 0, 10000);
+	Int_t nofTypes = 2;
+   fhNofHits.resize(nofTypes);
+   fhAaxis.resize(nofTypes);
+   fhBaxis.resize(nofTypes);
+   fhBoverA.resize(nofTypes);
+   fhXcYcEllipse.resize(nofTypes);
+   fhXcYcCircle.resize(nofTypes);
+   fhRadius.resize(nofTypes);
+   fhBaxisVsMom.resize(nofTypes);
+   fhAaxisVsMom.resize(nofTypes);
+   fhRadiusVsMom.resize(nofTypes);
+	for (Int_t i = 0; i < nofTypes; i++){
+	   stringstream ss;
+	   if (i == 0) ss << "_Hits";
+	   if (i == 1) ss << "_Points";
+	   string t = ss.str();
+      fhNofHits[i] = new TH1D(("fhNofHits"+t).c_str(), ("fhNofHits"+t).c_str(), 300, 0, 300);
+      fHists.push_back(fhNofHits[i]);
+      // ellipse fitting parameters
+      fhAaxis[i] = new TH1D(("fhAaxis"+t).c_str(), ("fhAaxis"+t).c_str(), 50, 0, 10);
+      fHists.push_back(fhAaxis[i]);
+      fhBaxis[i] = new TH1D(("fhBaxis"+t).c_str(), ("fhBaxis"+t).c_str(), 50, 0, 10);
+      fHists.push_back(fhBaxis[i]);
+      fhBoverA[i] = new TH1D(("fhBoverA"+t).c_str(), ("fhBoverA"+t).c_str(), 50, 0, 1);
+      fHists.push_back(fhBoverA[i]);
+      fhXcYcEllipse[i] = new TH2D(("fhXcYcEllipse"+t).c_str(), ("fhXcYcEllipse"+t).c_str(), 150, -150, 150, 250, -250, 250);
+      fHists.push_back(fhXcYcEllipse[i]);
+      // circle fitting parameters
+      fhXcYcCircle[i] = new TH2D(("fhXcYcCircle"+t).c_str(), ("fhXcYcCircle"+t).c_str(), 150, -150, 150, 250, -250, 250);
+      fHists.push_back(fhXcYcCircle[i]);
+      fhRadius[i] = new TH1D(("fhRadius"+t).c_str(), ("fhRadius"+t).c_str(), 50, 0, 10);
+      fHists.push_back(fhRadius[i]);
+      // A, B, radius vs. MC momentum
+      fhBaxisVsMom[i] = new TH2D(("fhBaxisVsMom"+t).c_str(), ("fhBaxisVsMom"+t).c_str(), 100, 0., 12, 100, 0., 10.);
+      fHists.push_back(fhBaxisVsMom[i]);
+      fhAaxisVsMom[i] = new TH2D(("fhAaxisVsMom"+t).c_str(), ("fhAaxisVsMom"+t).c_str(), 100, 0., 12, 100, 0., 10.);
+      fHists.push_back(fhAaxisVsMom[i]);
+      fhRadiusVsMom[i] = new TH2D(("fhRadiusVsMom"+t).c_str(), ("fhRadiusVsMom"+t).c_str(), 100, 0., 12, 100, 0., 10.);
+      fHists.push_back(fhRadiusVsMom[i]);
+	}
+
+   // Difference between Mc Points and Hits fit
+   fhDiffAaxis = new TH1D("fhDiffAaxis", "fhDiffAaxis", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffAaxis);
+   fhDiffBaxis = new TH1D("fhDiffBaxis", "fhDiffBaxis", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffBaxis);
+   fhDiffXcEllipse = new TH1D("fhDiffXcEllipse", "fhDiffXcEllipse", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffXcEllipse);
+   fhDiffYcEllipse = new TH1D("fhDiffXyEllipse", "fhDiffXyEllipse", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffYcEllipse);
+   fhDiffXcCircle = new TH1D("fhDiffXcCircle", "fhDiffXcCircle", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffXcCircle);
+   fhDiffYcCircle = new TH1D("fhDiffYcCircle", "fhDiffYcCircle", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffYcCircle);
+   fhDiffRadius = new TH1D("fhDiffRadius", "fhDiffRadius", 300, -1.5, 1.5);
+   fHists.push_back(fhDiffRadius);
+
+   // Hits and points
+   fhDiffXhit = new TH1D("fhDiffXhit", "fhDiffXhit", 200, -1., 1.);
+   fHists.push_back(fhDiffXhit);
+   fhDiffYhit = new TH1D("fhDiffYhit", "fhDiffYhit", 200, -1., 1.);
+   fHists.push_back(fhDiffYhit);
+
+   // fitting efficiency
+   fhNofHitsAll = new TH1D("fhNofHitsAll", "fhNofHitsAll", 50, 0., 50.);
+   fHists.push_back(fhNofHitsAll);
+   fhNofHitsCircleFit = new TH1D("fhNofHitsCircleFit", "fhNofHitsCircleFit", 50, 0., 50.);
+   fHists.push_back(fhNofHitsCircleFit);
+   fhNofHitsEllipseFit = new TH1D("fhNofHitsEllipseFit", "fhNofHitsEllipseFit", 50, 0., 50.);
+   fHists.push_back(fhNofHitsEllipseFit);
+   fhNofHitsCircleFitEff = new TH1D("fhNofHitsCircleFitEff", "fhNofHitsCircleFitEff", 50, 0., 50.);
+   fHists.push_back(fhNofHitsCircleFitEff);
+   fhNofHitsEllipseFitEff = new TH1D("fhNofHitsEllipseFitEff", "fhNofHitsEllipseFitEff", 50, 0., 50.);
+   fHists.push_back(fhNofHitsEllipseFitEff);
 }
 
 CbmRichGeoTest::CbmRichGeoTest(
@@ -58,15 +130,38 @@ CbmRichGeoTest::~CbmRichGeoTest()
 
 }
 
+void CbmRichGeoTest::SetParContainers()
+{
+  FairRunAna* ana = FairRunAna::Instance();
+  FairRuntimeDb* rtdb=ana->GetRuntimeDb();
+  fPar=(CbmGeoRichPar*)(rtdb->getContainer("CbmGeoRichPar"));
+}
+
 InitStatus CbmRichGeoTest::Init()
 {
-	cout << "InitStatus CbmRichGeoTest::Init"<<endl;
+   cout << "CbmRichGeoTest::Init"<<endl;
+   FairRootManager* ioman = FairRootManager::Instance();
+   if (NULL == ioman) { Fatal("CbmRichGeoTest::Init","RootManager not instantised!"); }
 
-	FairRootManager* ioman = FairRootManager::Instance();
-	if (NULL == ioman) { Fatal("CbmRichGeoTest::Init","RootManager not instantised!"); }
+   fSensNodes = fPar->GetGeoSensitiveNodes();
+   fPassNodes = fPar->GetGeoPassiveNodes();
 
-	fRichHits = (TClonesArray*) ioman->GetObject("RichHit");
-	if ( !fRichHits) { Fatal("CbmRichGeoTest::Init","No RichHit array!"); }
+   // get detector position:
+   FairGeoNode *det= dynamic_cast<FairGeoNode*> (fSensNodes->FindObject("rich1d#1"));
+   if (NULL == det) cout << " -I no RICH Geo Node  found !!!!!  " << endl;
+
+   FairGeoTransform* detTr=det->getLabTransform(); // detector position in labsystem
+   FairGeoVector detPosLab=detTr->getTranslation(); // ... in cm
+   FairGeoTransform detCen=det->getCenterPosition(); // center in Detector system
+   FairGeoVector detPosCen=detCen.getTranslation();
+
+   fDetZOrig = detPosLab.Z() + detPosCen.Z(); // z coordinate of photodetector (Labsystem, cm)
+   FairGeoRotation fdetR=detTr->getRotMatrix();
+   fTheta = TMath::ASin(fdetR(7)); // tilting angle around x-axis
+   fPhi = -1.*TMath::ASin(fdetR(2)); // tilting angle around y-axis
+
+   fRichHits = (TClonesArray*) ioman->GetObject("RichHit");
+   if ( !fRichHits) { Fatal("CbmRichGeoTest::Init","No RichHit array!"); }
 
 	fRichRings = (TClonesArray*) ioman->GetObject("RichRing");
 	if ( NULL == fRichRings) { Fatal("CbmRichGeoTest::Init","No RichRing array!"); }
@@ -80,6 +175,12 @@ InitStatus CbmRichGeoTest::Init()
 	fRichRingMatches = (TClonesArray*) ioman->GetObject("RichRingMatch");
 	if ( NULL == fRichRingMatches) { Fatal("CbmRichGeoTest::Init","No RichRingMatch array!"); }
 
+   fCopFit = new CbmRichRingFitterCOP();
+   fCopFit->Init();
+
+   fTauFit = new CbmRichRingFitterEllipseTau();
+   fTauFit->Init();
+
 	return kSUCCESS;
 }
 
@@ -89,8 +190,7 @@ void CbmRichGeoTest::Exec(
 	fEventNum++;
 	cout << "CbmRichGeoTest, event No. " <<  fEventNum << endl;
 	RingParameters();
-	EventPoints();
-	MCElectrons();
+	HitsAndPoints();
 }
 
 void CbmRichGeoTest::RingParameters()
@@ -111,84 +211,200 @@ void CbmRichGeoTest::RingParameters()
 
 		if (pdg != 11 || motherId != -1) continue; // only primary electrons
 
-		Double_t axisA = ring->GetAaxis();
-		Double_t axisB = ring->GetBaxis();
-		Double_t xc = ring->GetCenterX();
-		Double_t yc = ring->GetCenterY();
-		Int_t nofHitsRing = ring->GetNofHits();
+		vector<double> xMc, yMc;
+		Int_t nofRichPoints = fRichPoints->GetEntriesFast();
+		for (Int_t iPoint = 0; iPoint < nofRichPoints; iPoint++){
+		   CbmRichPoint* richPoint = (CbmRichPoint*) fRichPoints->At(iPoint);
+		   if (richPoint == NULL) continue;
+	      CbmMCTrack* mcTrackRich = (CbmMCTrack*)fMCTracks->At(richPoint->GetTrackID());
+	      if (!mcTrackRich) continue;
+	      Int_t motherIdRich = mcTrackRich->GetMotherId();
+		   if (motherIdRich == mcTrackId){
+		      TVector3 posPoint;
+		      richPoint->Position(posPoint);
+		      TVector3 detPoint;
+		      CbmRichHitProducer::TiltPoint(&posPoint, &detPoint, fPhi, fTheta, fDetZOrig);
+		      xMc.push_back(detPoint.X());
+		      yMc.push_back(detPoint.Y());
+		   }
+		}
+	   fhNofHitsAll->Fill(ring->GetNofHits());
 
-		if (nofHitsRing >= 5) fhAccEl->Fill(momentum);
+      vector<Double_t> x, y;
+		CbmRichRing* ringMc = new CbmRichRing();
 
-		if (axisA > 10 || axisA < 1) continue;
-		if (axisB > 10 || axisB < 1) continue;
+      FitAndFillHistCircle(0, ring, momentum, x, y); //hits
+      FitAndFillHistCircle(1, ringMc, momentum, xMc, yMc); // points
+      FillMcVsHitFitCircle(ring, ringMc);
+      if (ring->GetRadius() > fMinRadius && ring->GetRadius() < fMaxRadius){
+         fhNofHitsCircleFit->Fill(ring->GetNofHits());
+      }
 
-		fhBoverAXYEl->Fill(xc, yc, axisB/axisA);
-		fhBoverAEl->Fill(axisB/axisA);
-		fhXYEl->Fill(xc, yc);
-		fhAaxisEl->Fill(axisA);
-		fhBaxisEl->Fill(axisB);
-		fhNofHitsEl->Fill(nofHitsRing);
-		fhBaxisVsMomEl->Fill(momentum, axisB );
-		fhAaxisVsMomEl->Fill(momentum, axisA );
-	}
+		FitAndFillHistEllipse(0, ring, momentum, x, y);// hits
+		FitAndFillHistEllipse(1, ringMc, momentum, xMc, yMc); // points
+		FillMcVsHitFitEllipse(ring, ringMc);
+	   if (ring->GetAaxis() > fMinAaxis && ring->GetAaxis() < fMaxAaxis
+	       &&  ring->GetBaxis() > fMinBaxis && ring->GetAaxis() < fMaxBaxis ){
+	      fhNofHitsEllipseFit->Fill(ring->GetNofHits());
+	   }
+		delete ringMc;
+	}// iR
 }
 
-void CbmRichGeoTest::EventPoints()
+void CbmRichGeoTest::FitAndFillHistEllipse(
+      Int_t histIndex,
+      CbmRichRing* ring,
+      Double_t momentum,
+      const vector<Double_t>& x,
+      const vector<Double_t>& y)
 {
-	Int_t oneEventNum = 2;
-	Int_t nofPoints = fRichPoints->GetEntriesFast();
-	for (Int_t iP = 0; iP < nofPoints; iP++){
-		CbmRichPoint* point = (CbmRichPoint*)fRichPoints->At(iP);
-		if (!point)continue;
-		fhXYPoints->Fill(point->GetX(), point->GetY());
-		if (oneEventNum == fEventNum) fhXYPointsOneEvent->Fill(point->GetX(), point->GetY());
-	}
-
-	Int_t nofHits = fRichHits->GetEntriesFast();
-	for (Int_t iH = 0; iH < nofHits; iH++){
-		CbmRichHit* hit = (CbmRichHit*)fRichHits->At(iH);
-		if (!hit)continue;
-		fhXYHits->Fill(hit->GetX(), hit->GetY());
-		if (oneEventNum == fEventNum) fhXYHitsOneEvent->Fill(hit->GetX(), hit->GetY());
-	}
-	fhNofHitsEvent->Fill(nofHits);
-	fhNofPointsEvent->Fill(nofPoints);
+   if (x.size() != 0){ fTauFit->DoFit1(ring, x, y);}
+   else {fTauFit->DoFit(ring);}
+   Double_t axisA = ring->GetAaxis();
+   Double_t axisB = ring->GetBaxis();
+   Double_t xcEllipse = ring->GetCenterX();
+   Double_t ycEllipse = ring->GetCenterY();
+   Int_t nofHitsRing = ring->GetNofHits();
+   if (x.size() != 0) nofHitsRing = x.size();
+   fhBoverA[histIndex]->Fill(axisB/axisA);
+   fhXcYcEllipse[histIndex]->Fill(xcEllipse, ycEllipse);
+   fhAaxis[histIndex]->Fill(axisA);
+   fhBaxis[histIndex]->Fill(axisB);
+   fhNofHits[histIndex]->Fill(nofHitsRing);
+   fhBaxisVsMom[histIndex]->Fill(momentum, axisB);
+   fhAaxisVsMom[histIndex]->Fill(momentum, axisA);
 }
 
-void CbmRichGeoTest::MCElectrons()
+void CbmRichGeoTest::FitAndFillHistCircle(
+      Int_t histIndex,
+      CbmRichRing* ring,
+      Double_t momentum,
+      const vector<Double_t>& x,
+      const vector<Double_t>& y)
 {
-	Int_t nofMCTracks = fMCTracks->GetEntriesFast();
-	for (Int_t iT = 0; iT < nofMCTracks; iT++){
-		CbmMCTrack* mcTrack = (CbmMCTrack*)fMCTracks->At(iT);
-		if (!mcTrack) continue;
-		Int_t motherId = mcTrack->GetMotherId();
-		Int_t pdg = TMath::Abs(mcTrack->GetPdgCode());
-		if (pdg != 11 || motherId != -1) continue; // only primary electrons
+   if (x.size() != 0){ fCopFit->DoFit1(ring, x, y);}
+   else {fCopFit->DoFit(ring);}
+   Double_t radius = ring->GetRadius();
+   Double_t xcCircle = ring->GetCenterX();
+   Double_t ycCircle = ring->GetCenterY();
+   fhXcYcCircle[histIndex]->Fill(xcCircle, ycCircle);
+   fhRadius[histIndex]->Fill(radius);
+   fhRadiusVsMom[histIndex]->Fill(momentum, radius);
+}
 
-		fhMCEl->Fill(mcTrack->GetP());
-		fhNofPointsEl->Fill(mcTrack->GetNPoints(kRICH));
-	}
+void CbmRichGeoTest::FillMcVsHitFitEllipse(
+      CbmRichRing* ring,
+      CbmRichRing* ringMc)
+{
+   fhDiffAaxis->Fill(ringMc->GetAaxis() - ring->GetAaxis());
+   fhDiffBaxis->Fill(ringMc->GetBaxis() - ring->GetBaxis());
+   fhDiffXcEllipse->Fill(ringMc->GetCenterX() - ring->GetCenterX());
+   fhDiffYcEllipse->Fill(ringMc->GetCenterY() - ring->GetCenterY());
+}
+
+void CbmRichGeoTest::FillMcVsHitFitCircle(
+      CbmRichRing* ring,
+      CbmRichRing* ringMc)
+{
+   fhDiffXcCircle->Fill(ringMc->GetCenterX() - ring->GetCenterX());
+   fhDiffYcCircle->Fill(ringMc->GetCenterY() - ring->GetCenterY());
+   fhDiffRadius->Fill(ringMc->GetRadius() - ring->GetRadius());
+}
+
+void CbmRichGeoTest::HitsAndPoints()
+{
+   Int_t nofHits = fRichHits->GetEntriesFast();
+   for (Int_t iH = 0; iH < nofHits; iH++){
+      CbmRichHit *hit = (CbmRichHit*) fRichHits->At(iH);
+      if ( hit == NULL ) continue;
+      Int_t pointInd =  hit->GetRefId();
+      if (pointInd < 0) continue;
+      CbmRichPoint *point = (CbmRichPoint*) fRichPoints->At(pointInd);
+      if ( point == NULL ) continue;
+      TVector3 inPos(point->GetX(), point->GetY(), point->GetZ());
+      TVector3 outPos;
+      CbmRichHitProducer::TiltPoint(&inPos, &outPos, fPhi, fTheta, fDetZOrig);
+      fhDiffXhit->Fill(hit->GetX() - outPos.X());
+      fhDiffYhit->Fill(hit->GetY() - outPos.Y());
+   }
+}
+
+
+void CbmRichGeoTest::Draw()
+{
+   for (int i = 0; i < 2; i++){
+      stringstream ss;
+      ss << "canvas" << i ;
+      TCanvas *c = new TCanvas(ss.str().c_str(), ss.str().c_str(), 1000, 750);
+      c->Divide(4, 3);
+      c->cd(1);
+      fhBoverA[i]->Draw();
+      c->cd(2);
+      fhXcYcEllipse[i]->Draw("COLZ");
+      c->cd(3);
+      fhAaxis[i]->Draw();
+      c->cd(4);
+      fhBaxis[i]->Draw();
+      c->cd(5);
+      fhNofHits[i]->Draw();
+      c->cd(6);
+      fhXcYcCircle[i]->Draw("COLZ");
+      c->cd(7);
+      fhRadius[i]->Draw();
+      c->cd(8);
+      fhBaxisVsMom[i]->Draw("COLZ");
+      c->cd(9);
+      fhAaxisVsMom[i]->Draw("COLZ");
+      c->cd(10);
+      fhRadiusVsMom[i]->Draw("COLZ");
+   }
+
+   TCanvas *cDiff = new TCanvas("cDiff", "cDiff", 1000, 500);
+   cDiff->Divide(4,2);
+   cDiff->cd(1);
+   fhDiffAaxis->Draw();
+   cDiff->cd(2);
+   fhDiffBaxis->Draw();
+   cDiff->cd(3);
+   fhDiffXcEllipse->Draw();
+   cDiff->cd(4);
+   fhDiffYcEllipse->Draw();
+   cDiff->cd(5);
+   fhDiffXcCircle->Draw();
+   cDiff->cd(6);
+   fhDiffYcCircle->Draw();
+   cDiff->cd(7);
+   fhDiffRadius->Draw();
+
+   TCanvas *cHits = new TCanvas("cHits", "cHits", 1000, 500);
+   cHits->Divide(2,1);
+   cHits->cd(1);
+   fhDiffXhit->Draw();
+   cHits->cd(2);
+   fhDiffYhit->Draw();
+
+   TCanvas *cFitEff = new TCanvas("cFitEff", "cFitEff", 900, 600);
+   cFitEff->Divide(3,2);
+   cFitEff->cd(1);
+   fhNofHitsAll->Draw();
+   cFitEff->cd(2);
+   fhNofHitsCircleFit->Draw();
+   cFitEff->cd(3);
+   fhNofHitsEllipseFit->Draw();
+   cFitEff->cd(4);
+   fhNofHitsCircleFitEff->Divide(fhNofHitsCircleFit, fhNofHitsAll);
+   fhNofHitsCircleFitEff->Draw();
+   cFitEff->cd(5);
+   fhNofHitsEllipseFitEff->Divide(fhNofHitsEllipseFit, fhNofHitsAll);
+   fhNofHitsEllipseFitEff->Draw();
 }
 
 void CbmRichGeoTest::Finish()
 {
-	fhAaxisEl->Write();
-	fhBaxisEl->Write();
-	fhBoverAEl->Write();
-   fhBoverAXYEl->Write();
-	fhXYEl->Write();
-	fhXYHits->Write();
-	fhXYPoints->Write();
-	fhXYHitsOneEvent->Write();
-	fhXYPointsOneEvent->Write();
-	fhNofHitsEvent->Write();
-	fhNofPointsEvent->Write();
-	fhNofHitsEl->Write();
-	fhMCEl->Write();
-	fhAccEl->Write();
-	fhBaxisVsMomEl->Write();
-	fhAaxisVsMomEl->Write();
-	fhNofPointsEl->Write();
+   Draw();
+   for (Int_t i = 0; i < fHists.size(); i++){
+      fHists[i]->Write();
+   }
 }
 
 ClassImp(CbmRichGeoTest)
--- rich/qa/CbmRichGeoTest.h
+++ rich/qa/CbmRichGeoTest.h
@@ -7,67 +7,132 @@
 class TH1D;
 class TH2D;
 class TClonesArray;
+class CbmRichRingFitterCOP;
+class CbmRichRingFitterEllipseTau;
+class CbmRichRing;
+class CbmGeoRichPar;
 
-#include <map>
+#include <vector>
 
-using std::map;
+using namespace std;
 
-class CbmRichGeoTest : public FairTask{
+class CbmRichGeoTest : public FairTask
+{
 
 public:
-	CbmRichGeoTest();
+   CbmRichGeoTest();
 
-	CbmRichGeoTest(
+   CbmRichGeoTest(
 	      const char *name,
 	      const char *title,
 	      Int_t verbose);
 
-	virtual ~CbmRichGeoTest();
+   virtual ~CbmRichGeoTest();
 
-	virtual InitStatus Init();
+   void SetParContainers();
 
-	virtual void Exec(
-	      Option_t* option);
+   virtual InitStatus Init();
 
-	virtual void Finish();
-	
-	void RingParameters();
+   virtual void Exec(
+		   Option_t* option);
 
-	void EventPoints();
+   virtual void Finish();
 
-	void MCElectrons();
+
 
 private:
+
+	void RingParameters();
 	
+	void FitAndFillHistEllipse(
+	      Int_t histIndex,
+	      CbmRichRing* ring,
+	      Double_t momentum,
+	      const vector<Double_t>& x,
+	      const vector<Double_t>& y);
+
+   void FitAndFillHistCircle(
+         Int_t histIndex,
+         CbmRichRing* ring,
+         Double_t momentum,
+         const vector<Double_t>& x,
+         const vector<Double_t>& y);
+
+   void FillMcVsHitFitEllipse(
+         CbmRichRing* ring,
+         CbmRichRing* ringMc);
+
+   void FillMcVsHitFitCircle(
+         CbmRichRing* ring,
+         CbmRichRing* ringMc);
+
+   void HitsAndPoints();
+
+	void Draw();
+
 	TClonesArray* fRichHits;
 	TClonesArray* fRichRings;
 	TClonesArray* fRichPoints; 
 	TClonesArray* fMCTracks;
 	TClonesArray* fRichRingMatches; 
 	
-	Int_t fEventNum;
-// electrons
-	TH1D* fhNofHitsEl;
-	TH1D* fhAaxisEl;
-	TH1D* fhBaxisEl;
-	TH1D* fhBoverAEl;
-	TH2D* fhBoverAXYEl;
-	TH2D* fhBaxisVsMomEl;
-	TH2D* fhAaxisVsMomEl;
-	TH1D* fhNofPointsEl;
-	TH2D* fhXYEl;
-	TH1D* fhMCEl;
-	TH1D* fhAccEl;
+   Double_t fDetZOrig; // X-coordinate of photodetector (original from parameter file)
+   Double_t fTheta; // angle by which photodetector was tilted around x-axis
+   Double_t fPhi; // angle by which photodetector was tilted around y-axis
+   TObjArray* fSensNodes;
+   TObjArray* fPassNodes;
+   CbmGeoRichPar* fPar;
 
-// general plots
-	TH1D* fhNofHitsEvent;
-	TH1D* fhNofPointsEvent;
+   // rings will be fitted on a fly
+	CbmRichRingFitterCOP* fCopFit;
+	CbmRichRingFitterEllipseTau* fTauFit;
+
+	Int_t fEventNum;
+
+	vector<TH1D*> fhNofHits; // number of hits per ring
+	// ellipse fitting parameters
+	vector<TH1D*> fhAaxis; // major axis (A)
+	vector<TH1D*> fhBaxis; // minor axis (B)
+	vector<TH1D*> fhBoverA; // B/A distribution
+	vector<TH2D*> fhXcYcEllipse; // (Xc, Yc) of ellipse center
+   // circle fitting parameters
+	vector<TH2D*> fhXcYcCircle; // (Xc, Yc) of circle center
+	vector<TH1D*> fhRadius; // circle radius
+   // A, B, radius vs. MC momentum
+	vector<TH2D*> fhBaxisVsMom;
+	vector<TH2D*> fhAaxisVsMom;
+	vector<TH2D*> fhRadiusVsMom;
 	
-	TH2D* fhXYHits;
-	TH2D* fhXYPoints;
-	TH2D* fhXYHitsOneEvent;
-	TH2D* fhXYPointsOneEvent;
-	
+	// Difference between Mc Points and Hits fit
+   // ellipse fitting parameters
+   TH1D* fhDiffAaxis; // major axis (A)
+   TH1D* fhDiffBaxis; // minor axis (B)
+   TH1D* fhDiffXcEllipse; // Xc of ellipse center
+   TH1D* fhDiffYcEllipse; // Yc of ellipse center
+   // circle fitting parameters
+   TH1D* fhDiffXcCircle; // Xc of circle center
+   TH1D* fhDiffYcCircle; // Xc of circle center
+   TH1D* fhDiffRadius; // circle radius
+
+   // Hits and points
+   TH1D* fhDiffXhit;
+   TH1D* fhDiffYhit;
+
+   // fitting efficiency
+   Double_t fMinAaxis;
+   Double_t fMaxAaxis;
+   Double_t fMinBaxis;
+   Double_t fMaxBaxis;
+   Double_t fMinRadius;
+   Double_t fMaxRadius;
+   TH1D* fhNofHitsAll;
+   TH1D* fhNofHitsCircleFit;
+   TH1D* fhNofHitsEllipseFit;
+   TH1D* fhNofHitsCircleFitEff;
+   TH1D* fhNofHitsEllipseFitEff;
+
+	vector<TH1*> fHists;
+
 	ClassDef(CbmRichGeoTest,1)
 };
 

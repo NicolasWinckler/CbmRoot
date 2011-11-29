e2f6593063b280aa910145ae290931818d745e94
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2011-11-29 19:44:47 +0000

    add trd.v12a geometry, 10 layers, with 36 cm thick PE foam radiator material
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@13816 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ trd/v12x/CbmTrdPads12X_7.5mm_01cm2.h
@@ -0,0 +1,93 @@
+// -------------------------------------------------------------------------
+// -----                     CbmTrdPads header file                    -----
+// -----               Created 03/12/2009 by D. Emschermann            -----
+// -------------------------------------------------------------------------
+//
+// v1.4 - 20110616 DE - adapted to v11X with variable frame size - 570 mm and 960 mm - 7.5 mm pad width - 1 cm2
+// v1.3 - 20110616 DE - adapted to v11X with variable frame size - 570 mm and 960 mm - 5.0 mm pad width - 1 cm2
+// v1.2 - 20110524 DE - adapted to v11X (May11) with 24 TRD in the inner zone
+// v1.1 - 20110523 DE - 5x20 mm2 sized pads almost everywhere, tracking test
+// v1.0 - 20100708 DE - reduce pad size in trd1mod1-2 to 20/30 from 30/35 
+//                      and in trd1mod3-1 to 80/80 from 95/100
+// v0.9 - 20100601 DE - improve pad size ballance in trd1mod1-2 to 30/35 from 30/40
+// v0.8 - 20100601 DE - implement 5 sectors per module for code testing
+// v0.7 - 20100531 DE - implement 3 sectors of n*0.5 mm long pads per module
+// v0.6 - 20100530 DE - implement 2 sectors with identical pads per module
+// v0.5 - 20100525 DE - reduce the pad length in trd1mod1-2 of station 1 
+//                      to 30.67 mm
+// v0.4 - 20100520 FU - Implemented modules with single sector and same
+//                      pad geometry as in version 0.3
+// v0.3 - 20100511 DE - 5 mm pad width everywhere, fractional pad length
+// v0.2 - 20091207 DE - fixed typo in pad x size in large module
+// v0.1 - 20091204 DE - initial version
+
+#ifndef CBMTRDPADS_H
+#define CBMTRDPADS_H
+
+// v1.4
+  Int_t fst1_sect_count = 3;
+  Float_t fst1_pad_type[6][3][4] =   // array of pad sizes in station 1
+    { { { 57., 24., .75,  1.5 }, 
+        { 57.,  9., .75,  1.5 }, 
+        { 57., 24., .75,  1.5 } },     
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 57., 24., .75,  1.5 },  
+        { 57.,  9., .75,  1.5 },  
+        { 57., 24., .75,  1.5 } },
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 57., 24., .75,  1.5 },  
+        { 57.,  9., .75,  1.5 },  
+        { 57., 24., .75,  1.5 } },
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 57., 24., .75,  1.5 },  
+        { 57.,  9., .75,  1.5 },  
+        { 57., 24., .75,  1.5 } },
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 96., 30., .75,  1.5 },  
+        { 96., 36., .75,  1.5 },  
+        { 96., 30., .75,  1.5 } },
+        // 20 x  15 mm + 24 x  15 mm + 20 x  15 mm,  128 * 64 = 8192 pads
+      { { 96., 30., .75,  1.5 },  
+        { 96., 36., .75,  1.5 },  
+        { 96., 30., .75,  1.5 } } };
+        // 20 x  15 mm + 24 x  15 mm + 20 x  15 mm,  128 * 64 = 8192 pads
+
+  Int_t fst2_sect_count = 3;
+  Float_t fst2_pad_type[6][3][4] =   // array of pad sizes in station 2
+    { { { 57., 24., .75,  1.5 }, 
+        { 57.,  9., .75,  1.5 }, 
+        { 57., 24., .75,  1.5 } },     
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 57., 24., .75,  1.5 },  
+        { 57.,  9., .75,  1.5 },  
+        { 57., 24., .75,  1.5 } },
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 57., 24., .75,  1.5 },  
+        { 57.,  9., .75,  1.5 },  
+        { 57., 24., .75,  1.5 } },
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 57., 24., .75,  1.5 },  
+        { 57.,  9., .75,  1.5 },  
+        { 57., 24., .75,  1.5 } },
+        // 16 x  15 mm +  6 x  15 mm + 16 x  15 mm,   76 * 38 = 2888 pads
+      { { 96., 30., .75,  1.5 },  
+        { 96., 36., .75,  1.5 },  
+        { 96., 30., .75,  1.5 } },
+        // 20 x  15 mm + 24 x  15 mm + 20 x  15 mm,  128 * 64 = 8192 pads
+      { { 96., 30., .75,  1.5 },  
+        { 96., 36., .75,  1.5 },  
+        { 96., 30., .75,  1.5 } } };
+        // 20 x  15 mm + 24 x  15 mm + 20 x  15 mm,  128 * 64 = 8192 pads
+
+  Int_t fst3_sect_count = 3;
+  Float_t fst3_pad_type[2][3][4] =   // array of pad sizes in station 3
+    { { { 96., 30., .75,  1.5 },  
+        { 96., 36., .75,  1.5 },  
+        { 96., 30., .75,  1.5 } },
+        // 20 x  15 mm + 24 x  15 mm + 20 x  15 mm,  128 * 64 = 8192 pads
+      { { 96., 30., .75,  1.5 },  
+        { 96., 36., .75,  1.5 },  
+        { 96., 30., .75,  1.5 } } };
+        // 20 x  15 mm + 24 x  15 mm + 20 x  15 mm,  128 * 64 = 8192 pads
+
+#endif

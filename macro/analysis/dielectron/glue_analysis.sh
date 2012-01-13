1672ce147e7672a22e17a5f07fe0a1062022b0e5
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2012-01-13 14:25:45 +0000

    resolve conflict
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@14138 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/analysis/dielectron/glue_analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DIR=/lustre/cbm/user/ebelolap/aug11/25gev/100field/mvd/omega
-
-OUTPUTFILE=$DIR/analysis.delta.all.root
-INPUTFILES=$DIR/analysis.delta.0*.root
-
-hadd -T -f $OUTPUTFILE $INPUTFILES

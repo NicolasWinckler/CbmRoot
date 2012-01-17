fd8278fab6328e53f1429bc7005f59e66700fb01
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2012-01-17 14:50:44 +0000

    update copy_trd_geometries.sh
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@14165 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/copy_trd_geometries.sh
+++ macro/trd/copy_trd_geometries.sh
@@ -5,11 +5,11 @@
 echo \# copy geometries
 cp -v geometry/trd.v12/trd_v12a.geo ../../geometry/trd
 cp -v geometry/trd.v12/media.geo    ../../geometry
-#cp -v geometry/trd.v11/v11?/*.geo* ../../geometry/trd
+cp -v geometry/trd.v11/v11?/*.geo* ../../geometry/trd
 #cp -v geometry/trd.v11/*.geo       ../../geometry/trd
 echo
-echo \# copy digi.par files
-cp -v geometry/trd.v12/trd_v12a_7.5mm_1cm2.digi.par ../../parameters/trd/trd_v12a.digi.par
+#echo \# copy digi.par files
+#cp -v geometry/trd.v12/trd_v12a_7.5mm_1cm2.digi.par ../../parameters/trd/trd_v12a.digi.par
 #cp -v ../../parameters/trd/v11a/trd_v11a_7.5mm_01cm2.digi.par ../../parameters/trd/trd_v11a.digi.par
 #cp -v ../../parameters/trd/v11b/trd_v11b_7.5mm_01cm2.digi.par ../../parameters/trd/trd_v11b.digi.par
 #cp -v geometry/trd.v11/v11b/trd_v11b_7.5mm_1cm2.digi.par ../../parameters/trd/trd_v11b.digi.par

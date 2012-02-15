a9f8f8c1ff24cac416137c26f15a77e55883f85a
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2012-02-15 10:51:13 +0000

    extend CbmRichRingLight class.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@14479 0381ead4-6506-0410-b988-94b70fbc4730

--- cbmdata/rich/CbmRichRingLight.h
+++ cbmdata/rich/CbmRichRingLight.h
@@ -11,96 +11,315 @@
 #include <vector>
 #include <cmath>
 
+using std::vector;
+
 class CbmRichHitLight
 {
 public:
-	float fX;
-	float fY;
+   /**
+    * \brief Default constructor.
+    */
+   CbmRichHitLight():
+      fX(0.),
+      fY(0.){}
+
+   /**
+    * \brief Constructor with initialization.
+    * \param x X coordinate of hit.
+    * \param y Y coordinate of hit.
+    */
+   CbmRichHitLight(
+         float x,
+         float y)
+   {
+      fX = x;
+      fY = y;
+   }
+	float fX; // x coordinate of the hit
+	float fY; // y coordinate of the hit
 };
 
 class CbmRichRingLight
 {
 public:
-    CbmRichRingLight()
-      :  fHitCollection(),
-    fHitIdCollection(),
-    fCenterX(0.),
-    fCenterY(0.),
-    fRadius(0.),
-    fChi2(0.),
-    fAngle(0.),
-    fNofHitsOnRing(0),
-    fRecFlag(0),
-    fSelectionNN(0.)
-      {
-    	fHitCollection.reserve(30);
-    	fHitIdCollection.reserve(30);
+
+   /**
+    * \brief Standard constructor.
+    */
+   CbmRichRingLight():
+      fHits(),
+      fHitIds(),
+
+      fCenterX(0.f),
+      fCenterY(0.f),
+      fRadius(0.f),
+
+      fAaxis(0.f),
+      fBaxis(0.f),
+      fPhi(0.f),
+
+      fAPar(0.f),
+      fBPar(0.f),
+      fCPar(0.f),
+      fDPar(0.f),
+      fEPar(0.f),
+      fFPar(0.f),
+
+      fRecFlag(0),
+
+      fChi2(0.f),
+      fAngle(0.f),
+      fNofHitsOnRing(0),
+      fSelectionNN(0.f)
+
+   {
+      fHits.reserve(30);
+      fHitIds.reserve(30);
+   }
+
+   /**
+    * \brief Destructor.
+    */
+   ~CbmRichRingLight()
+   {
+      fHits.clear();
+      fHitIds.clear();
+   }
+
+   /**
+    * \brief Add new hit to the ring.
+    * \param[in] hit New hit to be added.
+    * \param[in] id index of hit in TClonesArray.
+    */
+   void AddHit(
+         CbmRichHitLight hit,
+         unsigned short id = -1)
+   {
+      fHits.push_back(hit);
+      fHitIds.push_back(id);
+   }
+
+   /**
+    * \brief Remove hit from the ring.
+    * \param[in] hitId index of hit in TClonesArray.
+    * \return true if hit was removed, false if hit was NOT removed.
+    */
+   bool RemoveHit(
+         int hitId)
+   {
+      vector<unsigned short>::iterator it;
+      for (it = fHitIds.begin(); it!= fHitIds.end(); it++){
+         if (hitId == *it){
+            fHitIds.erase(it);
+            return true;
+         }
       }
+      return false;
+   }
 
-	~CbmRichRingLight(){
-		fHitCollection.clear();
-	}
+   /**
+    * \brief Return number of hits in ring.
+    */
+	int GetNofHits() const {return fHitIds.size(); }
 
-	void AddHit(CbmRichHitLight pHit, unsigned short id) {
-		fHitCollection.push_back(pHit);
-		fHitIdCollection.push_back(id);
+	/**
+	 * \brief Return hit by the index.
+	 * \param[in] ind Index of hit in local array.
+	 */
+	CbmRichHitLight GetHit(int ind) {return fHits[ind];}
 
-	}
-	bool RemoveHit(int hitId){
-		//Int_t nofHits = fHitCollection.size();
-		std::vector<unsigned short>::iterator it;
-		for (it = fHitIdCollection.begin(); it!=fHitIdCollection.end(); it++){
-			if (hitId == *it){
-				fHitIdCollection.erase(it);
-				return true;
-			}
-		}
-		return false;
-	}
-
-	int GetNofHits() const {return fHitIdCollection.size(); }
-	CbmRichHitLight GetHit(int i) {return fHitCollection[i];}
-	unsigned short GetHitId(int i) {return fHitIdCollection[i];}
+	/**
+	 * \brief Return hit index in TClonesArray.
+	 * \param[in] ind Index of hit in local array.
+	 */
+	unsigned short GetHitId(int ind) {return fHitIds[ind];}
 
 	void SetCenterX(float x) {fCenterX = x;}
-    void SetCenterY(float y) {fCenterY = y;}
+   void SetCenterY(float y) {fCenterY = y;}
 	void SetRadius(float r) {fRadius = r;}
+
+   /**
+    * \brief Set all 3 circle parameters.
+    * \param[in] x X coordinate of circle center.
+    * \param[in] y Y coordinate of circle center.
+    * \param[in] r Radius of circle.
+    */
+	void SetXYR(
+	      float x,
+	      float y,
+	      float r)
+	{
+	   fCenterX = x;
+	   fCenterY = y;
+	   fRadius = r;
+	}
+
+   /**
+    * \brief Set all 5 ellipse parameters.
+    * \param[in] x X coordinate of ellipse center.
+    * \param[in] y Y coordinate of ellipse center.
+    * \param[in] a Major half axis of ellipse.
+    * \param[in] b Minor half axis of ellipse.
+    * \param[in] p Rotation angle of ellipse [rad].
+    */
+   void SetXYABP(
+         float x,
+         float y,
+         float a,
+         float b,
+         float p)
+   {
+      fCenterX = x;
+      fCenterY = y;
+      fAaxis = a;
+      fBaxis = b;
+      fPhi = p;
+   }
+
 	void SetChi2(float chi2) {fChi2 = chi2;}
 	void SetAngle( float angle) {fAngle = angle;}
 	void SetNofHitsOnRing(unsigned short onring) {fNofHitsOnRing = onring;}
-	/** number between -1 and 1: -1 = fake ring, 1 = good ring (selection by neural net)*/
 	void SetSelectionNN (float selectionNN ) {fSelectionNN = selectionNN;}
-	void SetRecFlag(short recFlag){fRecFlag = recFlag;}
 
 	float GetCenterX() const {return fCenterX;}
 	float GetCenterY() const {return fCenterY;}
 	float GetRadius() const {return fRadius;}
+
+   float GetAaxis() const {return fAaxis;}
+   float GetBaxis() const {return fBaxis;}
+   float GetPhi() const {return fPhi;}
+   void SetAaxis(double a) {fAaxis = a;}
+   void SetBaxis(double b) {fBaxis = b;}
+   void SetPhi(double phi) {fPhi = phi;}
+
+   /**
+    * \brief Calculate and return X coordinate of the first focus.
+    */
+   double GetXF1() const
+   {
+      double c = sqrt(fAaxis * fAaxis - fBaxis * fBaxis);
+      double xc = c*cos(fabs(fPhi));
+
+      return fCenterX+xc;
+   }
+
+   /**
+    * \brief Calculate and return Y coordinate of the first focus.
+    */
+   double GetYF1() const
+   {
+      double c = sqrt(fAaxis * fAaxis - fBaxis * fBaxis);
+      double yc = c * sin(fabs(fPhi));
+      if (fPhi >=0){
+         return fCenterY+yc;
+      }else{
+         return fCenterY-yc;
+      }
+   }
+
+   /**
+    * \brief Calculate and return X coordinate of the second focus.
+    */
+   double GetXF2() const
+   {
+      double c = sqrt(fAaxis * fAaxis - fBaxis * fBaxis);
+      double xc = c*cos(fabs(fPhi));
+
+      return fCenterX-xc;
+   }
+
+   /**
+    * \brief Calculate and return Y coordinate of the second focus.
+    */
+   double GetYF2() const
+   {
+      double c = sqrt(fAaxis * fAaxis - fBaxis * fBaxis);
+      double yc = c * sin(fabs(fPhi));
+      if (fPhi >= 0){
+         return fCenterY - yc;
+      }else{
+         return fCenterY + yc;
+      }
+   }
+
+   /**
+    * \brief Set all 6 parameters of curve equation Axx+Bxy+Cyy+Dx+Ey+F.
+    * \param[in] a A parameter.
+    * \param[in] b B parameter.
+    * \param[in] c C parameter.
+    * \param[in] d D parameter.
+    * \param[in] e E parameter.
+    * \param[in] f F parameter.
+    */
+   void SetABCDEF(
+         float a,
+         float b,
+         float c,
+         float d,
+         float e,
+         float f)
+   {
+      fAPar = a;
+      fBPar = b;
+      fCPar = c;
+      fDPar = d;
+      fEPar = e;
+      fFPar = f;
+   }
+
+   float GetAPar() const {return fAPar;}
+   float GetBPar() const {return fBPar;}
+   float GetCPar() const {return fCPar;}
+   float GetDPar() const {return fDPar;}
+   float GetEPar() const {return fEPar;}
+   float GetFPar() const {return fFPar;}
+
 	float GetSelectionNN() const {return fSelectionNN;}
 	float GetChi2() const {return fChi2;}
-	float GetNDF() const {return (float)(GetNofHits()-5);}
+
+	/**
+	 * \brief Return radial position of the ring.
+	 */
 	float GetRadialPosition() const{
 		if (fCenterY > 0.f){
-		       return sqrt(fCenterX*fCenterX +(fCenterY - 110.f)*(fCenterY - 110.f));
-		   } else {
-		       return sqrt(fCenterX*fCenterX+(fCenterY + 110.f)*(fCenterY + 110.f));
+		   return sqrt(fCenterX * fCenterX + (fCenterY - 110.f) * (fCenterY - 110.f));
+		} else {
+		   return sqrt(fCenterX * fCenterX + (fCenterY + 110.f) * (fCenterY + 110.f));
 		};
+		return 0.;
 	}
 
 	float GetAngle() const {return fAngle;}
+
 	unsigned short GetNofHitsOnRing() const {return fNofHitsOnRing;}
-	short GetRecFlag(){return fRecFlag;}
+
+	int GetRecFlag() const {return fRecFlag;}
+	void SetRecFlag(int r) { fRecFlag = r;}
 
 private:
-   std::vector<CbmRichHitLight> fHitCollection;
-   std::vector<unsigned short> fHitIdCollection; /** STL container to hold the hit indexes */
-protected:
-    float fCenterX;
+   vector<CbmRichHitLight> fHits; // STL container for CbmRichHitLight
+   vector<unsigned short> fHitIds; // STL container for hit indexes
+
+   float fCenterX;
 	float fCenterY;
 	float fRadius;
+
+	float fAaxis;
+	float fBaxis;
+	float fPhi;
+
+   float fAPar; // Axx+Bxy+Cyy+Dx+Ey+F
+   float fBPar;
+   float fCPar;
+   float fDPar;
+   float fEPar;
+   float fFPar;
+
+   int fRecFlag;
+
 	float fChi2;
 	float fAngle;
 	unsigned short fNofHitsOnRing;
-	short fRecFlag;
 	float fSelectionNN;
 };
 

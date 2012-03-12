d09bdd469c6b698d08cbd28bfcb95d22d5da8529
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2012-03-12 14:42:12 +0000

    Writeout buffer for time based trd digitization.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@14922 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ trd/CbmTrdDigiWriteoutBuffer.cxx
@@ -0,0 +1,105 @@
+/*
+ * CbmTrdDigiWriteoutBuffer.cxx
+ *
+ *  Created on: Mar 7, 2012
+ *      Author: uhlig
+ */
+
+#include "CbmTrdDigiWriteoutBuffer.h"
+#include "CbmTrdDigi.h"
+
+#include "FairLogger.h"
+
+CbmTrdDigiWriteoutBuffer::CbmTrdDigiWriteoutBuffer()
+  : FairWriteoutBuffer(),
+    fData_map()
+{
+}
+
+
+CbmTrdDigiWriteoutBuffer::CbmTrdDigiWriteoutBuffer(TString branchName, TString folderName, Bool_t persistance)
+  : FairWriteoutBuffer(branchName, "CbmTrdDigi", folderName, persistance),
+    fData_map()
+{
+}
+
+CbmTrdDigiWriteoutBuffer::~CbmTrdDigiWriteoutBuffer()
+{
+}
+
+//std::vector<std::pair<double, FairTimeStamp*> > Modify(std::pair<double, FairTimeStamp*> oldData, std::pair<double, FairTimeStamp*> newData) 
+//std::vector<std::pair<double, CbmTrdDigi*> > CbmTrdDigiWriteoutBuffer::Modify(std::pair<double, CbmTrdDigi*> oldData, std::pair<double, CbmTrdDigi*> newData)
+//{
+//  FairLogger* fLogger = FairLogger::GetLogger();
+//  fLogger->Debug(MESSAGE_ORIGIN,"Inside Modify:");
+//  std::vector<std::pair<double, CbmTrdDigi*> > result;
+//  result.push_back(oldData);
+//  return result;
+//  /*
+//  std::vector<std::pair<double, FairTimeStamp*> > result;
+//  return result;
+//  std::pair<double, CbmTrdDigi*> singleResult;
+//  if (newData.first > 0) {
+//    singleResult.first = oldData.first + newData.first;
+//  }
+//  singleResult.second = oldData.second;
+//  singleResult.second->AddCharge(newData.second->GetCharge());
+//  // if (fVerbose > 0) {
+//    std::cout << "Modify hit" << std::endl;
+//    std::cout << "OldData: " << oldData.first << " : " << oldData.second << " NewData: " << newData.first << " : " << newData.second << std::endl;
+//    std::cout << "Resulting Data: " << singleResult.first << " : " << singleResult.second << std::endl;
+//    //  }
+//
+//  result.push_back(singleResult);
+//  return result;
+//  */
+//}
+
+
+//std::vector<std::pair<double, FairTimeStamp*> > Modify(std::pair<double, FairTimeStamp*> oldData, std::pair<double, FairTimeStamp*> newData) 
+//{
+//  FairLogger* fLogger = FairLogger::GetLogger();
+//  fLogger->Debug(MESSAGE_ORIGIN,"Inside Modify:");
+//  std::vector<std::pair<double, FairTimeStamp*> > result;
+//  result.push_back(oldData);
+//  return result;
+//}
+
+void CbmTrdDigiWriteoutBuffer::AddNewDataToTClonesArray(FairTimeStamp* data)
+{
+  FairRootManager* ioman = FairRootManager::Instance();
+  TClonesArray* myArray = ioman->GetTClonesArray(fBranchName);
+  FairLogger* fLogger = FairLogger::GetLogger();
+  fLogger->Debug(MESSAGE_ORIGIN,"Data Inserted: ");
+  CbmTrdDigi* bla =(CbmTrdDigi*)(data);
+  bla->Print();
+  new ((*myArray)[myArray->GetEntries()]) CbmTrdDigi(*(CbmTrdDigi*)(data));
+}
+
+double CbmTrdDigiWriteoutBuffer::FindTimeForData(FairTimeStamp* data)
+{
+  std::map<CbmTrdDigi, double>::iterator it;
+  CbmTrdDigi myData = *(CbmTrdDigi*)data;
+  it = fData_map.find(myData);
+  if (it == fData_map.end()) {
+    return -1;
+  } else {
+    return it->second;
+  }
+}
+
+void CbmTrdDigiWriteoutBuffer::FillDataMap(FairTimeStamp* data, double activeTime)
+{
+  CbmTrdDigi myData = *(CbmTrdDigi*)data;
+  fData_map[myData] = activeTime;
+}
+
+void CbmTrdDigiWriteoutBuffer::EraseDataFromDataMap(FairTimeStamp* data)
+{
+  CbmTrdDigi myData = *(CbmTrdDigi*)data;
+  if (fData_map.find(myData) != fData_map.end()) {
+    fData_map.erase(fData_map.find(myData));
+  }
+}
+
+ClassImp(CbmTrdDigiWriteoutBuffer);
---
+++ trd/CbmTrdDigiWriteoutBuffer.h
@@ -0,0 +1,82 @@
+/* CbmTrdDigiWriteoutBuffer.h
+ *
+ *  Created on: Mar 7, 2012
+ *      Author: uhlig
+ */
+
+#ifndef CBMTRDDIGIWRITEOUTBUFFER_H_
+#define CBMTRDDIGIWRITEOUTBUFFER_H_
+
+#include "FairWriteoutBuffer.h"
+#include "CbmTrdDigi.h"
+
+class FairTimeStamp;
+
+class CbmTrdDigiWriteoutBuffer : public FairWriteoutBuffer
+{
+
+  public:
+    CbmTrdDigiWriteoutBuffer();
+    CbmTrdDigiWriteoutBuffer(TString branchName, TString folderName, Bool_t persistance);
+    void AddNewDataToTClonesArray(FairTimeStamp*);
+
+    virtual ~CbmTrdDigiWriteoutBuffer();
+
+    //    std::vector<std::pair<double, CbmTrdDigi*> > Modify(std::pair<double, CbmTrdDigi*> oldData, std::pair<double, CbmTrdDigi*> newData)
+    std::vector<std::pair<double, FairTimeStamp*> > Modify(std::pair<double, FairTimeStamp*> oldData, std::pair<double, FairTimeStamp*> newData)
+      {
+	FairLogger* fLogger = FairLogger::GetLogger();
+	fLogger->Debug(MESSAGE_ORIGIN,"Inside Modify:");
+	fLogger->Debug(MESSAGE_ORIGIN,"oldData.first %f", oldData.first);
+	fLogger->Debug(MESSAGE_ORIGIN,"newData.first: %f", newData.first);
+	
+	std::vector<std::pair<double, FairTimeStamp*> > result;
+
+	std::pair<double, FairTimeStamp*> singleResult;
+//
+	CbmTrdDigi* newDigiData = (CbmTrdDigi*) newData.second;
+	CbmTrdDigi* oldDigiData = (CbmTrdDigi*) oldData.second;
+	CbmTrdDigi* combinedDigiData = oldDigiData;
+        fLogger->Debug(MESSAGE_ORIGIN,"Old Charge: %f", oldDigiData->GetCharge());
+        fLogger->Debug(MESSAGE_ORIGIN,"Combined Charge: %f", combinedDigiData->GetCharge());
+
+//	if (newData.first > 0) {
+//	  singleResult.first = oldData.first + newData.first;
+	  singleResult.first = oldData.first;
+//	}
+	combinedDigiData->AddCharge(newDigiData->GetCharge());
+        fLogger->Debug(MESSAGE_ORIGIN,"New Charge: %f", newDigiData->GetCharge());
+        fLogger->Debug(MESSAGE_ORIGIN,"Combined Charge: %f", combinedDigiData->GetCharge());
+	singleResult.second = (FairTimeStamp*)combinedDigiData;
+	//        singleResult.second->Print();
+	CbmTrdDigi* bla = (CbmTrdDigi*) singleResult.second;
+        bla->Print();
+	//        fLogger->Debug(MESSAGE_ORIGIN,"Combined Charge: %f", combinedDigiData->GetCharge());
+//  if (fVerbose > 0) {
+//    std::cout << "Modify hit" << std::endl;
+//    std::cout << "OldData: " << oldData.first << " : " << oldData.second << " NewData: " << newData.first << " : " << newData.second << std::endl;
+//    std::cout << "Resulting Data: " << singleResult.first << " : " << singleResult.second << std::endl;
+//  }
+
+	result.push_back(singleResult);
+  return result;
+
+  
+  //	std::vector<std::pair<double, FairTimeStamp*> > result;
+  //        //std::vector<std::pair<double, CbmTrdDigi*> > result;
+  //  	result.push_back(oldData);
+  //	return result;
+      }; 
+
+    virtual double FindTimeForData(FairTimeStamp* data) ;
+    virtual void FillDataMap(FairTimeStamp* data, double activeTime) ;
+    virtual void EraseDataFromDataMap(FairTimeStamp* data);
+  protected:
+
+    std::map<CbmTrdDigi, double> fData_map;
+
+    ClassDef(CbmTrdDigiWriteoutBuffer, 1);
+};
+
+#endif /* CBMTRDDIGIWRITEOUTBUFFER_H_ */
+

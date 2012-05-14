e425b5a3fe193f88c8566fdc7eb6b407f3841f62
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2012-05-14 14:00:03 +0000

    Fix bug, un scripts correctly.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@15526 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/littrack/nightly_tests/run_test_electron.sh
+++ macro/littrack/nightly_tests/run_test_electron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-. ${VMCWORKDIR}/macro/littrack/nightly_tests/run_test_reco.sh littrack_global_reco_electron electron
+. ${VMCWORKDIR}/macro/littrack/nightly_tests/run_test_reco.sh littrack_global_reco_electron electron auau 25gev centr
--- macro/littrack/nightly_tests/run_test_muon.sh
+++ macro/littrack/nightly_tests/run_test_muon.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-. ${VMCWORKDIR}/macro/littrack/nightly_tests/run_test_reco.sh littrack_global_reco_muon muon
+. ${VMCWORKDIR}/macro/littrack/nightly_tests/run_test_reco.sh littrack_global_reco_muon muon auau 25gev centr

034f93b64cedd959cb5561c9cbc1966cfb095b0a
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2012-05-14 15:28:26 +0000

    Update scripts for TRD reconstruction testing.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@15531 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/littrack/scripts/trd_qa.sh
+++ macro/littrack/scripts/trd_qa.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
-export SCRIPT=yes
+. ${VMCWORKDIR}/macro/littrack/scripts/common.sh
 
-. $VMCWORKDIR/macro/littrack/common.sh
+export LIT_SCRIPT=yes
 
-export NEVENTS=$1
-export DETECTORSETUP=electron
+test_name=$1
+trd_hitproducer_type=$2
+trd_geom=$3
+trd_digi=$4
 
-create_output_dir /data.local1/andrey/events/trd_test_jan12/events_$2/
-create_result_dir /data.local1/andrey/events/trd_test_jan12/results_$2/
+create_output_dir events_${test_name}/
+create_result_dir ${test_name}/
+
+nevents=5
+export LIT_DETECTOR_SETUP=electron
 
 #     NMU+ NMU- NE- NE+ NPI+ NPI- NJPSIMU NJPSIE URQMD UNIGEN
-pars=(0    0    0   0   0    0    0       5      yes   no)
-set_simulation_parameters $pars 
+pars=(0    0    0   0   0    0    0       10     yes   no)
+set_simulation_parameters ${pars}    
 
 set_default_electron_geometry
 
-export GLOBALTRACKINGTYPE=branch
-export STSHITPRODUCERTYPE=real
-export TRDHITPRODUCERTYPE=$3
-export TRDGEOM=$4
-export TRDDIGI=$5
+export LIT_STS_HITPRODUCER_TYPE=real
+export LIT_TRD_HITPRODUCER_TYPE=${trd_hitproducer_type}
+export LIT_TRD_GEOM=${trd_geom}
+export LIT_TRD_DIGI=${trd_digi}
+export LIT_GLOBAL_TRACKING_TYPE=branch
 
-set_default_file_names $DIR 0000
-export URQMDFILE=/data.local1/andrey/tests/urqmd/auau/25gev/centr/urqmd.auau.25gev.centr.0000.ftn14
+set_default_file_names ${LIT_DIR} 0000
 
-$ROOTSYS/bin/root -b -q "./global_sim.C($NEVENTS)"
-$ROOTSYS/bin/root -b -q "./global_reco.C($NEVENTS, \"all\")"
-#$ROOTSYS/bin/root -b -q "./global_reco.C($NEVENTS, \"hits\")"
-#$ROOTSYS/bin/root -b -q "./global_reco.C($NEVENTS, \"tracking\")"
+export LIT_URQMD_FILE=/Users/andrey/Development/cbm/d/urqmd/auau/25gev/centr/urqmd.auau.25gev.centr.0000.ftn14
+    
+${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(${nevents})"
+${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_reco.C(${nevents}, \"all\")"
+#${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_reco.C(${nevents}, \"hits\")"
+#${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_reco.C(${nevents}, \"tracking\")"
 
-export SCRIPT=no
+export LIT_SCRIPT=no

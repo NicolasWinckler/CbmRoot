f7e0e5af07875283736d02fbcfc9d381cd3178d2
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2012-05-22 18:01:44 +0000

    modify pad sectors in mod1
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@15574 0381ead4-6506-0410-b988-94b70fbc4730

--- trd/v12x/CbmTrdPads12X_7.5mm_variable.h
+++ trd/v12x/CbmTrdPads12X_7.5mm_variable.h
@@ -3,6 +3,7 @@
 // -----               Created 03/12/2009 by D. Emschermann            -----
 // -------------------------------------------------------------------------
 //
+// v2.4 - 20120511 DE - v12f - modify sector size of mod1 to look different from mod2
 // v2.3 - 20120511 DE - v12c mod5 decrease pad size to 3.0 cm2 from 4.5 cm2
 // v2.2 - 20120511 DE - v12[b-x] - define layout for trd1mod[1-8] in common station1
 // v2.1 - 20120117 DE - v11x/v12x - even number of rows
@@ -39,15 +40,15 @@ using std::vector;
 using std::map;
 using std::pair;
 
-// v2.3
+// v2.4
   Int_t fst1_sect_count = 3;
   Float_t fst1_pad_type[8][3][4] =   // array of pad geometries in the TRD (trd1mod[1-8])
-    { { { 57., 21., .75,  1.5 }, 
-        { 57., 15., .75,  1.5 }, 
-        { 57., 21., .75,  1.5 } },     
+    { { { 57., 27., .75,  1.5 }, 
+        { 57.,  3., .75,  1.5 }, 
+        { 57., 27., .75,  1.5 } },     
         // mod1
         //  1.1 cm2
-        // 14 x  15 mm + 10 x  15 mm + 14 x  15 mm,   76 * 38 = 2888 pads
+        // 18 x  15 mm +  2 x  15 mm + 18 x  15 mm,   76 * 38 = 2888 pads
       { { 57., 21., .75,  3.0 }, 
         { 57., 15., .75,  2.5 }, 
         { 57., 21., .75,  3.0 } },     
@@ -60,12 +61,18 @@ using std::pair;
         // mod3
         //  4.5 cm2
         //  2 x  60 mm +  6 x  55 mm +  2 x  60 mm,   76 * 10 =  760 pads
-      { { 57., 19., .75,  9.5 }, 
-        { 57., 19., .75,  9.5 }, 
-        { 57., 19., .75,  9.5 } },     
-        // mod4
-        //  7.1 cm2
-        //  2 x  95 mm +  2 x  95 mm +  2 x  95 mm,   76 *  6 =  456 pads
+      { { 57., 12., .75,  6.0 }, 
+        { 57., 33., .75,  5.5 }, 
+        { 57., 12., .75,  6.0 } },     
+        // mod4 as mod3
+        //  4.5 cm2
+        //  2 x  60 mm +  6 x  55 mm +  2 x  60 mm,   76 * 10 =  760 pads
+//      { { 57., 19., .75,  9.5 }, 
+//        { 57., 19., .75,  9.5 }, 
+//        { 57., 19., .75,  9.5 } },     
+//        // mod4
+//        //  7.1 cm2
+//        //  2 x  95 mm +  2 x  95 mm +  2 x  95 mm,   76 *  6 =  456 pads
 //---
       { { 96., 44., .75,  4.0 },  
         { 96.,  8., .75,  4.0 },  

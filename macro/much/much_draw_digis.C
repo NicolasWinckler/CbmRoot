76d4743263b6aac593d5e52a48bf83d77a18aaf2
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2012-08-09 08:00:12 +0000

    visualization macro updated
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@16472 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/much/much_draw_digis.C
+++ macro/much/much_draw_digis.C
@@ -1,110 +1,103 @@
-void much_draw_digis(){
-  TString inputdir = gSystem->Getenv("VMCWORKDIR");
-  TString digiFile = inputdir + "/macro/much/data/much_digi_sector.root";
-  Int_t iStationSelected=0;
-  
+Bool_t IsSelected(Int_t detId, Int_t iStation,Int_t iLayer,Int_t iSide);
+
+void much_draw_digis(
+  TString mcFileName   = "data/mc_sector.root",
+  TString rcFileName   = "data/hits_sector.root",
+  TString digiFileName = "data/much_digi_sector.root",
+  Int_t iEvent = 0,
+  Int_t iStationSelected = 5,
+  Int_t iLayerSelected = 0,
+  Int_t iSideSelected = 0,
+  Double_t xmin = -60,
+  Double_t xmax =  60,
+  Double_t ymin = -60,
+  Double_t ymax =  60
+){
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
   basiclibs();
   gROOT->LoadMacro("$VMCWORKDIR/macro/much/muchlibs.C");
   muchlibs();
 
-  // digi scheme
-  TFile* file=new TFile("data/much_digi_sector.root");
-  TObjArray* stations = (TObjArray*) file->Get("stations");
+  // init digi scheme
+  TFile* digiFile = new TFile(digiFileName);
+  TObjArray* stations = (TObjArray*) digiFile->Get("stations");
   CbmMuchGeoScheme* digiScheme = CbmMuchGeoScheme::Instance();
   digiScheme->Init(stations);
-
-  TFile* fPoints = new TFile("data/Jpsi.auau.25gev.centr.mc.root");
-  TTree* tPoints = (TTree*) fPoints->Get("cbmsim");
-  printf("Test %i\n",tPoints->GetEntries());
   
+  // get TClonesArrays from trees
+  TFile* mcFile = new TFile(mcFileName);
+  TFile* rcFile = new TFile(rcFileName);
+  TTree* mcTree = (TTree*) mcFile->Get("cbmsim");
+  TTree* rcTree = (TTree*) rcFile->Get("cbmsim");
   TClonesArray* points = new TClonesArray("CbmMuchPoint");
-  tPoints->SetBranchAddress("MuchPoint",&points);
+  TClonesArray* digis  = new TClonesArray("CbmMuchDigi");
+  TClonesArray* hits   = new TClonesArray("CbmMuchPixelHit");
+  mcTree->SetBranchAddress("MuchPoint",&points);
+  rcTree->SetBranchAddress("MuchDigi",&digis);
+  rcTree->SetBranchAddress("MuchPixelHit",&hits);
+  mcTree->GetEntry(iEvent);
+  rcTree->GetEntry(iEvent);
   
-  tPoints->GetEntry(0);
-  
-  TFile* fDigis = new TFile("data/Jpsi.auau.25gev.centr.muchhits.root");
-  TTree* tDigis = (TTree*) fDigis->Get("cbmsim");
-  printf("Test %i\n",tDigis->GetEntries());
-  
-  TClonesArray* digis = new TClonesArray("CbmMuchDigi");
-  tDigis->SetBranchAddress("MuchDigi",&digis);
-  
-  tDigis->GetEntry(0);
-  printf("nDigis:%i\n",digis->GetEntriesFast());
-
+  // Mark fired pads
   for (Int_t i=0;i<digis->GetEntriesFast();i++){
     CbmMuchDigi* digi = (CbmMuchDigi*) digis->At(i);
-    Int_t detId        = digi->GetDetectorId();
-    Long64_t channelId = digi->GetChannelId();
-    Int_t iStation = digiScheme->GetStationIndex(detId);
-    Int_t iLayer   = digiScheme->GetLayerIndex(detId);
-    Int_t iSide    = digiScheme->GetLayerSideIndex(detId);
-//
-    if (iStation!=iStationSelected || iLayer!=0) continue;
-
-//    printf("detId=%5i channelId=%i ",detId,channelId);
-//    printf("iStation=%i iLayer=%i iSide=%i\n",iStation,iLayer,iSide);
-    CbmMuchModuleSector* module = (CbmMuchModuleSector*) digiScheme->GetModuleByDetId(detId);
-//    module->DrawModule(kRed);
-    CbmMuchRadialSector* sector = module->GetSector(channelId);
-    Int_t iSector  = module->GetSectorIndex(channelId);
-    Int_t iChannel = module->GetChannelIndex(channelId);
-    printf("iSector=%i  ",iSector);
-    printf("iChannel=%i\n",iChannel);
-    sector->SetFillColor(kBlue);
-//    sector->Draw("f");
-    if (iChannel>=sector->GetNChannels()) printf("Warning\n");
-    CbmMuchRadialPad* pad = sector->GetPad(iChannel);
-    if (!pad) continue; 
-    pad->SetFired(i,digi->GetADCCharge());
-//    printf("iPad=%i\n",pad);
-//    if (pad) pad->Draw("f");
- 
+    // Filter out points
+    Int_t detId = digi->GetDetectorId();
+    if (!IsSelected(detId,iStationSelected,iLayerSelected,iSideSelected)) continue;
+    CbmMuchModuleGem* module = (CbmMuchModuleGem*)digiScheme->GetModuleByDetId(detId);
+    if (!module) continue;
+    module->SetPadFired(digi->GetChannelId(),i,digi->GetADCCharge());
   }
 
   // Draw pads
-  TCanvas* c1 = new TCanvas("station","station",1000,1000);
-  c1->SetFillColor(0);
-  c1->Range(-80,-80,80,80);
-  CbmMuchStation* station = (CbmMuchStation*) stations->At(iStationSelected);
-  CbmMuchLayer* layer = station->GetLayer(0);
+  TCanvas* c1 = new TCanvas("station","station",1000*(xmax-xmin)/(ymax-ymin),1000);
+  c1->Range(xmin,ymin,xmax,ymax);
   for (Int_t iSide=1;iSide>=0;iSide--){
-    CbmMuchLayerSide* layerSide = layer->GetSide(iSide);
-    for (Int_t iModule=0;iModule<layerSide->GetNModules();++iModule) {
-      CbmMuchModule* mod = layerSide->GetModule(iModule);
-      mod->SetFillStyle(0);
-//        mod->Draw();
-      if(mod->GetDetectorType() != 3) continue;
-      CbmMuchModuleSector* module = (CbmMuchModuleSector*)mod;
-      for (Int_t iSector=0;iSector<module->GetNSectors();++iSector){
-        CbmMuchRadialSector* sector = module->GetSector(iSector);
-//        sector->Draw("f");
-        sector->DrawPads();
-      } // sectors
-    } // modules
-  } // sides
+    if (iSideSelected!=2 && iSide!=iSideSelected)continue;
+    CbmMuchLayerSide* side = digiScheme->GetLayerSide(iStationSelected,iLayerSelected,iSide);
+    for (Int_t iModule=0;iModule<side->GetNModules();++iModule) {
+      CbmMuchModuleGem* module = (CbmMuchModuleGem*) side->GetModule(iModule);
+      if (!module) continue;
+      module->DrawPads();
+    }
+  }
 
   // Draw points
   for (Int_t i=0;i<points->GetEntriesFast();i++){
     CbmMuchPoint* point = (CbmMuchPoint*) points->At(i);
     Int_t detId = point->GetDetectorId();
-    Int_t iStation = digiScheme->GetStationIndex(detId);
-    Int_t iLayer   = digiScheme->GetLayerIndex(detId);
-    Int_t iSide    = digiScheme->GetLayerSideIndex(detId);
-    if (iStation!=iStationSelected || iLayer!=0) continue;
-    Double_t x = 0.5*(point->GetXIn()+point->GetXOut());
-    Double_t y = 0.5*(point->GetYIn()+point->GetYOut());
-    TEllipse* p = new TEllipse(x,y,0.2);
-    p->SetFillColor(kBlack);
-    p->SetLineColor(kBlack);
-    p->Draw();
-    TLine* l = new TLine(point->GetXIn(),point->GetYIn(),point->GetXOut(),point->GetYOut());
+    if (!IsSelected(detId,iStationSelected,iLayerSelected,iSideSelected)) continue;
+    TLine* l = new TLine(point->GetXIn()-0.1,point->GetYIn()-0.1,point->GetXOut()+0.1,point->GetYOut()+0.1);
     l->SetLineColor(kBlack);
+    l->SetLineWidth(3);
     l->Draw();
   }
+
+  // Draw hits
+  for (Int_t i=0;i<hits->GetEntriesFast();i++){
+    CbmMuchPixelHit* hit = (CbmMuchPixelHit*) hits->At(i);
+    Int_t detId = hit->GetDetectorId();
+    if (!IsSelected(detId,iStationSelected,iLayerSelected,iSideSelected)) continue;
+    TEllipse* p = new TEllipse(hit->GetX(),hit->GetY(),0.12);
+    p->SetFillColor(kRed);
+    p->SetLineColor(kRed);
+    p->Draw();
+  }
+
   
   // Draw a hole
-  TArc* holeArc = new TArc(0.,0.,station->GetRmin());
+  TArc* holeArc = new TArc(0.,0.,digiScheme->GetStation(iStationSelected)->GetRmin());
   holeArc->Draw();
+  c1->Print("station.png");
+}
+
+Bool_t IsSelected(Int_t detId, Int_t iStationSelected,Int_t iLayerSelected,Int_t iSideSelected){
+  Int_t iStation = CbmMuchGeoScheme::GetStationIndex(detId);
+  Int_t iLayer   = CbmMuchGeoScheme::GetLayerIndex(detId);
+  Int_t iSide    = CbmMuchGeoScheme::GetLayerSideIndex(detId);
+  if (iStation!=iStationSelected) return 0;
+  if (iLayer!=iLayerSelected) return 0;
+  if (iSideSelected==2) return 1;
+  if (iSide!=iSideSelected) return 0;
+  return 1;
 }

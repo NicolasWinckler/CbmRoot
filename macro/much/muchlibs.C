9cf3ea31280bc516d73001542d9973e5917ccded
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2012-08-10 15:14:30 +0000

    updated macro to load libraries for much simulations
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@16510 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/much/muchlibs.C
+++ macro/much/muchlibs.C
@@ -5,22 +5,27 @@ void muchlibs(){
   gSystem->Load("libCbmBase");
   gSystem->Load("libCbmData");
   gSystem->Load("libCbmGenerators");
-  gSystem->Load("libRun");
   gSystem->Load("libField");
   gSystem->Load("libGen");
   gSystem->Load("libPassive");
   gSystem->Load("libTrkBase");
   gSystem->Load("libGeane");
+
   gSystem->Load("libSts");
-  gSystem->Load("libRich");
-  gSystem->Load("libMuch");
   gSystem->Load("libTrd");
-  gSystem->Load("libTof");
   gSystem->Load("libEcal");
-  gSystem->Load("libGlobal");
+
   gSystem->Load("libKF");
   gSystem->Load("libL1");
+  gSystem->Load("libMvd");
+
   gSystem->Load("libLittrack");
+
+  gSystem->Load("libRich");
+  gSystem->Load("libMuch");
+  gSystem->Load("libTof");
+  gSystem->Load("libGlobal");
+
   gSystem->Load("libMinuit2");
-  gSystem->Load("libAnalysis");
+//gSystem->Load("libAnalysis");
 }

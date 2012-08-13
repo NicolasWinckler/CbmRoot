232a9d9a7f54dbb2e9f2124f8060e8d7e2c2120e
kryshen <kryshen@0381ead4-6506-0410-b988-94b70fbc4730>
2012-08-13 08:24:47 +0000

    unnecessary data members/constructors removed, added new constructor for clusters with only one digi
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@16533 0381ead4-6506-0410-b988-94b70fbc4730

--- cbmdata/much/CbmMuchCluster.cxx
+++ cbmdata/much/CbmMuchCluster.cxx
@@ -1,69 +1,37 @@
-/** CbmMuchCluster.cxx
- *@author Mikhail Ryzhinskiy <m.ryzhinskiy@gsi.de>
- *@since 10.11.07
- *@version 1.0
- **
- ** Class describing a cluster consising of fired pads
- **/
-
 #include "CbmMuchCluster.h"
 
 
-// ------ Default constructor ----------------------------------------------
+// -------------------------------------------------------------------------
 CbmMuchCluster::CbmMuchCluster()
   : TObject(),
-    fDigiIndices(),
-    fCharge(0),
-    fQMax(0)
+    fDigiIndices()
 {
 }
 // -------------------------------------------------------------------------
 
 
-// ------ Standard constructor  --------------------------------------------
-CbmMuchCluster::CbmMuchCluster(vector<Int_t> digiIndices): TObject(),
+// -------------------------------------------------------------------------
+CbmMuchCluster::CbmMuchCluster(Int_t iDigi)
+  : TObject(),
+    fDigiIndices(1,&iDigi)
+{
+}
+// -------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
+CbmMuchCluster::CbmMuchCluster(vector<Int_t> digiIndices)
+  : TObject(),
     fDigiIndices(digiIndices.size(),&(digiIndices[0]))
 {
 }
 // -------------------------------------------------------------------------
 
-
-// ------ Standard constructor  --------------------------------------------
-CbmMuchCluster::CbmMuchCluster(vector<Int_t> digiIndices, Int_t charge)
+// -------------------------------------------------------------------------
+CbmMuchCluster::CbmMuchCluster(vector<Int_t> digiIndices, Int_t /* dummy*/, Int_t /* dummy*/)
   : TObject(),
-    fDigiIndices(),
-    fCharge(charge),
-    fQMax(0)
+    fDigiIndices(digiIndices.size(),&(digiIndices[0]))
 {
-   Int_t nDigis = digiIndices.size();
-   Int_t inc = 0;
-   fDigiIndices.Set(nDigis);
-   for(vector<Int_t>::iterator it = digiIndices.begin(); it != digiIndices.end(); it++){
-      fDigiIndices.AddAt(*it, inc++);
-   }
 }
 // -------------------------------------------------------------------------
-
-// ------ Standard constructor  --------------------------------------------
-CbmMuchCluster::CbmMuchCluster(vector<Int_t> digiIndices, Int_t charge , Int_t qMax)
-  : TObject(),
-    fDigiIndices(),
-    fCharge(charge),
-    fQMax(qMax)
-{
-   Int_t nDigis = digiIndices.size();
-   Int_t inc = 0;
-   fDigiIndices.Set(nDigis);
-   for(vector<Int_t>::iterator it = digiIndices.begin(); it != digiIndices.end(); it++){
-      fDigiIndices.AddAt(*it, inc++);
-   }
-}
-// -------------------------------------------------------------------------
-
-// ------  Destructor  -----------------------------------------------------
-CbmMuchCluster::~CbmMuchCluster(){
-}
-// -------------------------------------------------------------------------
-
 
 ClassImp(CbmMuchCluster)
--- cbmdata/much/CbmMuchCluster.h
+++ cbmdata/much/CbmMuchCluster.h
@@ -1,11 +1,14 @@
-/** CbmMuchCluster.h
+/** CbmMuchCluster.cxx
+ *@author Evgeny Kryshen <e.kryshen@gsi.de>
+ *@since 13.08.12
+ *@version 2.0
+
  *@author Mikhail Ryzhinskiy <m.ryzhinskiy@gsi.de>
  *@since 10.11.07
  *@version 1.0
  **
  ** Class describing a cluster consisting of fired pads
  **/
-
 #ifndef CBMMUCHCLUSTER_H
 #define CBMMUCHCLUSTER_H 1
 
@@ -18,51 +21,22 @@ using std::vector;
 class CbmMuchCluster : public TObject{
 
 public:
-
-  /**
-   * Default constructor.
-   */
   CbmMuchCluster();
+  CbmMuchCluster(Int_t iDigi);
   CbmMuchCluster(vector<Int_t> digiIndices);
-  /**
-   * Standard constructor.
-   * @param digiIndices Indices of the digis that are contained in the cluster
-   * @param charge      The whole cluster charge
-   */
-  CbmMuchCluster(vector<Int_t> digiIndices, Int_t charge);
-  /**
-   * Standard constructor.
-   * @param digiIndices Indices of the digis that are contained in the cluster
-   * @param charge      The whole cluster charge
-   * @param qMax        Maximum charge on a pad from the cluster
-   */
-  CbmMuchCluster(vector<Int_t> digiIndices, Int_t charge, Int_t qMax);
-
-  ~CbmMuchCluster();
-
+  CbmMuchCluster(vector<Int_t> digiIndices, Int_t dum1, Int_t dum2);
+  ~CbmMuchCluster(){}
   /** Gets TArrayI of digi indices. */
   TArrayI GetDigiIndices() const {return fDigiIndices;}
-
   /** Gets number of digis in the cluster. */
   Int_t GetNDigis() const {return fDigiIndices.GetSize();}
-
   /**
    * Gets the index of a digi.
    * @param iDigi Zero based ordinal number of digi in the internal list.
    */
   Int_t GetDigiIndex(Int_t iDigi) const {return fDigiIndices.At(iDigi);}
-
-  /** Gets total charge in the cluster. */
-  Int_t GetCharge() const {return fCharge;}
-
-  /** Maximum charge collected by one of the cluster's pad. */
-  Int_t GetMaxCharge() const {return fQMax;}
-
-
 private:
-  TArrayI             fDigiIndices;       // Array of digi indices which form the cluster
-  Int_t               fCharge;            // Summary charge of the cluster
-  Int_t               fQMax;              // Maximal charge collected by one of pads of the cluster
+  TArrayI fDigiIndices; // Array of digi indices which form the cluster
 
 ClassDef(CbmMuchCluster,1)
 };

a8d38529013ae99a1c37843b457e7a993046ae19
bergmann <bergmann@0381ead4-6506-0410-b988-94b70fbc4730>
2012-08-18 10:45:11 +0000

    new variable for urqmd input
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@16669 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/run_pi0.sh
+++ macro/trd/run_pi0.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# i can be between 0 and 199
-NEVENTS=100
-#for i in {0..1}
+# NEVENTS can be between 1 and 500
+NEVENTS=500
+# NURQMD can be between 0 and 199
+NURQMD=0
+#for i in {0..${NURQMD}}
 #do
 #root -l -b -q run_sim_pi0.C'('${NEVENTS},${i}')'
 #done
-for i in {0..0}
+for i in {0..${NURQMD}}
 do
 root -l -b -q run_reco_pi0.C'('${NEVENTS},${i}')'
 done

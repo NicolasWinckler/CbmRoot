e4c2eaa906a44c26168241e1978ba32e71bd40a2
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2012-09-17 13:04:39 +0000

    New scripts fo commit tests. Separate testing of sim and reo.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@17074 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/commit_tests/run_test.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+. ${VMCWORKDIR}/macro/littrack/scripts/common.sh
+
+export LIT_SCRIPT=yes
+
+#all, sim, reco
+setup=$1
+opt=$2
+
+if [ "${opt}" = "all" ] ; then
+   create_output_dir events_${setup}/
+   create_result_dir results_${setup}/
+fi
+
+nevents=5
+
+if [ "${setup}" = "electron" ] ; then
+   #     NMU+ NMU- NE- NE+ NPI+ NPI- NJPSIMU NJPSIE AU URQMD UNIGEN
+   pars=(0    0    0   0   0    0    0       5      0  yes   yes)
+   set_simulation_parameters ${pars}
+   set_default_electron_geometry
+elif [ "${setup}" = "muon" ] ; then
+   #     NMU+ NMU- NE- NE+ NPI+ NPI- NJPSIMU NJPSIE AU URQMD UNIGEN
+   pars=(0    0    0   0   0    0    5       0      0  yes   yes)
+   set_simulation_parameters ${pars}
+   set_default_muon_geometry
+fi
+
+export LIT_STS_HITPRODUCER_TYPE=real
+export LIT_TRD_HITPRODUCER_TYPE=smearing
+export LIT_MUCH_HITPRODUCER_TYPE=advanced
+export LIT_GLOBAL_TRACKING_TYPE=branch
+
+set_default_file_names ${LIT_DIR} 0000
+
+export LIT_URQMD_FILE=${VMCWORKDIR}/input/urqmd.auau.25gev.centr.root
+
+if [ "${opt}" = "all" ] ; then
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(${nevents})"
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_reco.C(${nevents}, \"all\")"
+elif [ "${opt}" = "sim" ] ; then
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(${nevents})"
+elif [ "${opt}" = "reco" ] ; then
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_reco.C(${nevents}, \"all\")"
+fi
+
+export LIT_SCRIPT=no

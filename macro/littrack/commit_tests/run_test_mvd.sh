b17b145a52cb2e8df666a5ccefad37eaeddee74b
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2012-09-17 14:03:55 +0000

    Run MVD test again.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@17078 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/littrack/commit_tests/run_test_mvd.sh
+++ macro/littrack/commit_tests/run_test_mvd.sh
@@ -4,10 +4,19 @@
 
 export LIT_SCRIPT=yes
 
-mvd_delta=$1
+setup=$1
+opt=$2
 
-create_output_dir events_mvd_${mvd_delta}/
-create_result_dir results_mvd_${mvd_delta}/
+create_output_dir events_mvd_${setup}/
+create_result_dir results_mvd_${setup}/
+
+if [ "${opt}" = "all" ] ; then
+   create_output_dir events_mvd_${setup}/
+   create_result_dir results_mvd_${setup}/
+else
+   export LIT_DIR=events_mvd_${setup}/
+   export LIT_RESULT_DIR=results_mvd_${setup}/
+fi
 
 nevents=5
 
@@ -21,27 +30,38 @@ set_default_file_names ${LIT_DIR} 0000
 
 export LIT_URQMD_FILE=${VMCWORKDIR}/input/urqmd.auau.25gev.centr.root
 
-# Generate file for delta electrons
-if [ "${mvd_delta}" = "delta" ] ; then
-    #     NMU+ NMU- NE- NE+ NPI+ NPI- NJPSIMU NJPSIE AU URQMD UNIGEN
-    pars=(0    0    0   0   0    0    0       0      1  no    no)
-    set_simulation_parameters ${pars}
-    export LIT_MC_FILE=${dir}/mc.delta.0000.root
-    export LIT_PAR_FILE=${dir}/param.delta.0000.root
-    ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(20)"
-    export LIT_NOF_MVD_DELTA_EVENTS=10
-    export LIT_MC_FILE=${dir}/mc.0000.root
-    export LIT_PAR_FILE=${dir}/param.0000.root
-fi
+function run_delta() {
+   # Generate file for delta electrons
+   if [ "${setup}" = "delta" ] ; then
+       #     NMU+ NMU- NE- NE+ NPI+ NPI- NJPSIMU NJPSIE AU URQMD UNIGEN
+       pars=(0    0    0   0   0    0    0       0      1  no    no)
+       set_simulation_parameters ${pars}
+       export LIT_MC_FILE=${LIT_DIR}/mc.delta.0000.root
+       export LIT_PAR_FILE=${LIT_DIR}/param.delta.0000.root
+       ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(20)"
+       export LIT_NOF_MVD_DELTA_EVENTS=10
+       export LIT_MC_FILE=${LIT_DIR}/mc.0000.root
+       export LIT_PAR_FILE=${LIT_DIR}/param.0000.root
+   fi
+}
 
 #     NMU+ NMU- NE- NE+ NPI+ NPI- NJPSIMU NJPSIE AU URQMD UNIGEN
 pars=(0    0    0   0   0    0    0       10      0  yes   yes)
 set_simulation_parameters ${pars}   
 
-export LIT_MC_FILE=${dir}/mc.0000.root
-export LIT_PAR_FILE=${dir}/param.0000.root
- 
-${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(${nevents})"
-${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/mvd_reco.C(${nevents})"
+export LIT_MC_FILE=${LIT_DIR}/mc.0000.root
+export LIT_PAR_FILE=${LIT_DIR}/param.0000.root
+
+if [ "${opt}" = "all" ] ; then
+   run_delta
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(${nevents})"
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/mvd_reco.C(${nevents})"
+elif [ "${opt}" = "sim" ] ; then
+   run_delta
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/global_sim.C(${nevents})"
+elif [ "${opt}" = "reco" ] ; then
+   ${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/mvd_reco.C(${nevents})"
+fi
+
 
 export LIT_SCRIPT=no

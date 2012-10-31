1ced41b50f7556b5b85f44ae0b3faa8946686640
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2012-10-31 10:08:40 +0000

    Add pragmas for drawing global functions to be able to use them in macro.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@17698 0381ead4-6506-0410-b988-94b70fbc4730

--- cbmbase/CbmBaseLinkDef.h
+++ cbmbase/CbmBaseLinkDef.h
@@ -41,5 +41,14 @@
 #pragma link C++ class CbmMCList+;
 
 #pragma link C++ class CbmModule+;
+
+#pragma link C++ function SetDefaultDrawStyle();
+#pragma link C++ function DrawH1(TH1*, HistScale, HistScale, const string&, Int_t, Int_t, Int_t, Int_t, Int_t);
+#pragma link C++ function DrawH2(TH2*, HistScale, HistScale, HistScale, const string&);
+#pragma link C++ function DrawH1(const vector<TH1*>&, const vector<string>&, HistScale, HistScale, Bool_t, Double_t, Double_t, Double_t, Double_t, const string&);
+#pragma link C++ function DrawGraph(TGraph*, HistScale, HistScale, const string&, Int_t, Int_t, Int_t, Int_t, Int_t);
+#pragma link C++ function DrawGraph(const vector<TGraph*>&, const vector<string>&, HistScale, HistScale, Bool_t, Double_t, Double_t, Double_t, Double_t);
+#pragma link C++ function DrawGraph2D(TGraph2D*, HistScale, HistScale, HistScale, const string&);
+
 #endif
 

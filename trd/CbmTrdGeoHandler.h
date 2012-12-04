40342d458bf4fc0780a7faccc39b6dc1e9c366e2
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2012-12-04 21:50:34 +0000

    extend CbmTrdGeoHandler for root geometries
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@18124 0381ead4-6506-0410-b988-94b70fbc4730

--- trd/CbmTrdGeoHandler.h
+++ trd/CbmTrdGeoHandler.h
@@ -79,6 +79,7 @@ class CbmTrdGeoHandler : public TObject {
 
   Bool_t GetLayerInfoFromOldGeometry(std::vector<Int_t> &layersBeforStation);
   Bool_t GetLayerInfoFromNewGeometry(std::vector<Int_t> &layersBeforStation);
+  Bool_t GetLayerInfoFromRootGeometry(std::vector<Int_t> &layersBeforStation);
   void FillDetectorInfoArray(Int_t uniqueId);
   void NavigateTo(TString volName);
 

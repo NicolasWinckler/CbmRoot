47f1bee00c7f1cb7a0a376c91d2aa74f9bf16bc7
andrey <andrey@0381ead4-6506-0410-b988-94b70fbc4730>
2013-01-25 12:53:45 +0000

    Add scripts to run rad len test for different TRD geometries.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@18387 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/littrack/scripts/radlength_qa.sh
@@ -0,0 +1,22 @@
+. ${VMCWORKDIR}/macro/littrack/scripts/common.sh
+
+export LIT_SCRIPT=yes
+
+test_name=$1
+trd_geom=$2
+
+create_output_dir events_${test_name}/
+create_result_dir ${test_name}/
+
+nevents=50000
+
+export LIT_CAVE_GEOM=cave.geo
+export LIT_TRD_GEOM=${trd_geom}
+export LIT_MC_FILE=${LIT_DIR}/radlength.mc.0000.root
+export LIT_PAR_FILE=${LIT_DIR}/radlength.param.0000.root
+export LIT_RAD_LENGTH_QA_FILE=${LIT_DIR}/radlength.qa.0000.root
+    
+${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/radlength_sim.C(${nevents})"
+${ROOTSYS}/bin/root -b -q -l "${VMCWORKDIR}/macro/littrack/radlength_ana.C(${nevents})"
+ 
+export LIT_SCRIPT=no
---
+++ macro/littrack/scripts/run_radlength_qa.sh
@@ -0,0 +1,4 @@
+xterm -hold -e ". ./radlength_qa.sh rl_trd_v10b trd/trd_v10b.geo"&
+xterm -hold -e ". ./radlength_qa.sh rl_trd_v11c trd/trd_v11c.geo"&
+xterm -hold -e ". ./radlength_qa.sh rl_trd_v12b trd/trd_v12b.geo"&
+xterm -hold -e ". ./radlength_qa.sh rl_trd_v13b trd/trd_v13b.root"&

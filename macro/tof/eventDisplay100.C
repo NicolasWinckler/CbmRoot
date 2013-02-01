15bf8a7dd0ad7fc476048ffc9fd62c6434735d8f
uhlig <uhlig@0381ead4-6506-0410-b988-94b70fbc4730>
2013-02-01 17:31:42 +0000

    Initial resion of macro to create the tof digitization parameter.
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@18439 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/tof/eventDisplay100.C
+++ macro/tof/eventDisplay100.C
@@ -2,7 +2,7 @@ eventDisplay100()
 {
 
   TString  InputFile     ="data/auaumbias.root";
-  TString  ParFile       ="data/testparams.root";
+  TString  ParFile       ="data/params.root";
 
 
   gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
@@ -31,8 +31,8 @@ eventDisplay100()
   FairRunAna *fRun= new FairRunAna();
 
 //  fRun->SetInputFile(InputFile.Data());
-  fRun->SetInputFile("data/auaumbias.root");
-  fRun->AddFriend("data/auaumbiasHits.root");
+  fRun->SetInputFile("data/test.mc.root");
+//  fRun->AddFriend("data/auaumbiasHits.root");
   fRun->SetOutputFile("data/test.root");
 
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
@@ -44,22 +44,22 @@ eventDisplay100()
   FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
   //  FairMCPointDraw *RichPoint =   new FairMCPointDraw ("RichPoint",kOrange,  kFullSquare);
   FairMCPointDraw *TofPoint =    new FairMCPointDraw ("TofPoint",kBlue,  kFullSquare);
-  FairMCPointDraw *TrdPoint= new FairMCPointDraw ("TrdPoint",kTeal,  kFullSquare);
-  FairMCPointDraw *EcalPoint =    new FairMCPointDraw ("EcalPoint",kYellow,  kFullSquare);
-  FairMCPointDraw *RefPlanePoint = new FairMCPointDraw ("RefPlanePoint",kPink,  kFullSquare);
-  FairMCPointDraw *StsPoint = new FairMCPointDraw ("StsPoint",kCyan,  kFullSquare);
+//  FairMCPointDraw *TrdPoint= new FairMCPointDraw ("TrdPoint",kTeal,  kFullSquare);
+  //FairMCPointDraw *EcalPoint =    new FairMCPointDraw ("EcalPoint",kYellow,  kFullSquare);
+  //FairMCPointDraw *RefPlanePoint = new FairMCPointDraw ("RefPlanePoint",kPink,  kFullSquare);
+  //FairMCPointDraw *StsPoint = new FairMCPointDraw ("StsPoint",kCyan,  kFullSquare);
    
-  CbmPixelHitSetDraw *TofHits =   new CbmPixelHitSetDraw ("TofHit",kRed,  kFullSquare);
+  //CbmPixelHitSetDraw *TofHits =   new CbmPixelHitSetDraw ("TofHit",kRed,  kFullSquare);
                                                               
   fMan->AddTask(Track);
   
   //  fMan->AddTask(RichPoint);
-  fMan->AddTask(EcalPoint);   
+  //fMan->AddTask(EcalPoint);   
   fMan->AddTask(TofPoint);   
-  fMan->AddTask(TofHits);   
-  fMan->AddTask(TrdPoint);
-  fMan->AddTask(RefPlanePoint);
-  fMan->AddTask(StsPoint);
+  //fMan->AddTask(TofHits);   
+  //fMan->AddTask(TrdPoint);
+  //fMan->AddTask(RefPlanePoint);
+  //fMan->AddTask(StsPoint);
  
     
   fMan->Init();                     

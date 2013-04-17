3df19d64084736be3a17398b4ac5325fe0b2bfb7
slebedev <slebedev@0381ead4-6506-0410-b988-94b70fbc4730>
2013-04-17 14:37:17 +0000

    comment ENNParallel due to the compilation error on some machines
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@19354 0381ead4-6506-0410-b988-94b70fbc4730

--- rich/CbmRichReconstruction.cxx
+++ rich/CbmRichReconstruction.cxx
@@ -19,7 +19,7 @@
 #include "CbmRichRingFinderHough.h"
 #include "CbmRichRingFinderIdeal.h"
 #include "CbmL1RichENNRingFinder.h"
-#include "CbmL1RichENNRingFinderParallel.h"
+//#include "CbmL1RichENNRingFinderParallel.h"
 #include "prototype/CbmRichProtRingFinderHough.h"
 
 #include "CbmRichRingFitterCircle.h"
@@ -163,7 +163,7 @@ void CbmRichReconstruction::InitFinder()
    } else if (fFinderName == "enn"){
       fRingFinder = new CbmL1RichENNRingFinder(0);
    } else if ((fFinderName == "enn_parallel")){
-      fRingFinder = new CbmL1RichENNRingFinderParallel(0);
+     // fRingFinder = new CbmL1RichENNRingFinderParallel(0);
    } else if (fFinderName == "hough_prototype") {
       fRingFinder = new CbmRichProtRingFinderHough();
    } else {

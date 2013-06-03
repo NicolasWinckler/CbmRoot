683f977732ef428971ddd9eb9cd1f9d55192ef4b
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2013-06-03 15:00:06 +0000

    update copy script
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@20082 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/copy_trd_geometries.sh
+++ macro/trd/copy_trd_geometries.sh
@@ -6,22 +6,23 @@ echo \# copy geometries
 cp -v geometry/trd.v13/media.geo ../../geometry
 cp -v geometry/trd.v13/trd_v13?.root ../../geometry/trd
 cp -v geometry/trd.v13/trd_v13?.digi.par ../../parameters/trd
+echo
 #ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13c.digi.par 
 #ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13d.digi.par 
 #ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13e.digi.par 
 #ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13f.digi.par 
 #ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13g.digi.par 
-ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13h.digi.par 
-ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13i.digi.par 
+#ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13h.digi.par 
+#ln -s ../../parameters/trd/trd_v13b.digi.par ../../parameters/trd/trd_v13i.digi.par 
 #
-cp -v geometry/trd.v12/trd_v12[b-d,f].geo ../../geometry/trd
-cp -v geometry/trd.v12/trd_v12[b-d,f].digi.par ../../parameters/trd
+#cp -v geometry/trd.v12/trd_v12[b-d,f].geo ../../geometry/trd
+#cp -v geometry/trd.v12/trd_v12[b-d,f].digi.par ../../parameters/trd
 #
 #cp -v geometry/trd.v12/trd_v12a.geo ../../geometry/trd
 #cp -v geometry/trd.v12/media.geo    ../../geometry
 #cp -v geometry/trd.v11/v11?/*.geo* ../../geometry/trd
 #cp -v geometry/trd.v11/*.geo       ../../geometry/trd
-echo
+#echo
 #echo \# copy digi.par files
 #cp -v geometry/trd.v12/trd_v12a_7.5mm_1cm2.digi.par ../../parameters/trd/trd_v12a.digi.par
 #cp -v ../../parameters/trd/v11a/trd_v11a_7.5mm_01cm2.digi.par ../../parameters/trd/trd_v11a.digi.par

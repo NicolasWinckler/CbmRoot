a560aa0aa503ac26575420f7a21ab37ffdc5f840
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2013-10-11 14:38:39 +0000

    remove target, add empty string for platform, prepare for pipe in root format
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@22050 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/trd/geometry/setup/sis300_electron_setup.C
@@ -0,0 +1,49 @@
+//
+//   comments
+//
+// 2013-10-11 - DE - add empty string defining the platform                                                                        
+// 2013-10-07 - DE - pipe_v13d.geo fixes overlap with TOF
+// 2013-10-07 - DE - rich_v13a is not at nominal position 1800mm, but at 1600mm
+//
+
+static TString caveGeom;
+static TString pipeGeom;
+static TString magnetGeom;
+static TString mvdGeom;
+static TString stsGeom;
+static TString richGeom;
+static TString muchGeom;
+static TString trdGeom;
+static TString tofGeom;
+static TString ecalGeom;
+static TString platformGeom;
+
+static TString  fieldMap;
+static Double_t fieldZ;
+static Double_t fieldScale;
+static Int_t    fieldSymType;
+
+void sis300_electron_setup()
+{
+  FairLogger* logger = FairLogger::GetLogger();
+
+  // -----   Magnetic field   -----------------------------------------------
+  fieldMap     = "field_v12b";   // name of field map
+  fieldZ       = 40.;            // field centre z position
+  fieldScale   =  1.;            // field scaling factor
+  fieldSymType =  3;
+
+  // -----  Geometries  -----------------------------------------------------
+  caveGeom     = "cave.geo";
+  pipeGeom     = "pipe/pipe_v13b.geo"; // "pipe/pipe_v13b.geo.root";
+  magnetGeom   = "magnet/magnet_v12b.geo.root";
+  mvdGeom      = "mvd/mvd_v07a.geo";
+  stsGeom      = "sts/sts_v13c.geo.root";
+  richGeom     = "rich/rich_v13a.gdml";
+  muchGeom     = "";
+  platformGeom = ""; // "passive/platform_v13b.geo";
+  trdGeom      = "trd/trd_v13g.geo.root";
+  tofGeom      = "tof/tof_v13-4d.geo.root";
+  ecalGeom     = "";
+  cout<<"Reading geometry setup and magnetic field settings from "<< gSystem->UnixPathName(gInterpreter->GetCurrentMacroName())<<endl;
+}

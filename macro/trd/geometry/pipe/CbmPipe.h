2612e481239d3ecf14e8fdf17510e0b9f3835b0a
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2013-10-16 13:49:03 +0000

    fix pipe.root WARNING by declaring CheckIfSensitive
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@22271 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macro/trd/geometry/pipe/CbmPipe.h
@@ -0,0 +1,22 @@
+#ifndef PIPE_H
+#define PIPE_H
+
+#include "FairModule.h"
+
+class CbmPipe : public FairModule {
+  public:
+    CbmPipe(const char * name, const char *Title="CBM Pipe");
+    CbmPipe();
+
+    virtual ~CbmPipe();
+    virtual void ConstructGeometry();
+    virtual void ConstructAsciiGeometry();
+
+    Bool_t CheckIfSensitive(std::string name);
+   
+  ClassDef(CbmPipe,1) //CBMPIPE
+
+};
+
+#endif //PIPE_H
+

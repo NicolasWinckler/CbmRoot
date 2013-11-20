963e2e86372310bc095fea1a11fa2beaeb14823f
bergmann <bergmann@0381ead4-6506-0410-b988-94b70fbc4730>
2013-11-20 09:25:36 +0000

    added class to collect usefull helper visualization methods
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@22924 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ trd/tools/CbmTrdUtils.cxx
@@ -0,0 +1,203 @@
+/**
+ * \file CbmTrdUtils.cxx
+ * \author Cyrano Bergmann
+ * \date 18/11/13
+ **/
+
+#include "CbmTrdUtils.h"
+#include "CbmTrdAddress.h"
+#include "TMath.h"
+#include "TCanvas.h"
+CbmTrdUtils::CbmTrdUtils(){}
+CbmTrdUtils::~CbmTrdUtils(){}
+
+   void CbmTrdUtils::NiceTH1(TH1 *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle) 
+  {
+    h->SetStats(kFALSE);
+    h->SetMarkerStyle(mStyle);
+    h->SetMarkerSize(mSize);  
+    h->SetMarkerColor(color);
+    h->SetLineColor(color);  
+    h->GetXaxis()->SetLabelSize(0.03);
+    h->GetYaxis()->SetLabelSize(0.03);
+    //h->GetZaxis()->SetLabelSize(0.03);
+    h->GetXaxis()->SetTitleSize(0.035);
+    h->GetXaxis()->SetTitleOffset(1.25);
+    h->GetYaxis()->SetTitleSize(0.035);
+    h->GetYaxis()->SetTitleOffset(1.25);
+    //h->GetZaxis()->SetTitleSize(0.035);
+    //h->GetZaxis()->SetTitleOffset(-2);
+    h->SetXTitle(xTitle);
+    h->SetYTitle(yTitle);
+  }
+
+   void CbmTrdUtils::NiceTH2(TH2 *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle, TString zTitle) 
+  {
+  h->SetStats(kFALSE);
+    h->SetMarkerStyle(mStyle);
+    h->SetMarkerSize(mSize);  
+    h->SetMarkerColor(color);
+    h->SetLineColor(color);  
+    h->GetXaxis()->SetLabelSize(0.03);
+    h->GetYaxis()->SetLabelSize(0.03);
+    h->GetZaxis()->SetLabelSize(0.03);
+    h->GetXaxis()->SetTitleSize(0.035);
+    h->GetXaxis()->SetTitleOffset(1.5);
+    h->GetYaxis()->SetTitleSize(0.035);
+    h->GetYaxis()->SetTitleOffset(1.5);
+    h->GetZaxis()->SetTitleSize(0.035);
+    h->GetZaxis()->SetTitleOffset(1.25);
+    h->SetXTitle(xTitle);
+    h->SetYTitle(yTitle);
+    h->SetZTitle(zTitle);
+  }
+
+   void CbmTrdUtils::NiceTH3(TH3 *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle, TString zTitle) 
+  {
+  h->SetStats(kFALSE);
+    h->SetMarkerStyle(mStyle);
+    h->SetMarkerSize(mSize);  
+    h->SetMarkerColor(color);
+    h->SetLineColor(color);  
+    h->GetXaxis()->SetLabelSize(0.03);
+    h->GetYaxis()->SetLabelSize(0.03);
+    h->GetZaxis()->SetLabelSize(0.03);
+    h->GetXaxis()->SetTitleSize(0.035);
+    h->GetXaxis()->SetTitleOffset(1.25);
+    h->GetYaxis()->SetTitleSize(0.035);
+    h->GetYaxis()->SetTitleOffset(1.25);
+    h->GetZaxis()->SetTitleSize(0.035);
+    h->GetZaxis()->SetTitleOffset(1.25);
+    h->SetXTitle(xTitle);
+    h->SetYTitle(yTitle);
+    h->SetZTitle(zTitle);
+  }
+
+void CbmTrdUtils::NiceTProfile(TProfile *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle) 
+{
+  h->SetStats(kFALSE);
+  h->SetMarkerStyle(mStyle);
+  h->SetMarkerSize(mSize);  
+  h->SetMarkerColor(color);
+  h->SetLineColor(color);  
+  h->GetXaxis()->SetLabelSize(0.03);
+  h->GetYaxis()->SetLabelSize(0.03);
+  //h->GetZaxis()->SetLabelSize(0.03);
+  h->GetXaxis()->SetTitleSize(0.035);
+  h->GetXaxis()->SetTitleOffset(1.25);
+  h->GetYaxis()->SetTitleSize(0.035);
+  h->GetYaxis()->SetTitleOffset(1.25);
+  //h->GetZaxis()->SetTitleSize(0.035);
+  //h->GetZaxis()->SetTitleOffset(-2);
+  h->SetXTitle(xTitle);
+  h->SetYTitle(yTitle);
+}
+
+   void CbmTrdUtils::NiceLegend(TLegend *l)
+  {
+    l->SetLineColor(0);
+    l->SetLineStyle(0);
+    l->SetFillStyle(0);
+    l->SetTextSize(0.03);
+  }
+
+void CbmTrdUtils::Statusbar(Int_t i, Int_t n) {
+  if (int(i * 100 / float(n)) - int((i-1) * 100 / float(n)) >= 1) {
+    if (int(i * 100 / float(n)) == 1 || i == 1 || i == 0) 
+      cout << "[" << flush;
+    cout << "-" << flush;
+    if (int(i * 10 / float(n)) - int((i-1) * 10 / float(n)) >= 1) 
+      cout << "|";
+    if (int(i * 100 / float(n)) >=99) 
+      cout << "]" <<endl;
+  }
+}
+   
+void CbmTrdUtils::CreateLayerView(std::map<Int_t/*moduleAddress*/, TH1*>& Map, CbmTrdModule *fModuleInfo, CbmTrdDigiPar *fDigiPar, const TString folder, const TString pics, const TString zAxisTitle, const Double_t fmax, const Double_t fmin, const Bool_t logScale){
+  TString title(""), name("");
+  //name.Form("_TH_%.2EGeV_",fTriggerThreshold);
+  TPaveText *text=NULL;
+  std::map<Int_t, TH1*>::iterator MapIt;
+  std::vector<Int_t> fColors;
+  std::vector<Double_t> fZLevel;
+  //Double_t fmax(20), fmin(0);
+  for (Int_t i = 0; i < TColor::GetNumberOfColors(); i++){
+    fColors.push_back(TColor::GetColorPalette(i));
+    if (logScale)
+      fZLevel.push_back(fmin + TMath::Power(10, TMath::Log10(fmax) / TColor::GetNumberOfColors() * i));// log scale
+    else
+      fZLevel.push_back(fmin + (fmax / TColor::GetNumberOfColors() * i)); // lin scale
+  }
+  TH2I* fLayerDummy = new TH2I("LayerDummy","",1,-600,600,1,-500,500);
+  fLayerDummy->SetXTitle("x-coordinate [cm]");
+  fLayerDummy->SetYTitle("y-coordinate [cm]");
+  fLayerDummy->GetXaxis()->SetLabelSize(0.02);
+  fLayerDummy->GetYaxis()->SetLabelSize(0.02);
+  fLayerDummy->GetZaxis()->SetLabelSize(0.02);
+  fLayerDummy->GetXaxis()->SetTitleSize(0.02);
+  fLayerDummy->GetXaxis()->SetTitleOffset(1.5);
+  fLayerDummy->GetYaxis()->SetTitleSize(0.02);
+  fLayerDummy->GetYaxis()->SetTitleOffset(2);
+  fLayerDummy->GetZaxis()->SetTitleSize(0.02);
+  fLayerDummy->GetZaxis()->SetTitleOffset(-2);
+  fLayerDummy->SetStats(kFALSE);
+  fLayerDummy->SetContour(99);
+  fLayerDummy->Fill(0.,0.,0.);
+  std::map<Int_t, TCanvas*> fLayerMap;
+  std::map<Int_t, TCanvas*>::iterator fLayerMapIt; 
+  /*
+    gDirectory->pwd();
+    if (!gDirectory->Cd(folder)) 
+    gDirectory->mkdir(folder);
+    gDirectory->Cd(folder);
+  */
+  for (MapIt = Map.begin(); MapIt != Map.end(); MapIt++) {
+    Double_t value = MapIt->second->GetMean(1);
+    Double_t valueE = MapIt->second->GetRMS(1);
+    fModuleInfo = fDigiPar->GetModule(MapIt->first);
+    Int_t Station  = CbmTrdAddress::GetLayerId(MapIt->first) / 4 + 1;//fGeoHandler->GetStation(moduleId);
+    Int_t Layer    = CbmTrdAddress::GetLayerId(MapIt->first) % 4 + 1;//fGeoHandler->GetLayer(moduleId);
+    Int_t combiId = 10 * Station + Layer;
+    if (fLayerMap.find(combiId) == fLayerMap.end()){
+      title.Form("Station%i_Layer%i",Station,Layer);
+      fLayerMap[combiId] =  new TCanvas(title,title,1200,1000);
+      fLayerDummy->SetZTitle(zAxisTitle);
+      fLayerDummy->GetZaxis()->SetRangeUser(fmin,fmax);
+      fLayerDummy->Draw("colz");
+    }
+    fLayerMap[combiId]->cd();
+ 
+    text = new TPaveText(fModuleInfo->GetX()-fModuleInfo->GetSizeX(),
+			 fModuleInfo->GetY()-fModuleInfo->GetSizeY(),
+			 fModuleInfo->GetX()+fModuleInfo->GetSizeX(),
+			 fModuleInfo->GetY()+fModuleInfo->GetSizeY());
+    text->SetFillStyle(1001);
+    text->SetLineColor(1);
+
+    Int_t j = 0;
+    while ((value > fZLevel[j]) && (j < (Int_t)fZLevel.size())){
+      //printf ("              %i<%i %i    %E <= %E\n",j,(Int_t)fZLevel.size(),fColors[j], rate, fZLevel[j]);
+      j++;
+    }
+    text->SetFillColor(fColors[j]);
+    if (j >= (Int_t)fZLevel.size())
+      text->SetFillColor(2);
+
+    if (fColors[j]<65)
+      text->SetTextColor(kWhite);
+    title.Form("%.1f#pm%.1f",value,valueE);
+    text->AddText(title);
+    text->Draw("same");
+  }
+  for (fLayerMapIt = fLayerMap.begin(); fLayerMapIt != fLayerMap.end(); fLayerMapIt++) {
+    //fLayerMapIt->second->Write("", TObject::kOverwrite);
+    title.Form("pics/TrdQa%s_S%i_L%i_%s.pdf",pics.Data(),fLayerMapIt->first/10,fLayerMapIt->first-(fLayerMapIt->first/10)*10,name.Data());
+    fLayerMapIt->second->SaveAs(title);
+    title.ReplaceAll("pdf","png");
+    fLayerMapIt->second->SaveAs(title);
+  }
+  for (MapIt = Map.begin(); MapIt != Map.end(); MapIt++) {
+    //MapIt->second->Write("", TObject::kOverwrite);
+  }
+  //gDirectory->Cd("..");
+}
---
+++ trd/tools/CbmTrdUtils.h
@@ -0,0 +1,240 @@
+#ifndef CBMTRDUTILS_H
+#define CBMTRDUTILS_H 1
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TProfile.h"
+#include "TLegend.h"
+#include "TString.h"
+#include "TPaveText.h"
+#include "TColor.h"
+#include <iostream>
+#include <map>
+#include "CbmTrdModule.h"
+#include "CbmTrdDigiPar.h"
+using std::cout;
+using std::endl;
+using std::flush;
+
+class CbmTrdUtils
+{
+ public:
+
+  CbmTrdUtils();
+  virtual ~CbmTrdUtils();
+  void Statusbar(Int_t i, Int_t n);
+  void NiceLegend(TLegend *l);
+  void NiceTProfile(TProfile *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle) ;
+  void NiceTH3(TH3 *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle, TString zTitle);
+  void NiceTH2(TH2 *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle, TString zTitle);
+  void NiceTH1(TH1 *h, Int_t color, Int_t mStyle, Int_t mSize, TString xTitle, TString yTitle);
+  void CreateLayerView(std::map<Int_t/*moduleAddress*/, TH1*>& Map, CbmTrdModule *fModuleInfo, CbmTrdDigiPar *fDigiPar, const TString folder, const TString pics, const TString zAxisTitle, const Double_t fmax, const Double_t fmin, const Bool_t logScale);
+
+  static TString GetGeantName(Int_t GeantId) {
+    if (GeantId == 1)
+      return (TString)"#gamma";
+    else if (GeantId == 2)
+      return (TString)"e^{+}";
+    else if (GeantId == 3)
+      return (TString)"e^{-}";
+    else if (GeantId == 4)
+      return (TString)"#nu_{(e;#mu;#tau)}";
+    else if (GeantId == 5)
+      return (TString)"#mu^{+}";
+    else if (GeantId == 6)
+      return (TString)"#mu^{-}";
+    else if (GeantId == 7)
+      return (TString)"#pi^{0}";
+    else if (GeantId == 8)
+      return (TString)"#pi^{+}";
+    else if (GeantId == 9)
+      return (TString)"#pi^{-}";
+    else if (GeantId ==10)
+      return (TString)"K^{0}_{L}";
+    else if (GeantId ==11)
+      return (TString)"K^{+}";
+    else if (GeantId ==12)
+      return (TString)"K^{-}";
+    else if (GeantId ==13)
+      return (TString)"n";
+    else if (GeantId ==14)
+      return (TString)"p";
+    else if (GeantId ==15)
+      return (TString)"#bar{p}";
+    else if (GeantId ==16)
+      return (TString)"K^{0}_{S}";
+    else if (GeantId ==17)
+      return (TString)"#eta";
+    else if (GeantId ==18)
+      return (TString)"#Lambda";
+    else if (GeantId ==19)
+      return (TString)"#Sigma^{+}";
+    else if (GeantId ==20)
+      return (TString)"#Sigma^{0}";
+    else if (GeantId ==21)
+      return (TString)"#Sigma^{-}";
+    else if (GeantId ==22)
+      return (TString)"#Xi^{0}";
+    else if (GeantId ==23)
+      return (TString)"#Xi^{-}";
+    else if (GeantId ==24)
+      return (TString)"#Omega^{-}";
+    else if (GeantId ==25)
+      return (TString)"#bar{n}";
+    else if (GeantId ==26)
+      return (TString)"#bar{#Lambda}";
+    else if (GeantId ==27)
+      return (TString)"#bar{#Sigma}^{-}";
+    else if (GeantId ==28)
+      return (TString)"#bar{#Sigma}^{0}";
+    else if (GeantId ==29)
+      return (TString)"#bar{#Sigma}^{+}";
+    else if (GeantId ==30)
+      return (TString)"#bar{#Xi}^{0}";
+    else if (GeantId ==31)
+      return (TString)"#bar{#Xi}^{+}";
+    else if (GeantId ==32)
+      return (TString)"#bar{#Omega}^{+}";
+    else if (GeantId ==33)
+      return (TString)"#tau^{+}";
+    else if (GeantId ==34)
+      return (TString)"#tau^{-}";
+    else if (GeantId ==35)
+      return (TString)"D^{+}";
+    else if (GeantId ==36)
+      return (TString)"D^{-}";
+    else if (GeantId ==37)
+      return (TString)"D^{0}";
+    else if (GeantId ==38)
+      return (TString)"#bar{D}^{+}";
+    else if (GeantId ==39)
+      return (TString)"D_{S}^{+}";
+    else if (GeantId ==40)
+      return (TString)"#bar{D_{S}}^{-}";
+    else if (GeantId ==41)
+      return (TString)"#Lambda_{C}^{+}";
+    else if (GeantId ==42)
+      return (TString)"W^{+}";
+    else if (GeantId ==43)
+      return (TString)"W^{-}";
+    else if (GeantId ==44)
+      return (TString)"Z^{0}";
+    else if (GeantId ==45)
+      return (TString)"d";
+    else if (GeantId ==46)
+      return (TString)"t";
+    else if (GeantId ==47)
+      return (TString)"He";
+    else if (GeantId ==48)
+      return (TString)"#gamma_{RICH}";
+    else if (GeantId ==49)
+      return (TString)"Primary";
+    else
+      return (TString)"not known";
+  };
+  static Int_t PdgToGeant(Int_t PdgCode){
+    if (PdgCode == 22)
+      return 1;
+    if (PdgCode == -11)
+      return 2;
+    if (PdgCode == 11)
+      return 3;
+    if (PdgCode == 12 || PdgCode == 14 || PdgCode == 16)
+      return 4;
+    if (PdgCode == -13)
+      return 5;
+    if (PdgCode == 13)
+      return 6;
+    if (PdgCode == 111)
+      return 7;
+    if (PdgCode == 211)
+      return 8;
+    if (PdgCode == -211)
+      return 9;
+    if (PdgCode == 130)
+      return 10;
+    if (PdgCode == 321)
+      return 11;
+    if (PdgCode == -321)
+      return 12;
+    if (PdgCode == 2112)
+      return 13;
+    if (PdgCode == 2212)
+      return 14;
+    if (PdgCode == -2212)
+      return 15;
+    if (PdgCode == 310)
+      return 16;
+    if (PdgCode == 221)
+      return 17;
+    if (PdgCode == 3122)
+      return 18;
+    if (PdgCode == 3222)
+      return 19;
+    if (PdgCode == 3212)
+      return 20;
+    if (PdgCode == 3112)
+      return 21;
+    if (PdgCode == 3322)
+      return 22;
+    if (PdgCode == 3312)
+      return 23;
+    if (PdgCode == 3332)
+      return 24;
+    if (PdgCode == -2112)
+      return 25;
+    if (PdgCode == -3122)
+      return 26;
+    if (PdgCode == -3112)
+      return 27;
+    if (PdgCode == -3212)
+      return 28;
+    if (PdgCode == -3322)
+      return 30;
+    if (PdgCode == -3312)
+      return 31;
+    if (PdgCode == -3332)
+      return 32;
+    if (PdgCode == -15)
+      return 33;
+    if (PdgCode == 15)
+      return 34;
+    if (PdgCode == 411)
+      return 35;
+    if (PdgCode == -411)
+      return 36;
+    if (PdgCode == 421)
+      return 37;
+    if (PdgCode == -412)
+      return 38;
+    if (PdgCode == 431)
+      return 39;
+    if (PdgCode == -431)
+      return 40;
+    if (PdgCode == 4122)
+      return 41;
+    if (PdgCode == 24)
+      return 42;
+    if (PdgCode == -24)
+      return 43;  
+    if (PdgCode == 23)
+      return 44;
+    if (PdgCode == 50000050)
+      return 45;
+    if (PdgCode == 1000010020)
+      return 46;
+    if (PdgCode == 1000010030)
+      return 47;
+    if (PdgCode == 1000020040)
+      return 48;
+    //if (PdgCode == -1)
+    //return 49;
+    cout << PdgCode << endl;
+    return 49;
+  };
+ private:
+
+
+};
+#endif

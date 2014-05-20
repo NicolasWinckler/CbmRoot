b283738945c27f17266f4589d578673de9d75a2e
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2014-05-20 14:48:13 +0000

    simulate delta electrons on maf
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@25043 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/maf/maf_run_reco.sh
+++ macro/trd/maf/maf_run_reco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. /data6/cbm/software/cbmroot_trunk_20140508/build/config.sh > /dev/null
+. /data6/cbm/software/cbmroot_trunk_20140515/build/config.sh > /dev/null
 nEvents=$NEVENTS
 urqmd=$NURQMD
 echo "root -l -b -q run_reco_maf.C'('$nEvents,$urqmd')'"
--- macro/trd/maf/maf_run_sim.sh
+++ macro/trd/maf/maf_run_sim.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-. /data6/cbm/software/cbmroot_trunk_20140508/build/config.sh > /dev/null
+. /data6/cbm/software/cbmroot_trunk_20140515/build/config.sh > /dev/null
 nEvents=$NEVENTS
 urqmd=$NURQMD
 centrality=mbias
 echo "root -l -b -q run_sim_maf.C'('$nEvents,$urqmd,'\"'$centrality'\"'')'"
 #echo "nice root -l -b -q run_sim_maf.C'('$nEvents,$urqmd,'\"'$centrality'\"'')' &"  > mymafsim
-echo "root -l -b -q run_sim_maf.C'('$nEvents,$urqmd,'\"'$centrality'\"'')'"  > mymafsim
+#
+#echo "root -l -b -q run_sim_maf.C'('$nEvents,$urqmd,'\"'$centrality'\"'')'"  > mymafsim
+echo "root -l -b -q run_sim_maf_delta.C'('$nEvents,$urqmd,'\"'$centrality'\"'')'"  > mymafsim
 . mymafsim

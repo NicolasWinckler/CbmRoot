574daaf2a9d3a3882ef1796e827865fdaf16470d
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2014-06-10 14:34:54 +0000

    update to recent geometries
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@25312 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/geometry/plot_pad_size_in_layer.sh
+++ macro/trd/geometry/plot_pad_size_in_layer.sh
@@ -1,11 +1,12 @@
 ## plot only 1 pad size per module, no rows in sector
-# root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p4.digi.par\"\)
+# root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p_3e.digi.par\"\)
 #
 ## plot only 1 pad size per module, no rows in sector, only 1st layer of each station
-#root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p4.digi.par\",1,0,0\)
+#root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p_3e.digi.par\",1,0,0\)
+root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v14/trd_v14x_3e.digi.par\",1,0,0\)
 #
 ## plot all 3 pad sizes per module, rows in sector, all layers
-# root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p4.digi.par\",3,1,1\)
+# root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p_3e.digi.par\",3,1,1\)
 #
 ## plot all 3 pad sizes per module, rows in sector, only 1st layer of each station
-root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p4.digi.par\",3,1,0\)
+#root -l -b -q plot_pad_size_in_layer.C++\(\"trd.v13/trd_v13p_3e.digi.par\",3,1,0\)

440aa6e41473a808843e87ef533e61aed26f0a38
demscher <demscher@0381ead4-6506-0410-b988-94b70fbc4730>
2014-06-11 20:49:55 +0000

    update maf submit macros
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@25324 0381ead4-6506-0410-b988-94b70fbc4730

--- macro/trd/maf/submit_maf_reco.sh
+++ macro/trd/maf/submit_maf_reco.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-CBMPATH=/data6/cbm/software/cbmroot_trunk_20140508/macro/trd/maf
+#
+##   a reco job with 1000 events runs for about 00:30 h
+#
+#CBMPATH=/data6/cbm/software/cbmroot_trunk_20140508/macro/trd/maf
+CBMPATH=/data6/cbm/software/cbmroot_trunk_20140515/macro/trd/maf
+
 # NEVENTS can be between 1 and 1000 - (limit is number of events in urqmd file)
 NEVENTS=1000
 #//NEVENTS=1
+
 # NURQMD can be between 0 and 399 - (number of urqmd files)
-NURQMD=399
-#//NURQMD=0
-for (( i = 0; i <= $NURQMD; i++ )) do
-  qsub -q "long"  -d $CBMPATH/.. -e $CBMPATH/output -o $CBMPATH/output -v NEVENTS=$NEVENTS,NURQMD=$i "$CBMPATH/maf_run_reco.sh";
-# qsub -q "short" -d $CBMPATH/.. -e $CBMPATH/output -o $CBMPATH/output -v NEVENTS=$NEVENTS,NURQMD=$i "$CBMPATH/maf_run_reco.sh";
-  sleep 5
-done
+NURQMDSTART=0
+NURQMDEND=149
+#NURQMDEND=399
+
+for i in `seq -f %04.0f $NURQMDSTART $NURQMDEND` 
+do
+  echo -n "$i " 
+
+  if [ -s /data6/cbm/software/reco/test.eds.$i.root ] 
+  then
+    echo exists
+  else
+    j=`expr $i + 0`
+#     echo $j
+#   qsub -q "long"   -d $CBMPATH/.. -e $CBMPATH/output -o $CBMPATH/output -v NEVENTS=$NEVENTS,NURQMD=$j "$CBMPATH/maf_run_reco.sh";
+    qsub -q "medium" -d $CBMPATH/.. -e $CBMPATH/output -o $CBMPATH/output -v NEVENTS=$NEVENTS,NURQMD=$j "$CBMPATH/maf_run_reco.sh";
+#   qsub -q "short"  -d $CBMPATH/.. -e $CBMPATH/output -o $CBMPATH/output -v NEVENTS=$NEVENTS,NURQMD=$j "$CBMPATH/maf_run_reco.sh";
+    sleep 3
+  fi 
+done 

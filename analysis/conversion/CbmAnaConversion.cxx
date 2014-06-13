54741b8459d1f65854073ee9da86242dbe839b91
reinecke <reinecke@0381ead4-6506-0410-b988-94b70fbc4730>
2014-06-13 12:19:21 +0000

    some furter steps in conversion analysis
    git-svn-id: https://subversion.gsi.de/fairroot/cbmroot/trunk@25363 0381ead4-6506-0410-b988-94b70fbc4730

--- analysis/conversion/CbmAnaConversion.cxx
+++ analysis/conversion/CbmAnaConversion.cxx
@@ -9,6 +9,7 @@
 
 #include "TH1D.h"
 #include "TH1.h"
+#include "TH3.h"
 #include "TCanvas.h"
 #include "TClonesArray.h"
 
@@ -67,13 +68,46 @@ InitStatus CbmAnaConversion::Init()
 	if (NULL == fGlobalTracks) { Fatal("CbmAnaConversion::Init","No GlobalTrack array!"); }
 
    InitHistograms();
+   
+   
+   testint = 0;
 
    return kSUCCESS;
 }
 
 void CbmAnaConversion::InitHistograms()
 {
-	fhGammaZ = new TH1D("fhGammaZ", "fhGammaZ;Z [cm];Entries", 100, 0.0, 100.);
+	fHistoList.clear();
+	
+	fhGammaZ = new TH1D("fhGammaZ", "fhGammaZ;Z [cm];Entries", 300, 0.0, 300.);
+	fhGammaZ_selected = new TH1D("fhGammaZ_selected", "fhGammaZ_selected;Z [cm];Entries", 600, 0.0, 600.);
+	
+	fTest = new TH2D("fTest", "fTest; X; Y", 200, -100, 100, 400, -200, 200);
+	
+	fhNofElPrim = new TH1D("fhNofElPrim", "fhNofElPrim;Nof prim El;Entries", 10., -0.5, 9.5);
+	fhNofElSec = new TH1D("fhNofElSec", "fhNofElSec;Nof Sec El;Entries", 20., -0.5, 19.5);
+	fhNofElAll = new TH1D("fhNofElAll", "fhNofElAll;Nof All El;Entries", 30., -0.5, 29.5);
+	
+	fhTomography = new TH3D("fhTomography", "Tomography/fhTomography;X;Y;Z", 200, -200., 200., 200, -200., 200., 500, 0., 1000.);
+	fhTomography_XZ = new TH2D("fhTomography_XZ", "fhTomography_XZ;X;Z", 1600, -400., 400., 2400, 0., 1200.);
+	fhTomography_YZ = new TH2D("fhTomography_YZ", "fhTomography_YZ;Y;Z", 1600, -400., 400., 2400, 0., 1200.);
+	fHistoList.push_back(fhTomography);
+	fHistoList.push_back(fhTomography_XZ);
+	fHistoList.push_back(fhTomography_YZ);
+	
+	fhTomography_RICH_beampipe = new TH2D("fhTomography_RICH_beampipe", "fhTomography_RICH_beampipe;X;Y", 400, -100., 100., 200, -50., 50.);
+	fHistoList.push_back(fhTomography_RICH_beampipe);
+	
+	
+	fhConversion = new TH1D("fhConversion", "fhConversion;Z;# conversions", 1200, 0., 1200.);
+	fHistoList.push_back(fhConversion);
+	fhConversion_prob = new TH1D("fhConversion_prob", "fhConversion_prob;Z;# conversions", 1200, 0., 1200.);
+	fHistoList.push_back(fhConversion_prob);
+	
+	
+	fhInvariantMass_test = new TH1D("fhInvariant", "fhInvariant;mass;#", 1000, 0., 1000.);
+	fHistoList.push_back(fhInvariantMass_test);
+
 
 }
 
@@ -88,21 +122,38 @@ void CbmAnaConversion::Exec(
    for (int i = 0; i < nofPoints; i++) {
 	   CbmRichPoint* point = (CbmRichPoint*)fRichPoints->At(i);
 	   if (point == NULL) continue;
-	   //cout << point->GetX() << endl;
+	   //cout << point->GetX() << "\t" << point->GetY() << endl;
+	   fTest->Fill(point->GetX(), point->GetY());
    }
+   
+   
+   TomographyMC();
 
 
    Int_t nofMcTracks = fMcTracks->GetEntriesFast();
    int countPrimEl = 0;
+   int countSecEl = 0;
    int countAllEl  = 0;
    int countGammaEl = 0;
+   int countMothers = 0;
+   int countPrimPart = 0;
    for (int i = 0; i < nofMcTracks; i++) {
 
 	   CbmMCTrack* mctrack = (CbmMCTrack*)fMcTracks->At(i);
 	   if (mctrack == NULL) continue;
+	   
+	   if (mctrack->GetMotherId() == -1) {
+	   		countPrimPart++;
+	   }
+	   
 	   if (mctrack->GetMotherId() == -1 && TMath::Abs( mctrack->GetPdgCode())  == 11) {
 		  countPrimEl++;
 	   }
+	   
+	   if (mctrack->GetMotherId() != -1 && TMath::Abs( mctrack->GetPdgCode())  == 11) {
+		  countSecEl++;
+	   }
+	   
 	   if (TMath::Abs( mctrack->GetPdgCode())  == 11) {
 		   countAllEl++;
 	   }
@@ -113,7 +164,7 @@ void CbmAnaConversion::Exec(
 	   	   CbmMCTrack* mother = (CbmMCTrack*) fMcTracks->At(motherId);
 	   	   int mcMotherPdg  = -1;
 	       if (NULL != mother) mcMotherPdg = mother->GetPdgCode();
-	       cout << mcMotherPdg << endl;
+	       //cout << mcMotherPdg << endl;
 
 		  if (mcMotherPdg == 22) {
 			  TVector3 v;
@@ -122,12 +173,44 @@ void CbmAnaConversion::Exec(
 			  countGammaEl++;
 		  }
 	   }
+	   
+	   
+	   
+   }
+   
+   
+	fhNofElPrim->Fill(countPrimEl);
+	fhNofElSec->Fill(countSecEl);
+	fhNofElAll->Fill(countAllEl);
+   
+   if(countPrimEl == 2 && countAllEl == 4) {
+   		testint++;
+   		for (int i = 0; i < nofMcTracks; i++) {
+			CbmMCTrack* mctrack = (CbmMCTrack*)fMcTracks->At(i);
+			if (TMath::Abs( mctrack->GetPdgCode())  == 11) {
+				int motherId = mctrack->GetMotherId();
+				if (motherId == -1) continue;
+	   			CbmMCTrack* mother = (CbmMCTrack*) fMcTracks->At(motherId);
+	   			int mcMotherPdg  = -1;
+	    		if (NULL != mother) mcMotherPdg = mother->GetPdgCode();
+	    
+				if (mcMotherPdg == 22) {
+					TVector3 v;
+					mctrack->GetStartVertex(v);
+					fhGammaZ_selected->Fill(v.Z());
+		  		}
+	   		}
+	   }
    }
 
    cout << "Nof Prim El = " << countPrimEl << endl;
+   cout << "Nof Sec El = " << countSecEl << endl;
    cout << "Nof All El = " << countAllEl << endl;
    cout << "Nof Gamma El = " << countGammaEl << endl;
-
+   cout << "Nof McTracks = " << nofMcTracks << endl;
+   cout << "Nof PrimPart = " << countPrimPart << endl;
+   
+   
 
    Int_t ngTracks = fGlobalTracks->GetEntriesFast();
    for (Int_t i = 0; i < ngTracks; i++) {
@@ -207,10 +290,158 @@ void CbmAnaConversion::Exec(
 
 void CbmAnaConversion::Finish()
 {
+	Probability();
+	
 	TCanvas* c = new TCanvas();
-	DrawH1(fhGammaZ);
+	c->SetWindowSize(800, 1600);
+	DrawH1(fTest);
 	//fhGammaZ->Write();
+	
+	TCanvas* c2 = new TCanvas();
+	c2->SetWindowSize(2000, 1600);
+	DrawH1(fhGammaZ_selected);
+	
+	TCanvas* c3 = new TCanvas();
+	c3->SetWindowSize(2000, 1600);
+	gPad->Divide(3,1);
+	c3->cd(1);
+	DrawH1(fhNofElPrim);
+	c3->cd(2);
+	DrawH1(fhNofElSec);
+	c3->cd(3);
+	DrawH1(fhNofElAll);
+	
+	
+	//TCanvas* c4 = new TCanvas();
+	//c4->SetWindowSize(2000, 1600);
+	//DrawH1(fhTomography);
+	
+	
+	// Write histograms to a file
+	gDirectory->mkdir("analysis-conversion");
+	gDirectory->cd("analysis-conversion");
+	for (Int_t i = 0; i < fHistoList.size(); i++){
+		fHistoList[i]->Write();
+	}
+	gDirectory->cd("..");
 
+}
+
+
+void CbmAnaConversion::TomographyMC()
+{
+	Int_t nofMcTracks = fMcTracks->GetEntriesFast();
+	for (int i = 0; i < nofMcTracks; i++) {
+		CbmMCTrack* mctrack = (CbmMCTrack*)fMcTracks->At(i);
+		
+		if (mctrack == NULL) continue;
+	   
+		if (TMath::Abs( mctrack->GetPdgCode())  == 11) { 
+			int motherId = mctrack->GetMotherId();
+			if (motherId == -1) continue;
+			CbmMCTrack* mother = (CbmMCTrack*) fMcTracks->At(motherId);
+			int mcMotherPdg  = -1;
+			if (NULL != mother) mcMotherPdg = mother->GetPdgCode();
+
+			if (mcMotherPdg == 22) {
+				TVector3 v;
+				mctrack->GetStartVertex(v);
+				fhGammaZ->Fill(v.Z());
+			  
+				fhTomography->Fill(v.X(), v.Y(), v.Z());
+				fhTomography_XZ->Fill(v.X(), v.Z());
+				fhTomography_YZ->Fill(v.Y(), v.Z());
+				if(v.z() >= 220 && v.Z() <= 280) {
+					fhTomography_RICH_beampipe->Fill(v.X(), v.Y());
+				}
+				
+				fhConversion->Fill(v.z());
+			}
+		}
+	}
+}
+
+/*
+void CbmAnaConversion::TomographyReco()
+{
+	Int_t ngTracks = fGlobalTracks->GetEntriesFast();
+	for (int i = 0; i < ngTracks; i++) {
+		CbmGlobalTrack* gTrack = (CbmGlobalTrack*) fGlobalTracks->At(i);
+		
+		if (gTrack == NULL) continue;
+	   
+		if (TMath::Abs( gTrack->GetPdgCode())  == 11) {
+			int motherId = gTrack->GetMotherId();
+			if (motherId == -1) continue;
+			CbmGlobalTrack* mother = (CbmGlobalTrack*) fGlobalTracks->At(motherId);
+			int mcMotherPdg  = -1;
+			if (NULL != mother) mcMotherPdg = mother->GetPdgCode();
+
+			if (mcMotherPdg == 22) {
+				TVector3 v;
+				gTrack->GetStartVertex(v);
+			  
+				fhTomography->Fill(v.X(), v.Y(), v.Z());
+				fhTomography_XZ->Fill(v.X(), v.Z());
+				fhTomography_YZ->Fill(v.Y(), v.Z());
+			}
+		}
+	}
+
+}
+*/
+
+
+
+void CbmAnaConversion::Probability() 
+{
+	for(int i=1; i<=fhConversion->GetNbinsX(); i++) {
+		int temp = 0;
+		for(int run=1; run<=i; run++ ) {
+			temp += fhConversion->GetBinContent(run);
+		}
+		fhConversion_prob->SetBinContent(i, temp);
+	}
+}
+
+
+
+void CbmAnaConversion::InvariantMassTest() 
+{
+/*	Int_t nofMcTracks = fMcTracks->GetEntriesFast();
+	for (int i = 0; i < nofMcTracks; i++) {
+		CbmMCTrack* mctrack = (CbmMCTrack*)fMcTracks->At(i);
+		
+		if (mctrack == NULL) continue;
+	   
+		if (TMath::Abs( mctrack->GetPdgCode())  == 22) { 
+			int motherId = mctrack->GetMotherId();
+			if (motherId == -1) continue;
+			CbmMCTrack* mother = (CbmMCTrack*) fMcTracks->At(motherId);
+			int mcMotherPdg  = -1;
+			if (NULL != mother) mcMotherPdg = mother->GetPdgCode();
+
+			if (mcMotherPdg == 111) {	// pdf code 111 = pi0
+				TVector3 v;
+				mctrack->GetStartVertex(v);
+				fhGammaZ->Fill(v.Z());
+			}
+		}
+	}
+*/
+}
+
+
+
+int CbmAnaConversion::GetTest()
+{
+	return testint;
+}
+
+
+int CbmAnaConversion::GetNofEvents()
+{
+	return fEventNum;
 }
 
 
--- analysis/conversion/CbmAnaConversion.h
+++ analysis/conversion/CbmAnaConversion.h
@@ -13,8 +13,11 @@
 #include "FairTask.h"
 class TH1;
 class TH2;
+class TH3;
 class TH1D;
 class TH2D;
+class TH2I;
+class TH3D;
 class TClonesArray;
 class CbmRichRing;
 class TCanvas;
@@ -61,11 +64,34 @@ public:
     * \brief Inherited from FairTask.
     */
    virtual void Finish();
+   
+   virtual void TomographyMC();
+   //virtual void TomographyReco();
+   virtual void Probability();
+   virtual void InvariantMassTest();
+   virtual int GetTest();
+   virtual int GetNofEvents();
 
 
 
 private:
    TH1D * fhGammaZ;
+   TH1D * fhGammaZ_selected;
+   TH2D * fTest;
+   
+   TH1D * fhNofElPrim;
+   TH1D * fhNofElSec;
+   TH1D * fhNofElAll;
+   
+   TH3D * fhTomography;
+   TH2D * fhTomography_XZ;
+   TH2D * fhTomography_YZ;
+   TH2D * fhTomography_RICH_beampipe;
+   
+   TH1D * fhConversion;
+   TH1D * fhConversion_prob;
+   
+   TH1D * fhInvariantMass_test;
 
    TClonesArray* fRichPoints;
    TClonesArray* fMcTracks;
@@ -74,6 +100,8 @@ private:
    TClonesArray* fGlobalTracks;
 
    Int_t fEventNum;
+   
+   int testint;
 
 
 
@@ -81,6 +109,8 @@ private:
     * \brief Initialize histograms.
     */
    void InitHistograms();
+   
+   vector<TH1*> fHistoList; //list of all histograms
 
 
    /**

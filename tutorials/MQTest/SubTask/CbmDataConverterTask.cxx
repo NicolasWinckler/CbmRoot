7bd09db5923765b8d0371a993cb32d837daea708
NicolasWinckler <n.winckler@gsi.de>
2014-09-05 13:13:41 +0200

    test

--- tutorials/MQTest/SubTask/CbmDataConverterTask.cxx
+++ tutorials/MQTest/SubTask/CbmDataConverterTask.cxx
@@ -343,5 +343,7 @@ void CbmDataConverterTask::SetPrintOption(uint32_t DigiToPrint, bool print)
     fDigiToPrint=DigiToPrint;
     fStsConverter->SetPrintOption(fDigiToPrint,fPrint);
     fMuchConverter->SetPrintOption(fDigiToPrint,fPrint);
+    if(fPrint)
+        MQLOG(ERROR)<<"It PRINT";
 }
 
